53fb7a5ea4c37115960a1fb9fba4a11451ff533a
Morten Gjerding <mogje@fysik.dtu.dk>
2019-02-27 15:52:14 +0100

    Give summary of results

---
+++ recipies/summary.py
@@ -0,0 +1,28 @@
+def summary(**kwargs):
+    from pathlib import Path
+    from importlib import import_module
+    
+    pathlist = Path(__file__).parent.glob('*.py')
+    for path in pathlist:
+        name = path.with_suffix('').name
+        module = import_module('.recipies.' + name, package='mcr')
+
+        try:
+            module.print_results()
+        except AttributeError:
+            continue
+
+
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description='Print summary of results')
+    parser.set_defaults(func=main)
+
+    return parser
+
+
+def main(args=None):
+    if args is None:
+        parser = get_parser()
+        args = parser.parse_args()
+    summary(**vars(args))
