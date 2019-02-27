0428fed829912d26a9b08e5a7836e31e7d68e598
Morten Gjerding <mogje@fysik.dtu.dk>
2019-02-27 12:56:57 +0100

    Dynamically add modules to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
-from mcr.recipies import relax
+import importlib
+from pathlib import Path
+import sys
+modules = Path(__file__).parent.glob('./recipies/*.py')
+for module in modules:
+    name = module.with_suffix('').name
+    mod = importlib.import_module('.recipies.' + name, package='mcr')
+    sys.modules['mcr.' + name] = mod
+
