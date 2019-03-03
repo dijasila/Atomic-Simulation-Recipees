a2ab20f708771e7ab1955dd00213000d907a2a4b
Morten Gjerding <mogje@fysik.dtu.dk>
2019-03-03 15:44:00 +0100

    Move band structure data collection

--- recipies/bandstructure.py
+++ recipies/bandstructure.py
@@ -1,4 +1,5 @@
 import os
+import os.path as op
 
 import numpy as np
 from gpaw import GPAW
@@ -7,7 +8,8 @@ from ase.io import read
 from ase.geometry import crystal_structure_from_cell
 
 from ase.dft.kpoints import special_paths
-from c2db.utils import get_special_2d_path, eigenvalues, gpw2eigs
+from c2db.utils import get_special_2d_path, eigenvalues, gpw2eigs, spin_axis
+
 creates = ['bs.gpw', 'eigs_spinorbit.npz']
 dependencies = ['gs.py']
 
@@ -66,6 +68,79 @@ def bandstructure(kptpath=None, npoints=400, emptybands=20):
         with open('eigs_spinorbit.npz', 'wb') as f:
             np.savez(f, e_mk=e_km.T, s_mvk=s_kvm.transpose(2, 1, 0),
                      e_nosoc_skn=e_nosoc_skn, path=path)
+
+
+def is_symmetry_protected(kpt, op_scc):
+    mirror_count = 0
+    for symm in op_scc:
+        # Inversion symmetry forces spin degeneracy and 180 degree rotation
+        # forces the spins to lie in plane
+        if (np.allclose(symm, -1 * np.eye(3)) or
+                np.allclose(symm, np.array([-1, -1, 1] * np.eye(3)))):
+            return True
+        vals, vecs = np.linalg.eigh(symm)
+        # A mirror plane
+        if np.allclose(np.abs(vals), 1) and np.allclose(np.prod(vals), -1):
+            # Mapping k -> k, modulo a lattice vector
+            if np.allclose(kpt % 1, (np.dot(symm, kpt)) % 1):
+                mirror_count += 1
+    # If we have two or more mirror planes, then we must have a spin-degenerate
+    # subspace
+    if mirror_count >= 2:
+        return True
+    return False
+
+
+def collect_data(kvp, data, atoms, verbose):
+    """Band structure PBE and GW +- SOC."""
+    if not op.isfile('eigs_spinorbit.npz'):
+        return
+    print('Collecting PBE bands-structure data')
+    evac = kvp.get('evac')
+    with open('eigs_spinorbit.npz', 'rb') as fd:
+        soc = dict(np.load(fd))
+    if 'e_nosoc_skn' in soc and 'path' in soc:
+        eps_skn = soc['e_nosoc_skn']
+        path = soc['path']
+        # atoms = read('bs.gpw')
+    else:
+        nosoc = GPAW('bs.gpw', txt=None)
+        # atoms = nosoc.get_atoms()
+        eps_skn = eigenvalues(nosoc)
+        path = nosoc.get_bz_k_points()
+    npoints = len(path)
+    s_mvk = soc.get('s_mvk', soc.get('s_mk'))
+    if s_mvk.ndim == 3:
+        sz_mk = s_mvk[:, spin_axis(), :]  # take x, y or z component
+        if sz_mk.shape.index(npoints) == 0:
+            sz_mk = sz_mk.transpose()
+    else:
+        sz_mk = s_mvk
+
+    assert sz_mk.shape[1] == npoints, 'sz_mk has wrong dims'
+
+    efermi = np.load('gap_soc.npz')['efermi']
+    efermi_nosoc = np.load('gap.npz')['efermi']
+
+    pbe = {
+        'path': path,
+        'eps_skn': eps_skn - evac,
+        'efermi_nosoc': efermi_nosoc - evac,
+        'efermi': efermi - evac,
+        'eps_so_mk': soc['e_mk'] - evac,
+        'sz_mk': sz_mk}
+    try:
+        op_scc = data['op_scc']
+    except KeyError:
+        from gpaw.symmetry import atoms2symmetry
+        op_scc = atoms2symmetry(atoms).op_scc
+    magstate = kvp['magstate']
+    for idx, kpt in enumerate(path):
+        if (magstate == 'NM' and is_symmetry_protected(kpt, op_scc) or
+                magstate == 'AFM'):
+            pbe['sz_mk'][:, idx] = 0.0
+
+    data['bs_pbe'] = pbe
 
 
 def get_parser():
