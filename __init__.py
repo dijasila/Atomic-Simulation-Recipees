6ea5b5c870238e45e3ab08ab76f37fdcbe5b1187
Morten Gjerding <mogje@fysik.dtu.dk>
2019-03-26 09:31:29 +0100

    Make summary of recipes in repository

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
+import importlib
+from pathlib import Path
+
+files = Path(__file__).parent.glob('*.py')
+
+recipes = []
+for file in files:
+    is_recipe = False
+    with open(str(file), 'r') as fd:
+        for line in fd:
+            if line.startswith('def main('):
+                is_recipe = True
+                break
+    if is_recipe:
+        name = file.with_suffix('').name
+        module = importlib.import_module(f'asr.{name}')
+        recipes.append(module)
 
 
---
+++ __main__.py
@@ -0,0 +1,55 @@
+# Find external recipes
+def summary(content, indent=0, title=None, pad=2):
+    colwidth_c = []
+    for row in content:
+        if isinstance(row, str):
+            continue
+        for c, element in enumerate(row):
+            nchar = len(element)
+            try:
+                colwidth_c[c] = max(colwidth_c[c], nchar)
+            except IndexError:
+                colwidth_c.append(nchar)
+
+    output = ''
+    if title:
+        output = f'\n{title}\n'
+    for row in content:
+        out = ' ' * indent
+        if isinstance(row, str):
+            output += f'\n  {row}\n'
+            continue
+        for colw, desc in zip(colwidth_c, row):
+            out += f'{desc: <{colw}}' + ' ' * pad
+        output += out
+        output += '\n'
+
+    return output
+
+
+def check_recipes():
+    print('Checking recipes...')
+    from asr import recipes
+
+    attributes = ['main',
+                  'collect_data',
+                  'webpanel',
+                  'resources']
+
+    panel = []
+    panel.append(['name', *attributes])
+    for recipe in recipes:
+        status = [recipe.__name__]
+        for attr in attributes:
+            if hasattr(recipe, attr):
+                status.append('.')
+            else:
+                status.append('N')
+        panel.append(status)
+
+    pretty_output = summary(panel)
+    print(pretty_output)
+
+
+if __name__ == '__main__':
+    check_recipes()
