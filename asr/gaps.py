e89f7dc76e051e1d823f8b129f1ab2863e9ea5ea
asras <asbra@fysik.dtu.dk>
2019-03-29 15:50:36 +0100

    Removed junk

--- asr/gaps.py
+++ asr/gaps.py
@@ -229,27 +229,8 @@ def get_spin_direction(fname="anisotropy_xy.npz"):
     return theta, phi
 
 
-# def webpanel(row, key_descriptions):
-#     long_names = ['VBM vs. vacuum',
-#                   'CBM vs. vacuum',
-#                   'Band gap', 'Direct band gap']
-
-#     xcs = ['PBE', 'HSE', 'GLLBSC', 'GW']
-#     for xc, xc_name in zip(['', '_hse', '_gllbsc', '_gw'], xcs):
-#         for base, s, l in zip(['vbm', 'cbm', 'gap', 'dir_gap'],
-#                               ['VBM', 'CBM', '', ''], long_names):
-#             key = base + xc
-#             add_nosoc += [key]
-#             description = '{} ({})'.format(l, xc_name)
-#             if s:
-#                 value = (s, description, 'eV')
-#             else:
-#                 value = (description, '', 'eV')
-#             key_descriptions[key] = value
-
-
 group = 'Postprocessing'
-#dependencies = ['asr.anisotropy']
+dependencies = ['asr.anisotropy']
 
 
 if __name__=='__main__':
