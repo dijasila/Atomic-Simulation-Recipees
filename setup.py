288d6b3d1844c3a381b7f65799b07e8d8ae4e8d0
Morten Gjerding <mortengjerding@gmail.com>
2019-03-29 13:23:48 +0100

    Add gitlab-ci and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import re
+from pathlib import Path
+from setuptools import setup, find_packages
+
+txt = Path('asr/__init__.py').read_text()
+version = re.search("__version__ = '(.*)'", txt).group(1)
+
+long_description = Path('README.md').read_text()
+
+setup(name='asr',
+      version=version,
+      description='Atomic Simulation Recipes',
+      long_description=long_description,
+      author='M. N. Gjerding',
+      author_email='mogje@fysik.dtu.dk',
+      url='https://gitlab.com/mortengjerding/asr',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 0 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: '
+          'GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7'])
