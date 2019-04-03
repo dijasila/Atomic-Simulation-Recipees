395d87f386a3a9c8598065ae56f1d8dd13becc90
Morten Gjerding <mortengjerding@gmail.com>
2019-04-03 10:59:22 +0200

    Some more setup.py

--- setup.py
+++ setup.py
@@ -7,21 +7,23 @@ version = re.search("__version__ = '(.*)'", txt).group(1)
 
 long_description = Path('README.md').read_text()
 
-setup(name='asr',
-      version=version,
-      description='Atomic Simulation Recipes',
-      long_description=long_description,
-      author='M. N. Gjerding',
-      author_email='mogje@fysik.dtu.dk',
-      url='https://gitlab.com/mortengjerding/asr',
-      packages=find_packages(),
-      classifiers=[
-          'Development Status :: 0 - Beta',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: '
-          'GNU General Public License v3 or later (GPLv3+)',
-          'Operating System :: Unix',
-          'Programming Language :: Python :: 3 :: Only',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7'])
+setup(
+    name='asr',
+    version=version,
+    description='Atomic Simulation Recipes',
+    long_description=long_description,
+    author='M. N. Gjerding',
+    author_email='mogje@fysik.dtu.dk',
+    url='https://gitlab.com/mortengjerding/asr',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Click', 'spglib', 'ase', 'pytest', 'gpaw'],
+    classifiers=[
+        'Development Status :: 0 - Beta', 'Environment :: Console',
+        'Intended Audience :: Developers', 'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ])
