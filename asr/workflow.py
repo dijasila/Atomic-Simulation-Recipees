eafda3ca06a4a7f50071a095338f7bf1aec55c73
Morten Gjerding <mogje@fysik.dtu.dk>
2019-04-24 15:03:39 +0200

    Work on workflow recipe

--- asr/workflow.py
+++ asr/workflow.py
@@ -5,7 +5,37 @@ import click
 @click.command()
 def main():
     """Run all recipes"""
-    pass
+    from asr import quickinfo
+    print('Quickinfo...')
+    quickinfo.main(standalone_mode=False)
+
+    from asr import gs
+    print('Ground state...')
+    gs.main(standalone_mode=False)
+
+    from asr import convex_hull
+    print('Convex hull...')
+    convex_hull.main(standalone_mode=False, args=['--references', 'oqmd12.db'])
+
+    from asr import gaps
+    print('Band gaps...')
+    gaps.main(standalone_mode=False)
+  
+    from asr import bandstructure 
+    print('Band structure...')
+    bandstructure.main(standalone_mode=False)
+
+    from asr import bader
+    print('Bader analysis...')
+    bader.main(standalone_mode=False)
+
+    from asr import dos
+    print('Density of states...')
+    dos.main(standalone_mode=False)
+
+    from asr import collect
+    print('Collect data...')
+    collect.main(standalone_mode=False, args=['.'])
 
 
 folder = Path(__file__).parent.resolve()
