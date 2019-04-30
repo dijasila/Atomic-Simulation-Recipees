5318616dc53f9b9928cfbdf5bf53914a7391132d
Morten Gjerding <mogje@fysik.dtu.dk>
2019-04-30 15:56:43 +0200

    Add test to gitlab ci

--- asr/tests/test_style.py
+++ asr/tests/test_style.py
@@ -10,15 +10,14 @@ recipes = get_recipes()
 
 class TestImports():
     @pytest.mark.parametrize('recipe', recipes)
-    def test_imports(self, recipe):
+    def test_main_function(self, recipe):
         """Call all main functions with --help"""
-        print(recipe.__name__)
         # Make sure that the correct
         if hasattr(recipe, 'main'):
             try:
                 assert hasattr(recipe.main, '_asr_command')
             except AssertionError:
-                msg = ('Dont use @click.command! '
-                       'Please use the "from asr.utils import command"'
-                       ' in stead')
+                msg = ('Dont use @click.command! Please use '
+                       'the "from asr.utils import command" '
+                       'in stead')
                 raise AssertionError(msg)
