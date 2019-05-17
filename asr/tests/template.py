228593e33b02d768a8ad35a966fd6244e000e22c
Morten Gjerding <mortengjerding@gmail.com>
2019-05-17 23:32:08 +0200

    Update template

--- asr/tests/template.py
+++ asr/tests/template.py
@@ -15,7 +15,7 @@ def directory(tmpdir_factory):
     path = tmpdir_factory.mktemp(f'test_{recipename}')
     srcstart = Path(__file__).parent / 'Si.json'
     srcparams = Path(__file__).parent / 'small_params.json'
-    dststart = Path(path) / 'start.json'
+    dststart = Path(path) / 'unrelaxed.json'
     dstparams = Path(path) / 'params.json'
     dststart.write_bytes(srcstart.read_bytes())
     dstparams.write_bytes(srcparams.read_bytes())
@@ -26,10 +26,22 @@ class TestWorkflow():
     ...
 
 
+def relax(cls, directory):
+    with chdir(directory):
+        # Make sure to reload the module
+        recipe = Recipe.frompath('asr.relax', reload=True)
+        recipe.main(args=[])
+
+
+setattr(TestWorkflow, f'test_relax', relax)
+
 # Dynamically add tests of all recipes
 recipes = get_dep_tree(f'asr.{recipename}')
 for recipe in recipes:
     name = recipe.__name__
+
+    if recipe.name == 'asr.relax':
+        continue
 
     if recipe.group:
         if recipe.group == 'postprocessing':
@@ -44,14 +56,14 @@ for recipe in recipes:
     setattr(TestWorkflow, f'test_{name}', func)
 
 
-def collect(cls, directory, name=name):
+def collect(cls, directory):
     with chdir(directory):
         # Make sure to reload the module
         recipe = Recipe.frompath('asr.collect', reload=True)
         recipe.main(args=[])
 
 
-def browser(cls, directory, name=name):
+def browser(cls, directory):
     with chdir(directory):
         # Make sure to reload the module
         from asr.utils.cli import browser
