e2074e20b7c959da8c58cd782b5ec6cd4b779b00
Morten Gjerding <mortengjerding@gmail.com>
2019-05-30 23:31:12 +0200

    Update template and test generation

--- asr/tests/generatetests.py
+++ asr/tests/generatetests.py
@@ -1,17 +1,20 @@
 def generatetests():
     from pathlib import Path
-    from asr.utils import get_recipes
+    from asr.utils import get_recipes, get_dep_tree
 
     recipes = get_recipes(group='property')
 
     for recipe in recipes:
         name = recipe.__name__.split('.')[1]
-        if name == 'collect':
-            continue
         template = (Path(__file__).parent / 'template.py').read_text()
 
         text = template.replace('###', name)
 
+        depsection = ''
+        for dep in get_dep_tree(f'{recipe.name}'):
+            depsection += f"Recipe.frompath('{dep.name}', reload=True).run()\n"
+
+        text = text.replace('# DepSection #', depsection)
         testname = f'test_auto_{name}.py'
         print(f'Writing {testname}')
         (Path(__file__).parent / testname).write_text(text)
