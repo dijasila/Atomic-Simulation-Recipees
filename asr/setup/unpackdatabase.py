19d1be9e02780ebc39f11c89f6c1575d8d00420e
Morten Gjerding <mortengjerding@gmail.com>
2019-06-04 18:14:41 +0200

    Update docstring

--- asr/setup/unpackdatabase.py
+++ asr/setup/unpackdatabase.py
@@ -19,7 +19,57 @@ from asr.utils import command, argument, option
         help='Divide the tree into N chunks')
 def main(database, run, selection, tree_structure,
          sort, kvp, data, atomsname, chunks):
-    """Set up folders with atomic structures based on ase-database"""
+    """Unpack an ASE database to a tree of folders.
+
+    This setup recipe can unpack an ASE database to into folders
+    that have a tree like structure where directory names can be
+    given by the material parameters such stoichiometry, spacegroup
+    number for example: stoichiometry/spacegroup/formula.
+
+    The specific tree structure is given by the --tree-structure
+    option which can be customized according to the following table
+
+    \b
+    {stoi}: Material stoichiometry
+    {spg}: Material spacegroup number
+    {formula}: Chemical formula. A possible variant is {formula:metal}
+        in which case the formula will be sorted by metal atoms
+    {wyck}: Unique wyckoff positions. The unique alphabetically sorted
+        Wyckoff positions.
+    {uid}: This is a unique identifier which starts at 0 and adds 1 if
+        collisions (cases where two materials would go to the same folder)
+        occur. In practice, if two materials would be unpacked in A-0/
+        they would now be unpacked in A-0/ and A-1/.
+
+    By default, the atomic structures will be saved into an unrelaxed.json
+    file which is be ready to be relaxed. This filename can be changed with
+    the --atomsname switch.
+
+    If the database contains some interesting data and key-value-pairs
+    these can also be unpacked with the --kvp and --data switches,
+    respectively. The key-value-pairs will be saved to key-value-pairs.json
+    and each key in data will be saved to "key.json"
+
+    Examples:
+    For all these example, suppose you have a database named "database.db".
+
+    \b
+    Unpack database using default parameters:
+        asr run setup.unpackdatabase database.db --run
+    Don't actually unpack the database but do a dry-run
+        asr run setup.unpackdatabase database.db
+    Only select a part of the database to unpack:
+        asr run setup.unpackdatabase database.db --selection "natoms<3" --run
+    Set custom folder tree-structure
+        asr run setup.unpackdatabase database.db --tree-structure \
+            tree/{stoi}/{spg}/{formula:metal} --run
+    Divide the tree into 2 chunks (in case the study of the materials)
+    is divided between 2 people). Also sort after number of atoms,
+    so computationally expensive materials are divided evenly
+        asr run setup.unpackdatabase database.db --sort natoms --chunks 2 --run
+    Unpack key-value-pairs and data keys of the ASE database as well
+        asr run setup.unpackdatabase database.db --kvp --data --run
+    """
     from os import makedirs
     from pathlib import Path
     from ase.db import connect
