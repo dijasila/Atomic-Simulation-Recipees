de7de8a9bc9b287e05972c2579eee51eaf472105
fBert31 <fafb@dtu.dk>
2019-06-14 17:09:11 +0200

    new functions inside defect_supercell.py

--- asr/setup/defect_supercell.py
+++ asr/setup/defect_supercell.py
@@ -18,9 +18,42 @@ def main(structure):
     Path('something.json').write_text(json.dumps(results))
     return results
 
+def setup_supercell(structure, max_lattice=6., is_2D=True):
+    """
+    Sets up the supercell of a given structure depending on a 
+    maximum supercell lattice vector length for 2D or 3D structures.
 
-def calculate_something(number):
-    return number + 2
+    :param structure: input structure (primitive cell)
+    :param max_lattice (float): maximum supercell lattice vector length in Å
+    :param is_2D (boolean): choose 2D or 3D supercell
+
+    :return structure_sc: supercell structure 
+    """
+    # TBD    
+    return structure_sc, N_x, N_y, N_z
+
+def def setup_defects(structure, intrinsic=True, charge_states=3, vacancies=True,
+                  extrinsic=False, replace_list=None):
+    """
+    Sets up all possible defects (i.e. vacancies, intrinsic anti-sites, extrinsic
+    point defects('extrinsic=True')) for a given structure.
+
+    :param structure: input structure (primitive cell)
+    :param intrinsic (boolean): incorporate intrinsic point defects
+    :param vacancies (boolean): incorporate vacancies
+    :param extrinsic (boolean): incorporate extrinsic point defects
+    :param replace_list (array): array of extrinsic dopants and their respective positions
+
+    :return structure_dict: dictionary of all possible defect configurations of the given 
+                            structure with different charge states. The dictionary is built 
+                            up in the following way: 
+                            structure_dict = {'formula_N_x, N_y, N_z.(pristine, vacancy, defect)@
+                                               atom_index.charged_(q)': {'structure': defect_structure,
+                                               'parameters': parameters},
+                                              'formula_ ... : {'structure': ..., 'parameters': ...}}
+    """
+    # TBD
+    return structure_dict
 
 
 def collect_data(atoms):
