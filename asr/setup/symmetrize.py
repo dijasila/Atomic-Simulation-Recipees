2e17717cb98f2b1163a540b11dca69cb05a1aa35
Morten Gjerding <mortengjerding@gmail.com>
2019-06-19 15:29:43 +0200

    Update symmetrize with a GPAW check

--- asr/setup/symmetrize.py
+++ asr/setup/symmetrize.py
@@ -1,6 +1,37 @@
 from asr.utils import command, option
 
 
+def print_symmetries(symmetry):
+    n = len(symmetry.op_scc)
+    nft = symmetry.ft_sc.any(1).sum()
+    lines = ['Symmetries present (total): {0}'.format(n)]
+    if not symmetry.symmorphic:
+        lines.append(
+            'Symmetries with fractional translations: {0}'.format(nft))
+
+    # X-Y grid of symmetry matrices:
+
+    lines.append('')
+    nx = 6 if symmetry.symmorphic else 3
+    ns = len(symmetry.op_scc)
+    y = 0
+    for y in range((ns + nx - 1) // nx):
+        for c in range(3):
+            line = ''
+            for x in range(nx):
+                s = x + y * nx
+                if s == ns:
+                    break
+                op_c = symmetry.op_scc[s, c]
+                ft = symmetry.ft_sc[s, c]
+                line += '  (%2d %2d %2d)' % tuple(op_c)
+                if not symmetry.symmorphic:
+                    line += f' + ({ft:.3e})'
+            lines.append(line)
+        lines.append('')
+    return '\n'.join(lines)
+
+
 def atomstospgcell(atoms, magmoms=None):
     from ase.calculators.calculator import PropertyNotImplementedError
     lattice = atoms.get_cell()
@@ -17,9 +48,54 @@ def atomstospgcell(atoms, magmoms=None):
         return (lattice, positions, numbers)
 
 
+def symmetrize_atoms(atoms, tolerance=None, angle_tolerance=None,
+                     return_dataset=False):
+    """ Atoms() -> SymmetrizedAtoms()"""
+
+    import spglib
+    import numpy as np
+    from ase import Atoms
+
+    spgcell = atomstospgcell(atoms)
+    if len(spgcell) > 3:
+        magmoms_a = spgcell[3]
+    else:
+        magmoms_a = None
+        newmagmoms_a = None
+    dataset = spglib.get_symmetry_dataset(spgcell,
+                                          symprec=tolerance,
+                                          angle_tolerance=angle_tolerance)
+    
+    newspgcell = spglib.standardize_cell(spgcell, symprec=tolerance,
+                                         to_primitive=True,
+                                         no_idealize=False,
+                                         angle_tolerance=angle_tolerance)
+    newspgcell = spglib.standardize_cell(newspgcell, symprec=tolerance,
+                                         to_primitive=True,
+                                         no_idealize=False,
+                                         angle_tolerance=angle_tolerance)
+    if magmoms_a:
+        newmagmoms_a = np.zeros(len(newspgcell[1]), float)
+        newmagmoms_a[dataset['mapping_to_primitive']] = magmoms_a
+
+    newdataset = spglib.get_symmetry_dataset(newspgcell,
+                                             symprec=tolerance,
+                                             angle_tolerance=angle_tolerance)
+
+    cell = newspgcell[0]
+    spos_ac = newspgcell[1]
+    numbers = newspgcell[2]
+    idealized = Atoms(numbers=numbers, scaled_positions=spos_ac, cell=cell,
+                      pbc=True, magmoms=newmagmoms_a)
+    if return_dataset:
+        return idealized, dataset, newdataset
+
+    return idealized
+    
+
 @command('asr.setup.symmetrize',
          save_results_file=False)
-@option('--tolerance', type=float, default=1e-3,
+@option('--tolerance', type=float, default=1e-2,
         help='Tolerance when evaluating symmetries')
 @option('--angle-tolerance', type=float, default=0.1,
         help='Tolerance one angles when evaluating symmetries')
@@ -43,110 +119,73 @@ def main(tolerance, angle_tolerance):
     Symmetrize an atomic structure using the default tolerances
         asr run setup.symmetrize
     """
-
     import numpy as np
-    import spglib
     from ase.io import read, write
-    from ase.calculators.calculator import PropertyNotImplementedError
     atoms = read('original.json')
-    try:
-        magmoms_a = atoms.get_magnetic_moments()
-    except PropertyNotImplementedError:
-        magmoms_a = None
-        
-    spgcell = atomstospgcell(atoms)
-    symmetry = spglib.get_symmetry(spgcell, symprec=tolerance,
-                                   angle_tolerance=angle_tolerance)
-    spos_ac = atoms.get_scaled_positions(wrap=False)
-    cell_cv = atoms.get_cell()
 
-    spacegroup = spglib.get_spacegroup(spgcell, symprec=tolerance,
-                                       angle_tolerance=angle_tolerance)
+    assert atoms.pbc.all(), \
+        ('Symmetrization has only been tested for 3D systems! '
+         'To apply it to other systems you will have to test and update '
+         'the code.')
+    idealized = atoms.copy()
+    spgs = []
+    # There is a chance that the space group changes when symmetrizing
+    # structure.
+    maxiter = 2
+    for i in range(maxiter):
+        atol = angle_tolerance
+        idealized, dataset1, dataset2 = symmetrize_atoms(idealized,
+                                                         tolerance=tolerance,
+                                                         angle_tolerance=atol,
+                                                         return_dataset=True)
+        spg1 = '{} ({})'.format(dataset1['international'],
+                                dataset1['number'])
+        spg2 = '{} ({})'.format(dataset2['international'],
+                                dataset2['number'])
+        if i == 0:
+            spgs.extend([spg1, spg2])
+        else:
+            spgs.append(spg2)
 
-    uspos_sac = []
-    M_scc = []
-    for U_cc, t_c in zip(symmetry['rotations'], symmetry['translations']):
-        symspos_ac = np.dot(spos_ac, U_cc.T) + t_c
+        if spg1 == spg2:
+            break
+        print(f'Spacegroup changed {spg1} -> {spg2}. Trying again.')
+    else:
+        msg = 'Reached maximum iteration! Went through ' + ' -> '.join(spgs)
+        raise RuntimeError(msg)
+    print(f'Idealizing structure into spacegroup {spg2} using SPGLIB.')
 
-        symcell_cv = np.dot(U_cc.T, cell_cv)
-
-        # Cell metric
-        M_cc = np.dot(symcell_cv, symcell_cv.T)
-        M_scc.append(M_cc)
-        inds = []
-        for i, s_c in enumerate(spos_ac):
-            d_ac = s_c - symspos_ac
-            dm_ac = np.abs(d_ac - np.round(d_ac))
-            ind = np.argwhere(np.all(dm_ac < tolerance, axis=1))[0][0]
-            symspos_ac[ind] += np.round(d_ac[ind])
-            inds.append(ind)
-            assert atoms.numbers[i] == atoms.numbers[ind]
-            
-        assert len(set(inds)) == len(atoms)
-        uspos_ac = symspos_ac[inds]
-        uspos_sac.append(uspos_ac)
-
-        assert np.all(np.abs(spos_ac - uspos_ac) < tolerance)
-    spos_ac = np.mean(uspos_sac, axis=0)
-    M_cc = np.mean(M_scc, axis=0)
-
-    from ase.geometry.cell import cellpar_to_cell
-    dotprods = M_cc[[0, 1, 2, 1, 0, 0], [0, 1, 2, 2, 2, 1]]
-    l_c = np.sqrt(dotprods[:3])
-    angles_c = np.arccos(dotprods[3:] / l_c[[1, 0, 0]] / l_c[[2, 2, 1]])
-    angles_c *= 180 / np.pi
-
-    cp = np.concatenate([l_c, angles_c])
-    origcp = atoms.cell.cellpar()
-
-    nsym = len(symmetry['rotations'])
-    print(f'Forcing structure into spacegroup {spacegroup} '
-          f'with {nsym} symmetries (tol: {tolerance})')
-    
-    a1, b1, c1, alpha1, beta1, gamma1 = cp - origcp
-
-    print('Cell Change: (Δa, Δb, Δc, Δα, Δβ, Δγ) = '
-          f'({a1:.1e} Å, {b1:.1e} Å, {c1:.1e} Å, '
-          f'{alpha1:.2e}°, {beta1:.2e}°, {gamma1:.2e}°)')
-
-    abcabg = cp - origcp
-    assert (np.abs(abcabg[:3]) < 10 * tolerance).all(), \
-        'a, b and/or c changed too much! See output above.'
-    assert (np.abs(abcabg[3:]) < 10 * angle_tolerance).all(), \
-        'α, β and/or γ changed too much! See output above.'
-
-    origcell = atoms.get_cell()
-    ab_normal = np.cross(origcell[0], origcell[1])
-    cell = cellpar_to_cell(cp, ab_normal=ab_normal,
-                           a_direction=origcell[0])
-
-    origspos_ac = atoms.get_scaled_positions(wrap=False)
-    dpos_av = np.dot(spos_ac - origspos_ac, cell)
-    dpos_a = np.sqrt(np.sum(dpos_av**2, 1))
-    with np.printoptions(precision=2, suppress=False):
-        print(f'Change of positions:')
-        msg = '    '
-        for symbol, dpos in zip(atoms.symbols, dpos_a):
-            msg += f' {symbol}: {dpos:.1e} Å,'
-            if len(msg) > 70:
-                print(msg[:-1])
-                msg = '    '
-        print(msg[:-1])
-
-    assert (dpos_a < 10 * tolerance).all(), \
-        'Some atoms moved too much! See output above.'
-    atoms.set_cell(cell)
-    originatom = np.argmin(np.sum(np.abs(spos_ac * atoms.pbc), axis=1))
-    spos_ac -= spos_ac[originatom] * atoms.pbc
-    atoms.set_scaled_positions(spos_ac)
+    nsym = len(dataset2['rotations'])
 
     # Sanity check
-    newspacegroup = spglib.get_spacegroup(atomstospgcell(atoms,
-                                                         magmoms=magmoms_a),
-                                          symprec=tolerance,
-                                          angle_tolerance=angle_tolerance)
-    assert spacegroup == newspacegroup, f'{spacegroup} -> {newspacegroup}'
-    write('unrelaxed.json', atoms)
+    from gpaw.symmetry import Symmetry
+    id_a = idealized.get_atomic_numbers()
+    symmetry = Symmetry(id_a, idealized.cell, idealized.pbc,
+                        symmorphic=False,
+                        time_reversal=False)
+    symmetry.analyze(idealized.get_scaled_positions())
+
+    nsymgpaw = len(symmetry.op_scc)
+
+    write('unrelaxed.json', idealized)
+
+    if not nsym == nsymgpaw:
+        msg = ('GPAW not finding as many symmetries as SPGLIB.\n'
+               f'nsymgpaw={nsymgpaw} nsymspglib={nsym}\n')
+        
+        msg += 'GPAW symmetries:\n'
+        msg += symmetry.__str__()
+
+        msg += '\nSPGLIB symmetries:\n'
+        # Monkey patch object
+        spgsym = Symmetry(id_a, idealized.cell,
+                          symmorphic=False,
+                          time_reversal=False)
+
+        spgsym.op_scc = np.array([op_cc for op_cc in dataset2['rotations']])
+        spgsym.ft_sc = np.array([t_c for t_c in dataset2['translations']])
+        msg += print_symmetries(spgsym)
+        raise AssertionError(msg)
 
 
 group = 'setup'
