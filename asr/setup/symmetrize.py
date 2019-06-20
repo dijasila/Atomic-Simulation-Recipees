236f679a0dc62f917e470564c5cd9e7850db4a0b
Morten Gjerding <mogje@fysik.dtu.dk>
2019-06-20 18:51:40 +0200

    Update symmetrize with an algorithm to choose the origin

--- asr/setup/symmetrize.py
+++ asr/setup/symmetrize.py
@@ -1,6 +1,13 @@
 from asr.utils import command, option
 
 
+def sfrac(f):
+    from gpaw.symmetry import frac
+    if f == 0:
+        return '0'
+    return '%d/%d' % frac(f, n=2 * 3 * 4 * 5 * 6)
+
+
 def print_symmetries(symmetry):
     n = len(symmetry.op_scc)
     nft = symmetry.ft_sc.any(1).sum()
@@ -26,7 +33,10 @@ def print_symmetries(symmetry):
                 ft = symmetry.ft_sc[s, c]
                 line += '  (%2d %2d %2d)' % tuple(op_c)
                 if not symmetry.symmorphic:
-                    line += f' + ({ft:.3e})'
+                    try:
+                        line += ' + (%9s)' % sfrac(ft)
+                    except ValueError:
+                        line += f' + ({ft:.3e})'
             lines.append(line)
         lines.append('')
     return '\n'.join(lines)
@@ -34,7 +44,7 @@ def print_symmetries(symmetry):
 
 def atomstospgcell(atoms, magmoms=None):
     from ase.calculators.calculator import PropertyNotImplementedError
-    lattice = atoms.get_cell()
+    lattice = atoms.get_cell().array
     positions = atoms.get_scaled_positions(wrap=False)
     numbers = atoms.get_atomic_numbers()
     if magmoms is None:
@@ -44,12 +54,92 @@ def atomstospgcell(atoms, magmoms=None):
             magmoms = None
     if magmoms is not None:
         return (lattice, positions, numbers, magmoms)
-    else:
-        return (lattice, positions, numbers)
+    return (lattice, positions, numbers)
 
 
-def symmetrize_atoms(atoms, tolerance=None, angle_tolerance=None,
+def symmetrize_atoms(atoms, tolerance=None,
+                     angle_tolerance=None,
                      return_dataset=False):
+    import spglib
+    import numpy as np
+    from ase import Atoms
+    spgcell = atomstospgcell(atoms)
+    dataset = spglib.get_symmetry_dataset(spgcell, symprec=tolerance,
+                                          angle_tolerance=angle_tolerance)
+    cell_cv = spgcell[0]
+    spos_ac = spgcell[1]
+    numbers = spgcell[2]
+    if len(spgcell) > 3:
+        magmoms_a = spgcell[3]
+    else:
+        magmoms_a = None
+
+    uspos_sac = []
+    M_scc = []
+    origin_sc = []
+    point_c = np.zeros(3, float)
+    U_scc = dataset['rotations']
+    t_sc = dataset['translations']
+    # t_sc -= np.rint(t_sc)
+    for U_cc, t_c in zip(U_scc, t_sc):
+        origin_sc.append(np.dot(U_cc, point_c) + t_c)
+        symspos_ac = np.dot(spos_ac, U_cc.T) + t_c
+
+        symcell_cv = np.dot(U_cc.T, cell_cv)
+
+        # Cell metric
+        M_cc = np.dot(symcell_cv, symcell_cv.T)
+        M_scc.append(M_cc)
+        inds = []
+        for i, s_c in enumerate(spos_ac):
+            d_ac = s_c - symspos_ac
+            dm_ac = np.abs(d_ac - np.round(d_ac))
+            ind = np.argwhere(np.all(dm_ac < tolerance, axis=1))[0][0]
+            symspos_ac[ind] += np.round(d_ac[ind])
+            inds.append(ind)
+            assert atoms.numbers[i] == atoms.numbers[ind]
+
+        assert len(set(inds)) == len(atoms)
+        uspos_ac = symspos_ac[inds]
+        uspos_sac.append(uspos_ac)
+
+        assert np.all(np.abs(spos_ac - uspos_ac) < tolerance)
+
+    origin_sc = np.array(origin_sc)
+    origin_sc -= np.rint(t_sc)
+    origin_c = np.mean(origin_sc, axis=0)
+    # Shift origin
+    print('Origin shifted by', origin_c)
+    spos_ac = np.mean(uspos_sac, axis=0) - origin_c
+    M_cc = np.mean(M_scc, axis=0)
+
+    from ase.geometry.cell import cellpar_to_cell
+    dotprods = M_cc[[0, 1, 2, 1, 0, 0], [0, 1, 2, 2, 2, 1]]
+    l_c = np.sqrt(dotprods[:3])
+    angles_c = np.arccos(dotprods[3:] / l_c[[1, 0, 0]] / l_c[[2, 2, 1]])
+    angles_c *= 180 / np.pi
+
+    cp = np.concatenate([l_c, angles_c])
+
+    ab_normal = np.cross(atoms.cell[0], atoms.cell[1])
+    cell = cellpar_to_cell(cp, ab_normal=ab_normal,
+                           a_direction=atoms.cell[0])
+
+    idealized = Atoms(numbers=numbers,
+                      scaled_positions=spos_ac, cell=cell,
+                      pbc=True, magmoms=magmoms_a)
+
+    newdataset = spglib.get_symmetry_dataset(atomstospgcell(idealized),
+                                             symprec=tolerance,
+                                             angle_tolerance=angle_tolerance)
+    if return_dataset:
+        return idealized, origin_c, dataset, newdataset
+
+    return idealized, origin_c
+
+
+def symmetrize_atoms_spglib(atoms, tolerance=None, angle_tolerance=None,
+                            return_dataset=False):
     """ Atoms() -> SymmetrizedAtoms()"""
 
     import spglib
@@ -65,15 +155,35 @@ def symmetrize_atoms(atoms, tolerance=None, angle_tolerance=None,
     dataset = spglib.get_symmetry_dataset(spgcell,
                                           symprec=tolerance,
                                           angle_tolerance=angle_tolerance)
+    TM = dataset['transformation_matrix']
+    RM = dataset['std_rotation_matrix']
+    symcell = (TM.T).dot(dataset['std_lattice'].dot(np.linalg.inv(RM)))
+    assert np.allclose(symcell, spgcell[0], atol=1e-3)
+
+    sympos_ac = np.dot(dataset['std_positions'],
+                       np.linalg.inv(TM.T))
+    tmppos_ac = sympos_ac
+
+    spos_ac = []
+    inds = []
+
+    for pos_c, number in zip(spgcell[1], spgcell[2]):
+        delta_ac = tmppos_ac - pos_c
+        delta_ac -= np.rint(delta_ac)
+        ind = np.argmin(np.abs(delta_ac).sum(1))
+        if not number == dataset['std_types'][ind]:
+            continue
+        assert ind not in inds, delta_ac
+        inds.append(ind)
+        newpos_c = sympos_ac[ind] + np.rint(pos_c - tmppos_ac[ind])
+        spos_ac.append(newpos_c)
+
+    assert len(inds) == len(atoms)
+    spos_ac = np.mod(np.array(spos_ac), 1)
+
+    # Translate such that atoms are moved as little as possible
+    newspgcell = (symcell, spos_ac, spgcell[2])
     
-    newspgcell = spglib.standardize_cell(spgcell, symprec=tolerance,
-                                         to_primitive=True,
-                                         no_idealize=False,
-                                         angle_tolerance=angle_tolerance)
-    newspgcell = spglib.standardize_cell(newspgcell, symprec=tolerance,
-                                         to_primitive=True,
-                                         no_idealize=False,
-                                         angle_tolerance=angle_tolerance)
     if magmoms_a:
         newmagmoms_a = np.zeros(len(newspgcell[1]), float)
         newmagmoms_a[dataset['mapping_to_primitive']] = magmoms_a
@@ -134,10 +244,11 @@ def main(tolerance, angle_tolerance):
     maxiter = 2
     for i in range(maxiter):
         atol = angle_tolerance
-        idealized, dataset1, dataset2 = symmetrize_atoms(idealized,
-                                                         tolerance=tolerance,
-                                                         angle_tolerance=atol,
-                                                         return_dataset=True)
+        idealized, origin_c, dataset1, dataset2 = \
+            symmetrize_atoms(idealized,
+                             tolerance=tolerance,
+                             angle_tolerance=atol,
+                             return_dataset=True)
         spg1 = '{} ({})'.format(dataset1['international'],
                                 dataset1['number'])
         spg2 = '{} ({})'.format(dataset2['international'],
@@ -154,10 +265,44 @@ def main(tolerance, angle_tolerance):
         msg = 'Reached maximum iteration! Went through ' + ' -> '.join(spgs)
         raise RuntimeError(msg)
     print(f'Idealizing structure into spacegroup {spg2} using SPGLIB.')
+    write('unrelaxed.json', idealized)
 
+    # Check that the cell was only slightly perturbed
     nsym = len(dataset2['rotations'])
+    cp = atoms.cell.cellpar()
+    idcp = idealized.cell.cellpar()
+    deltacp = idcp - cp
+    abc, abg = deltacp[:3], deltacp[3:]
 
-    # Sanity check
+    print('Cell Change: (Δa, Δb, Δc, Δα, Δβ, Δγ) = '
+          f'({abc[0]:.1e} Å, {abc[1]:.1e} Å, {abc[2]:.1e} Å, '
+          f'{abg[0]:.2e}°, {abg[1]:.2e}°, {abg[2]:.2e}°)')
+
+    assert (np.abs(abc) < 10 * tolerance).all(), \
+        'a, b and/or c changed too much! See output above.'
+    assert (np.abs(abg[3:]) < 10 * angle_tolerance).all(), \
+        'α, β and/or γ changed too much! See output above.'
+
+    cell = idealized.get_cell()
+    spos_ac = atoms.get_scaled_positions(wrap=False)
+    idspos_ac = idealized.get_scaled_positions(wrap=False) + origin_c
+    dpos_av = np.dot(idspos_ac - spos_ac, cell)
+    dpos_a = np.sqrt(np.sum(dpos_av**2, 1))
+    with np.printoptions(precision=2, suppress=False):
+        print(f'Change of positions:')
+        msg = '    '
+        for symbol, dpos in zip(atoms.symbols, dpos_a):
+            msg += f' {symbol}: {dpos:.1e} Å,'
+            if len(msg) > 70:
+                print(msg[:-1])
+                msg = '    '
+        print(msg[:-1])
+
+    assert (dpos_a < 10 * tolerance).all(), \
+        'Some atoms moved too much! See output above.'
+
+    # Check that GPAW indeed does find the same symmetries as
+    # spglib
     from gpaw.symmetry import Symmetry
     id_a = idealized.get_atomic_numbers()
     symmetry = Symmetry(id_a, idealized.cell, idealized.pbc,
@@ -166,8 +311,6 @@ def main(tolerance, angle_tolerance):
     symmetry.analyze(idealized.get_scaled_positions())
 
     nsymgpaw = len(symmetry.op_scc)
-
-    write('unrelaxed.json', idealized)
 
     if not nsym == nsymgpaw:
         msg = ('GPAW not finding as many symmetries as SPGLIB.\n'
@@ -186,6 +329,8 @@ def main(tolerance, angle_tolerance):
         spgsym.ft_sc = np.array([t_c for t_c in dataset2['translations']])
         msg += print_symmetries(spgsym)
         raise AssertionError(msg)
+
+
 
 
 group = 'setup'
