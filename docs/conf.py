9b889bcc2185f732dd37f105a45d8b703bbed978
Morten Gjerding <mortengjerding@gmail.com>
2019-09-06 09:25:08 +0200

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,13 +31,17 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'autoapi.extension']
+autoapi_type = 'python'
+autoapi_dirs = ['../asr']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/install.rst
+++ docs/install.rst
@@ -6,13 +6,12 @@ Install ASE, ASR and GPAW from PyPI::
   $ pip install git+http://gitlab.com/mortengjerding/asr.git
   $ python3 -m pip install git+https://gitlab.com/gpaw/gpaw.git
 
-
-
-
 Additionally, you might also want
+
 * myqueue
 
 if you want to run jobs on a computer-cluster.
 
 Finally, test the package with::
+
   $ asr test
