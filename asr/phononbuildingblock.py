bcc124748b5d9d7493283a369be236e26341831b
Morten Gjerding <mogje@fysik.dtu.dk>
2019-09-13 16:31:20 +0200

    Make phonon building block recipe

---
+++ asr/phononbuildingblock.py
@@ -0,0 +1,56 @@
+from asr.utils import command
+
+
+def phononbuildingblock(atoms, Z_avv, force_constant_matrix):
+    # Simple function for calculating phonon building block
+
+    phbb = {'Z_avv': Z_avv,
+            'C_NN': force_constant_matrix,
+            'masses': atoms.get_masses(),
+            'symbols': atoms.get_chemical_symbols(),
+            'cell': atoms.cell}
+
+    return phbb
+
+
+@command(module='asr.phononbuildingblock',
+         requires=['borncharges-0.01.json',
+                   'results-asr.phonons@calculate.json'],
+         creates=['phononbuildingblock.npz'],
+         dependencies=['asr.phonons@calculate'])
+def main():
+    """Calculate Phonon Building Block for QEH model.
+
+    """
+
+    import numpy as np
+    from ase.io import read, jsonio
+    from gpaw.mpi import world
+    from asr.utils import read_json, get_dimensionality
+    from ase.phonons import Phonons
+    
+    dct = read_json('results-asr.phonons@calculate.json')
+    atoms = read('structure.json')
+    n = dct['__params__']['n']
+    nd = get_dimensionality()
+    assert nd == 2, 'Phonon BB can only be calculated for 2D mat'
+
+    supercell = (n, n, 1)
+    atoms = read('gs.gpw')
+    p = Phonons(atoms=atoms, supercell=supercell)
+    p.read()
+
+    C_NN = p.C_N.sum(axis=0)  # For q=0 the Fourier transform is just the sum
+
+    dct = jsonio.decode(read_json('borncharges-0.01.json'))
+    print(dct)
+    Z_avv = dct['Z_avv']
+    phbb = phononbuildingblock(atoms=atoms, Z_avv=Z_avv,
+                               force_constant_matrix=C_NN)
+
+    if world.rank == 0:
+        np.savez_compressed('phononbuildingblock.npz', **phbb)
+
+
+if __name__ == '__main__':
+    main.cli()
