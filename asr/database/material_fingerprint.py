4125854bb46039149708a655fb9fd710ef598ca9
Morten Gjerding <mogje@fysik.dtu.dk>
2019-09-19 08:59:41 +0200

    Update material fingerprint recipe

--- asr/database/material_fingerprint.py
+++ asr/database/material_fingerprint.py
@@ -7,14 +7,24 @@ def main():
     from hashlib import md5
     import json
     from ase.io import read
+    from collections import OrderedDict
 
     atoms = read('structure.json')
     dct = atoms.todict()
+
     for key, value in dct.items():
         if isinstance(value, np.ndarray):
             value = value.tolist()
         dct[key] = value
-    hash = md5(json.dumps(dct).encode()).hexdigest()
+
+    # Make sure that that keys appear in order
+    orddct = OrderedDict()
+    keys = list(dct.keys())
+    keys.sort()
+    for key in keys:
+        orddct[key] = dct[key]
+
+    hash = md5(json.dumps(orddct).encode()).hexdigest()
     results = {'asr_id': hash}
     results['__key_descriptions__'] = {'asr_id': 'KVP: Material fingerprint'}
     return results
