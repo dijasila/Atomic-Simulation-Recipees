4ef64b0ff6ad9c9551663f5e5c7bbc103b49b5ff
Morten Gjerding <mogje@fysik.dtu.dk>
2019-09-20 17:31:11 +0200

    Update strain setup

--- asr/setup/strains.py
+++ asr/setup/strains.py
@@ -17,9 +17,12 @@ def get_relevant_strains(pbc):
 
 def get_strained_folder_name(strain_percent, i, j):
     from pathlib import Path
+    import numpy as np
     itov_i = ['x', 'y', 'z']
     name = itov_i[i] + itov_i[j]
-    folder = Path(f'strains-{strain_percent}%-{name}/')
+    sign = ['', '+', '-'][int(np.sign(strain_percent))]
+    strain_percent = abs(float(strain_percent))
+    folder = Path(f'strains-{sign}{strain_percent}%-{name}/')
     return folder
 
 
@@ -41,18 +44,20 @@ def main(strain_percent=1, kptdensity=6.0):
 
     nk1, nk2, nk3 = size
     for i, j in ij:
-        strain_vv = np.eye(3)
-        strain_vv[i, j] = strain_percent / 100.0
-        strain_vv = (strain_vv + strain_vv.T) / 2
-        strained_cell_cv = np.dot(cell_cv, strain_vv)
-        atoms.set_cell(strained_cell_cv, scale_atoms=True)
-        folder = get_strained_folder_name(strain_percent, i, j)
-        folder.mkdir()
-        atoms.write(str(folder / 'unrelaxed.json'))
+        for sign in [-1, 1]:
+            signed_strain = sign * strain_percent
+            strain_vv = np.eye(3)
+            strain_vv[i, j] += signed_strain / 100.0
+            strain_vv = (strain_vv + strain_vv.T) / 2
+            strained_cell_cv = np.dot(cell_cv, strain_vv)
+            atoms.set_cell(strained_cell_cv, scale_atoms=True)
+            folder = get_strained_folder_name(signed_strain, i, j)
+            folder.mkdir()
+            atoms.write(str(folder / 'unrelaxed.json'))
 
-        with chdir(folder):
-            params = ("asr.relax:kpts {'size':[" +
-                      '{},{},{}'.format(*size)
-                      + ']}').split()
-            params.extend(['asr.relax:fixcell', 'True'])
-            setup_params(params=params)
+            with chdir(folder):
+                params = ("asr.relax:kpts {'size':[" +
+                          '{},{},{}'.format(*size)
+                          + ']}').split()
+                params.extend(['asr.relax:fixcell', 'True'])
+                setup_params(params=params)
