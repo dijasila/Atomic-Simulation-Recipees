c3fbd26135a678590c7709cff46131653e563403
Morten Gjerding <mortengjerding@gmail.com>
2019-10-03 08:45:03 +0200

    Update merge recipe

--- asr/database/merge.py
+++ asr/database/merge.py
@@ -9,7 +9,6 @@ def main(databases, databaseout, identifier='asr_id'):
     """Merge two ASE databases."""
     from ase.db import connect
     from pathlib import Path
-    from click import progressbar
 
     print(f'Merging {databases} into {databaseout}')
 
@@ -28,14 +27,14 @@ def main(databases, databaseout, identifier='asr_id'):
         tmpdest.unlink()
 
     # First merge rows common in both databases
-    for database in databases:
+    for i_d, database in enumerate(databases):
         # Database for looking up existing materials
         tmpdestsearch = Path('_' + str(tmpdest))
         if tmpdestsearch.is_file():
             tmpdestsearch.unlink()
 
         if tmpdest.is_file():
-            tmpdestsearch.write_bytes(tmpdest.read_bytes())
+            tmpdest.rename(tmpdestsearch)
 
         db = connect(database)
         dbsearch = connect(str(tmpdestsearch))
@@ -44,15 +43,14 @@ def main(databases, databaseout, identifier='asr_id'):
         # Update metadata
         metadata = {}
         if dbsearch.metadata:
-            metadata.update(dbsearch.metadata)
+            metadata.update(dbsearch.metadata.copy())
         if db.metadata:
-            metadata.update(db.metadata)
+            metadata.update(db.metadata.copy())
         dbmerged.metadata = metadata
 
-        selection = progressbar(list(db.select()),
-                                label=f'Merging {database}',
-                                item_show_func=item_show_func)
-        with dbmerged, dbsearch, selection:
+        selection = db.select()
+        with dbmerged:
+            id_matches = []
             for row1 in selection:
                 structid = row1.get(identifier)
                 matching = list(dbsearch.select(f'{identifier}={structid}'))
@@ -62,23 +60,33 @@ def main(databases, databaseout, identifier='asr_id'):
                                        f'in {databaseout} '
                                        f'matching {identifier}={structid}')
                 elif len(matching) == 0:
-                    dbmerged.write(row1.toatoms(),
-                                   key_value_pairs=row1.key_value_pairs,
-                                   data=row1.data)
+                    atoms = row1.toatoms()
+                    kvp = row1.key_value_pairs
+                    data = row1.data
                 else:
                     row2 = matching[0]
-                    data = row2.data.copy()
-                    kvp = row2.key_value_pairs.copy()
+                    id_matches.append(row2.id)
+                    data = row2.data
+                    kvp = row2.key_value_pairs
 
-                    data.update(row1.data)
-                    kvp.update(row1.key_value_pairs)
+                    data.update(row1.data.copy())
+                    kvp.update(row1.key_value_pairs.copy())
 
                     atoms1 = row1.toatoms()
                     atoms2 = row2.toatoms()
                     assert atoms1 == atoms2, 'Atoms not matching!'
-                    dbmerged.update(row2.id,
-                                    data=data,
-                                    **kvp)
+                    atoms = atoms1
+
+                dbmerged.write(atoms,
+                               key_value_pairs=kvp,
+                               data=data)
+
+            # Write the remaining rows from db2 that wasn't matched
+            for row2 in dbsearch.select():
+                if row2.id not in id_matches:
+                    dbmerged.write(row2.toatoms(),
+                                   key_value_pairs=row2.key_value_pairs,
+                                   data=row2.data)
 
     # Remove lookup db
     tmpdestsearch.unlink()
