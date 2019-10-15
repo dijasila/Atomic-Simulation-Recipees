260c3c82e35b22e067594be787e3fa43e8d0c3a0
Morten Gjerding <mortengjerding@gmail.com>
2019-10-15 22:37:37 +0200

    Update printing of errors

--- asr/core/testrunner.py
+++ asr/core/testrunner.py
@@ -72,7 +72,7 @@ class TestRunner:
             description = f'{testname}'
         return description
 
-    def run(self, raiseexc, tmpdir=None):
+    def run(self, tmpdir=None):
         # Make temporary directory and print some execution info
         self.cwd = Path('.').absolute()
         if tmpdir is None:
@@ -99,8 +99,6 @@ class TestRunner:
         else:
             self.log.write('All tests passed!\n')
         self.log.write('=' * 77 + '\n')
-        if raiseexc and self.failed:
-            raise AssertionError('Some tests failed!')
         return self.failed
 
     def run_tests(self):
