2da4f08a4fce99869e91d181c8aef77f030fedaf
Thorbjørn Skovhus <s134264@student.dtu.dk>
2020-01-06 14:18:14 +0100

    added get_orbital_ldos functionality

--- asr/pdos_bandstructure.py
+++ asr/pdos_bandstructure.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from asr.core import command
 
 
@@ -24,7 +26,88 @@ def webpanel(row, key_descriptions):
          dependencies=['asr.gs', 'asr.bandstructure'],
          webpanel=webpanel)
 def main():
-    return {}
+    from gpaw import GPAW
+
+    # Get bandstructure calculation
+    calc = GPAW('bs.gpw', txt=None)
+
+    results = {}
+
+    # Extract projections
+    weight_skni, yl_i = get_orbital_ldos(calc)
+    results['weight_skni'] = weight_skni
+    results['yl_i'] = yl_i
+    
+    return results
+
+
+# ---------- Recipe methodology ---------- #
+
+
+def get_orbital_ldos(calc):
+    """Get the projection weights on different orbitals
+
+    Returns
+    -------
+    weight_skni : nd.array
+        weight of each projector (indexed by (s, k, n)) on orbitals i
+    yl_i : list
+        symbol and orbital angular momentum string ('y,l') of each orbital i
+    """
+    from ase.utils import DevNull
+    from ase.parallel import parprint
+    import gpaw.mpi as mpi
+    from gpaw.utilities.dos import raw_orbital_LDOS
+    from gpaw.utilities.progressbar import ProgressBar
+    from asr.pdos import get_l_a
+
+    ns = calc.get_number_of_spins()
+    zs = calc.atoms.get_atomic_numbers()
+    chem_symbols = calc.atoms.get_chemical_symbols()
+    l_a = get_l_a(zs)
+
+    # We distinguish in (chemical symbol(y), angular momentum (l)),
+    # that is if there are multiple atoms in the unit cell of the same chemical
+    # species, their weights are added together.
+    # x index for each unique atom
+    a_x = [a for a in l_a for l in l_a[a]]
+    l_x = [l for a in l_a for l in l_a[a]]
+    # Get i index for each unique symbol
+    yl_i = []
+    i_x = []
+    for a, l in zip(a_x, l_x):
+        symbol = chem_symbols[a]
+        yl = ','.join([str(symbol), str(l)])
+        if yl in yl_i:
+            i = yl_i.index(yl)
+        else:
+            i = len(yl_i)
+            yl_i.append(yl)
+        i_x.append(i)
+
+    # Allocate output array
+    nk, nb = calc.wfs.kd.nibzkpts, calc.wfs.bd.nbands
+    weight_skni = np.zeros((ns, nk, nb, len(yl_i)))
+
+    # Set up progressbar
+    ali_x = [(a, l, i) for (a, l, i) in zip(a_x, l_x, i_x)]
+    parprint('Computing orbital ldos')
+    if mpi.world.rank == 0:
+        pb = ProgressBar()
+    else:
+        devnull = DevNull()
+        pb = ProgressBar(devnull)
+
+    for _, (a, l, i) in pb.enumerate(ali_x):
+        # Extract weights
+        for s in range(ns):
+            __, weights = raw_orbital_LDOS(calc, a, s, l)
+            weight_kn = weights.reshape((nk, nb))
+            # Renormalize (don't include reciprocal space volume in weight)
+            weight_kn /= calc.wfs.kd.weight_k[:, np.newaxis]
+            weight_skni[s, :, :, i] += weight_kn
+
+    return weight_skni, yl_i
 
 
 # ---------- Plotting ---------- #
