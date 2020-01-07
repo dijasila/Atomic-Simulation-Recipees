f2698270cde28881646881b8f4a90da3aac603ee
Hadeel Moustafa <hadeelm@sylg.fysik.dtu.dk>
2020-01-07 11:01:30 +0100

    1D

--- asr/bandstructure.py
+++ asr/bandstructure.py
@@ -40,7 +40,7 @@ def calculate(kptpath=None, npoints=400, emptybands=20):
     """Calculate electronic band structure"""
     from gpaw import GPAW
     from ase.io import read
-    atoms = read('structure.json')
+    atoms = read('gs.gpw')
     if kptpath is None:
         path = atoms.cell.bandpath(npoints=npoints, pbc=atoms.pbc)
     else:
