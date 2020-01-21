8747365797f4ccaa8cc54a3e0ce09e5055bfc602
Morten Gjerding <mortengjerding@gmail.com>
2020-01-21 22:51:25 +0100

    Add bandstructure test

---
+++ asr/test/test_bandstructure.py
@@ -0,0 +1,6 @@
+def test_bandstructure_gpaw_mock(isolated_filesystem, mock_GPAW):
+    from ase.build import bulk
+    from asr.bandstructure import main
+    structure = bulk('C')
+    structure.write('structure.json')
+    main()
--- asr/test/test_gs.py
+++ asr/test/test_gs.py
@@ -4,5 +4,4 @@ def test_gs_gpaw_mock(isolated_filesystem, mock_GPAW):
     structure = bulk('C')
     structure.write('structure.json')
     results = main()
-    print('gaps', results.get('gaps_nosoc').get('gap'), flush=True)
     assert results.get('gap') < 0.001
