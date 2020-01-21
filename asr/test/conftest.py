daa07232e17142dc257f0a429c2db47875bf0c69
Morten Gjerding <mogje@fysik.dtu.dk>
2020-01-21 16:56:37 +0100

    Make complete mock of GPAW

--- asr/test/conftest.py
+++ asr/test/conftest.py
@@ -1,14 +1,14 @@
 import pytest
 import os
 import numpy as np
-from ase.calculators.calculator import Calculator, kpts2ndarray
+from ase.calculators.calculator import kpts2ndarray
 from ase.units import Bohr, Ha
 
 from pathlib import Path
 
 
-class FreeElectronsGPAW(Calculator):
-    """Free-electron band calculator.
+class GPAWMock():
+    """Mock of GPAW calcualtor
 
     Parameters:
 
@@ -22,11 +22,20 @@ class FreeElectronsGPAW(Calculator):
     >>> calc = FreeElectrons(nvalence=1, kpts={'path': 'GXL'})
     """
 
-    implemented_properties = ['energy', 'forces', 'stress']
+    implemented_properties = ['energy', 'forces', 'stress', 'dipole',
+                              'magmom', 'magmoms']
+
     default_parameters = {'kpts': np.zeros((1, 3)),
                           'nvalence': 0.0,
                           'nbands': 20,
                           'gridsize': 7}
+
+    class Occupations:
+
+        def todict(self):
+            return {'name': 'SomeOccupationName', 'width': 0.05}
+
+    occupations = Occupations()
 
     class Setups(list):
         nvalence = 20
@@ -35,20 +44,19 @@ class FreeElectronsGPAW(Calculator):
     setups = Setups()
     setups.append(SimpleNamespace(symbol='MySymbol', fingerprint='asdf1234'))
 
-    def calculate(self, atoms, properties, system_changes):
-        Calculator.calculate(self, atoms)
-        self.kpts = kpts2ndarray(self.parameters.kpts, atoms)
-        icell = atoms.get_reciprocal_cell() * 2 * np.pi * Bohr
-        n = self.parameters.gridsize
+    def __init__(self, *args, **kwargs):
+        from ase.io import read
+        self.atoms = read('structure.json')
+        n = 4
+        self.nvalence = 4
+        self.kpts = kpts2ndarray([n, n, n], self.atoms)
+        self.nk = len(self.kpts)
+        icell = self.atoms.get_reciprocal_cell() * 2 * np.pi * Bohr
         offsets = np.indices((n, n, n)).T.reshape((n**3, 1, 3)) - n // 2
         eps = 0.5 * (np.dot(self.kpts + offsets, icell)**2).sum(2).T
         eps.sort()
-        if isinstance(self.parameters.nbands, str) and \
-           self.parameters.nbands[-1] == '%':
-            self.parameters.nbands = \
-                int(float(self.parameters.nbands[:-1]) / 100 * 20)
-        print(self.parameters.nbands)
-        self.eigenvalues = eps[:, :self.parameters.nbands] * Ha
+        nbands = 10
+        self.eigenvalues = eps[:, :nbands] * Ha
         self.results = {'energy': 0.0,
                         'forces': np.zeros((len(self.atoms), 3), float),
                         'stress': np.zeros((3, 3), float)}
@@ -57,9 +65,12 @@ class FreeElectronsGPAW(Calculator):
         assert spin == 0
         return self.eigenvalues[kpt].copy()
 
+    def get_k_point_weights(self):
+        return [1 / self.nk] * self.nk
+    
     def get_fermi_level(self):
         v = self.atoms.get_volume() / Bohr**3
-        kF = (self.parameters.nvalence / v * 3 * np.pi**2)**(1 / 3)
+        kF = (self.nvalence / v * 3 * np.pi**2)**(1 / 3)
         return 0.5 * kF**2 * Ha
 
     def get_ibz_k_points(self):
@@ -68,17 +79,94 @@ class FreeElectronsGPAW(Calculator):
     def get_number_of_spins(self):
         return 1
 
+    def get_number_of_bands(self):
+        return 10
+
+    def get_number_of_electrons(self):
+        return 4
+
     def write(self, name):
         Path(name).write_text('Test calculation')
 
     def read(self, name):
-        Path(name).read_text('Test calculation')
+        Path(name).read_text()
+
+    def get_electrostatic_potential():
+        pass
+
+    def get_property(self, name, atoms=None, allow_calculation=True):
+        assert name in self.implemented_properties
+
+        if name == 'magmom':
+            return 0.0
+
+        if name == 'energy':
+            return self.get_potential_energy(atoms)
+
+        if name == 'dipole':
+            return self.get_dipole_moment(atoms)
+
+        if name == 'stress':
+            return self.get_stress(atoms)
+
+        if name == 'forces':
+            return self.get_forces(atoms)
+
+    def todict(self):
+        return {'convergence': {'bands': 4}}
+
+    def get_forces(self, atoms=None):
+        if atoms:
+            na = len(atoms)
+        else:
+            na = len(self.atoms)
+        return np.zeros((na, 3), float)
+
+    def get_stress(self, atoms=None):
+        return np.eye(3)
+
+    def set_atoms(self, atoms):
+        self.atoms = atoms
+
+    def get_potential_energy(self, atoms=None, force_consistent=False):
+        return 1234.4321
 
 
 @pytest.fixture
-def mock_GPAW_freeelectrons(monkeypatch):
+def mock_GPAW(monkeypatch):
     import gpaw
-    monkeypatch.setattr(gpaw, 'GPAW', FreeElectronsGPAW)
+
+    def get_spinorbit_eigenvalues(calc, bands=None, gw_kn=None,
+                                  return_spin=False,
+                                  return_wfs=False, scale=1.0,
+                                  theta=0.0, phi=0.0):
+
+        nk = len(calc.get_ibz_k_points())
+        nspins = calc.get_number_of_spins()
+        nbands = calc.get_number_of_bands()
+        bands = list(range(nbands))
+
+        e_ksn = np.array([[calc.get_eigenvalues(kpt=k, spin=s)[bands]
+                           for s in range(nspins)] for k in range(nk)])
+
+        s_kvm = np.zeros((nk, 3, nbands), float)
+        s_kvm[:, 2, ::2] = 1
+        s_kvm[:, 2, ::2] = -1
+        e_km = e_ksn.reshape((nk, -1))
+        if return_spin:
+            return e_km.T, s_kvm
+        else:
+            return e_km.T
+
+    def occupation_numbers(occ, eps_skn, weight_k, nelectrons):
+        return 0, 1, 2, 3
+
+    monkeypatch.setattr(gpaw, 'GPAW', GPAWMock)
+    monkeypatch.setattr(gpaw.spinorbit, 'get_spinorbit_eigenvalues',
+                        get_spinorbit_eigenvalues)
+
+    monkeypatch.setattr(gpaw.occupations, 'occupation_numbers',
+                        occupation_numbers)
 
 
 @pytest.fixture
