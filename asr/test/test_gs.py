896afab10404b9b245315ede59f956377824cb4e
Morten Gjerding <mortengjerding@gmail.com>
2020-01-21 14:05:22 +0100

    Make gs test

---
+++ asr/test/test_gs.py
@@ -0,0 +1,14 @@
+def test_gs_calculate_gpaw_mock(isolated_filesystem, mock_GPAW_freeelectrons):
+    from ase.build import bulk
+    from asr.gs import calculate
+    structure = bulk('C')
+    structure.write('structure.json')
+    calculate()
+
+
+def test_gs_gpaw_mock(isolated_filesystem, mock_GPAW_freeelectrons):
+    from ase.build import bulk
+    from asr.gs import main
+    structure = bulk('C')
+    structure.write('structure.json')
+    main()
