486db3b993439d318ee73af957b6fbb6a116d5cb
Morten Gjerding <mogje@fysik.dtu.dk>
2020-01-22 16:16:55 +0100

    Update gs test

--- asr/test/conftest.py
+++ asr/test/conftest.py
@@ -4,8 +4,6 @@ import numpy as np
 from ase.calculators.calculator import kpts2ndarray, Calculator
 from ase.units import Bohr, Ha
 
-from pathlib import Path
-
 
 class GPAWMock(Calculator):
     """Mock of GPAW calculator.
@@ -13,19 +11,28 @@ class GPAWMock(Calculator):
     Sets up a free electron like gpaw calculator.
     """
 
-    implemented_properties = ['energy', 'forces',
-                              'stress', 'dipole',
-                              'magmom', 'magmoms']
+    implemented_properties = [
+        "energy",
+        "forces",
+        "stress",
+        "dipole",
+        "magmom",
+        "magmoms",
+    ]
 
-    default_parameters = {'kpts': (20, 20, 20),
-                          'nvalence': 1.0,
-                          'gridsize': 7,
-                          'nbands': 20,
-                          'nspins': 1}
+    default_parameters = {
+        "kpts": (4, 4, 4),
+        "nelectrons": 4,
+        "fermi_level": 0,
+        "gap": 0,
+        "gridsize": 3,
+        "nbands": 8,
+        "nspins": 1,
+    }
 
     class Occupations:
         def todict(self):
-            return {'name': 'SomeOccupationName', 'width': 0.05}
+            return {"name": "fermi-dirac", "width": 0.05}
 
     occupations = Occupations()
 
@@ -33,11 +40,11 @@ class GPAWMock(Calculator):
         nvalence = None
 
     from types import SimpleNamespace
+
     setups = Setups()
-    setups.append(SimpleNamespace(symbol='MySymbol', fingerprint='asdf1234'))
+    setups.append(SimpleNamespace(symbol="MySymbol", fingerprint="asdf1234"))
 
     class WaveFunctions:
-
         class GridDescriptor:
             pass
 
@@ -48,34 +55,60 @@ class GPAWMock(Calculator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         from ase.io import read
-        self.atoms = read('structure.json')
+
+        self.atoms = read("structure.json")
+
+    @classmethod
+    def set_property(cls, **kwargs):
+        cls.default_parameters.update(kwargs)
 
     def calculate(self, atoms, *args, **kwargs):
-        Calculator.calculate(self, atoms)
+        if atoms is not None:
+            self.atoms = atoms
+        print(f"Mocked GPAW parameters {self.parameters}")
+        if isinstance(self.parameters.nbands, str):
+            self.parameters.nbands = int(
+                float(self.parameters.nbands[:-1])
+                / 100
+                * self.parameters.nelectrons
+            )
         self.kpts = kpts2ndarray(self.parameters.kpts, atoms)
         icell = atoms.get_reciprocal_cell() * 2 * np.pi * Bohr
+
+        # Simple parabolic band
         n = self.parameters.gridsize
-        offsets = np.indices((n, n, n)).T.reshape((n**3, 1, 3)) - n // 2
-        eps = 0.5 * (np.dot(self.kpts + offsets, icell)**2).sum(2).T
-        eps.sort()
-        self.parameters.nbands = self.default_parameters['nbands']
-        self.setups.nvalence = self.parameters.nvalence
+        offsets = np.indices((n, n, n)).T.reshape((n ** 3, 1, 3)) - n // 2
+        eps_kn = 0.5 * (np.dot(self.kpts + offsets, icell) ** 2).sum(2).T
+        eps_kn.sort()
+
+        # If gap is finite we add another bunch of valence bands
+        gap = self.parameters.gap
+        eps_kn += gap * 0.5 / Ha
+        eps_kn = np.concatenate(
+            (-eps_kn[:, ::-1][:, -self.parameters.nelectrons:], eps_kn,),
+            axis=1,
+        )
+
+        self.setups.nvalence = self.parameters.nelectrons
         self.wfs.gd.cell_cv = atoms.get_cell() / Bohr
-        self.eigenvalues = eps[:, :self.parameters.nbands] * Ha
-        self.results = {'energy': 0.0,
-                        'forces': np.zeros((len(self.atoms), 3), float),
-                        'stress': np.zeros((3, 3), float),
-                        'dipole': np.zeros(3, float),
-                        'magmom': 0.0,
-                        'magmoms': np.zeros((len(self.atoms), 3), float)}
+        self.eigenvalues = eps_kn[:, : self.parameters.nbands] * Ha
+        assert self.eigenvalues.shape[0] == len(self.kpts)
+        assert self.eigenvalues.shape[1] == self.parameters.nbands
+
+        self.results = {
+            "energy": 0.0,
+            "forces": np.zeros((len(self.atoms), 3), float),
+            "stress": np.zeros((3, 3), float),
+            "dipole": np.zeros(3, float),
+            "magmom": 0.0,
+            "magmoms": np.zeros((len(self.atoms), 3), float),
+        }
 
     def get_fermi_level(self):
-        v = self.atoms.get_volume() / Bohr**3
-        kF = (self.parameters.nvalence / v * 3 * np.pi**2)**(1 / 3)
-        return 0.5 * kF**2 * Ha
+        return self.parameters.fermi_level
 
     def get_eigenvalues(self, kpt, spin=0):
-        return self.eigenvalues[kpt].copy()
+        return self.eigenvalues[kpt]
 
     def get_k_point_weights(self):
         return [1 / len(self.kpts)] * len(self.kpts)
@@ -90,22 +123,23 @@ class GPAWMock(Calculator):
         return self.parameters.nbands
 
     def get_number_of_electrons(self):
-        return self.parameters.nvalence
+        return self.parameters.nelectrons
 
     def write(self, name):
         from asr.core import write_json
+
         write_json(name, self.parameters)
 
     def read(self, name):
         from asr.core import read_json
-        
+
         class Parameters(dict):
             """Dictionary for parameters.
             
             Special feature: If param is a Parameters instance, then param.xc
             is a shorthand for param['xc'].
             """
-            
+
             def __getattr__(self, key):
                 if key not in self:
                     return dict.__getattribute__(self, key)
@@ -117,7 +151,8 @@ class GPAWMock(Calculator):
         parameters = Parameters(**read_json(name))
         self.parameters = parameters
         from ase.io import read
-        self.atoms = read('structure.json')
+
+        self.atoms = read("structure.json")
         self.calculate(self.atoms)
 
     def set_atoms(self, atoms):
@@ -128,18 +163,31 @@ class GPAWMock(Calculator):
 def mock_GPAW(monkeypatch):
     import gpaw
 
-    def get_spinorbit_eigenvalues(calc, bands=None, gw_kn=None,
-                                  return_spin=False,
-                                  return_wfs=False, scale=1.0,
-                                  theta=0.0, phi=0.0):
+    def get_spinorbit_eigenvalues(
+        calc,
+        bands=None,
+        gw_kn=None,
+        return_spin=False,
+        return_wfs=False,
+        scale=1.0,
+        theta=0.0,
+        phi=0.0,
+    ):
 
         nk = len(calc.get_ibz_k_points())
-        nspins = calc.get_number_of_spins()
+        nspins = 2
         nbands = calc.get_number_of_bands()
         bands = list(range(nbands))
 
-        e_ksn = np.array([[calc.get_eigenvalues(kpt=k, spin=s)[bands]
-                           for s in range(nspins)] for k in range(nk)])
+        e_ksn = np.array(
+            [
+                [
+                    calc.get_eigenvalues(kpt=k, spin=s)[bands]
+                    for s in range(nspins)
+                ]
+                for k in range(nk)
+            ]
+        )
 
         s_kvm = np.zeros((nk, 3, nbands), float)
         s_kvm[:, 2, ::2] = 1
@@ -151,17 +199,26 @@ def mock_GPAW(monkeypatch):
             return e_km.T
 
     def occupation_numbers(occ, eps_skn, weight_k, nelectrons):
-        return 0, 1, 2, 3
+        fermilevel = GPAWMock.default_parameters.get("fermi_level") / Ha
+        f_skn = (eps_skn < fermilevel).astype(float)
+        f_skn /= np.prod(f_skn.shape) * nelectrons
+        return f_skn, fermilevel, 0.0, 0.0
 
-    monkeypatch.setattr(gpaw, 'GPAW', GPAWMock)
-    monkeypatch.setattr(gpaw.spinorbit, 'get_spinorbit_eigenvalues',
-                        get_spinorbit_eigenvalues)
+    from ase.build import bulk
 
-    monkeypatch.setattr(gpaw.occupations, 'occupation_numbers',
-                        occupation_numbers)
+    structure = bulk("Si")
+    structure.write("structure.json")
+    monkeypatch.setattr(gpaw, "GPAW", GPAWMock)
+    monkeypatch.setattr(
+        gpaw.spinorbit, "get_spinorbit_eigenvalues", get_spinorbit_eigenvalues
+    )
+    monkeypatch.setattr(
+        gpaw.occupations, "occupation_numbers", occupation_numbers
+    )
+    return GPAWMock
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def isolated_filesystem(tmpdir):
     """A context manager that creates a temporary folder and changes
     the current working directory to it for isolated filesystem tests.
@@ -177,6 +234,7 @@ def isolated_filesystem(tmpdir):
 def pytest_configure(config):
     # register an additional marker
     config.addinivalue_line(
-        "markers", '''slow: marks tests as slow (deselect
- with '-m "not slow"')'''
+        "markers",
+        """slow: marks tests as slow (deselect
+ with '-m "not slow"')""",
     )
--- asr/test/test_gs.py
+++ asr/test/test_gs.py
@@ -1,12 +1,30 @@
-def test_gs_calculate(isolated_filesystem, mock_GPAW):
-    from ase.build import bulk
-    from asr.gs import calculate
-    structure = bulk('C')
-    structure.write('structure.json')
-    calculate()
+import pytest
+from pytest import approx
 
 
-def test_gs_main(isolated_filesystem, mock_GPAW):
-    from asr.gs import main
+@pytest.mark.parametrize("efermi", [0, 0.25, 0.99])
+@pytest.mark.parametrize("gap", [1, 2])
+def test_gs_main(isolated_filesystem, mock_GPAW, gap, efermi):
+    mock_GPAW.set_property(gap=gap, fermi_level=efermi)
+
+    from asr.gs import calculate, main
+
+    calculate(calculator={'name': 'gpaw',
+                          'mode': {'name': 'pw', 'ecut': 800},
+                          'xc': 'PBE',
+                          'basis': 'dzp',
+                          'kpts': {'density': 2, 'gamma': True},
+                          'occupations': {'name': 'fermi-dirac',
+                                          'width': 0.05},
+                          'convergence': {'bands': 'CBM+3.0'},
+                          'nbands': '200%',
+                          'txt': 'gs.txt',
+                          'charge': 0})
+
     results = main()
-    assert results.get('gap') < 0.001
+    if gap / 2 > efermi:
+        assert results.get('gap') == approx(gap)
+    else:
+        assert results.get('gap') == approx(0)
+    assert results.get('gaps_nosoc').get('efermi') == approx(efermi)
+    assert results.get('efermi') == approx(efermi)
