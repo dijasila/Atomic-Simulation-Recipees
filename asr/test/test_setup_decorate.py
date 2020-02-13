cc5172b489759f7989ec0244b720a022ab54996d
Morten Gjerding <mortengjerding@gmail.com>
2020-02-13 23:32:27 +0100

    Update test

--- asr/test/test_setup_decorate.py
+++ asr/test/test_setup_decorate.py
@@ -1,23 +1,17 @@
 import pytest
-from .conftest import test_materials
-
 from ase.build import bulk
 
 Si = bulk('Si')
 
 
 @pytest.mark.ci
-@pytest.mark.parametrize("atoms", [Si] + test_materials)
-def test_setup_decorate(separate_folder, usemocks, atoms):
+def test_setup_decorate_si(separate_folder, usemocks):
     from asr.setup.decorate import main
     from ase.io import write
     from ase.db import connect
     from pathlib import Path
-    write('structure.json', atoms)
+    write('structure.json', Si)
     main(atoms='structure.json')
-
     assert Path('decorated.db').is_file()
-
     db = connect('decorated.db')
-    if all(map(lambda x: x == 'Si', atoms.get_chemical_symbols())):
-        db.get('Ge')
+    db.get('Ge')
