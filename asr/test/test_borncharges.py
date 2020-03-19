ca149e4bf4e0138d5921dcc4d78c342e9ada4aad
Morten Gjerding <mortengjerding@gmail.com>
2020-03-19 11:14:10 +0100

    Update tests

--- asr/test/test_borncharges.py
+++ asr/test/test_borncharges.py
@@ -4,7 +4,7 @@ from .conftest import test_materials
 
 @pytest.mark.ci
 @pytest.mark.parametrize("atoms", test_materials)
-def test_formalpolarization(separate_folder, mockgpaw, atoms):
+def test_borncharges(separate_folder, mockgpaw, atoms):
     from asr.formalpolarization import main
     atoms.write('structure.json')
     main()
