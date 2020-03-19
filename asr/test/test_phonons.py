87c510fca970b1e9aa013075942eff7916dcebbf
Morten Gjerding <mortengjerding@gmail.com>
2020-03-19 17:17:05 +0100

    Add docstring for phonon test

--- asr/test/test_phonons.py
+++ asr/test/test_phonons.py
@@ -5,6 +5,7 @@ from .conftest import test_materials
 @pytest.mark.ci
 @pytest.mark.parametrize("atoms", test_materials)
 def test_phonons(separate_folder, mockgpaw, atoms):
+    """Simple test of phonon recipe."""
     from asr.core import read_json
     from asr.phonons import main
     atoms.write('structure.json')
