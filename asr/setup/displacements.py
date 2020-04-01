437d2f3fd4835a70da7d8c095b06752769b1533f
Morten Gjerding <mortengjerding@gmail.com>
2020-04-01 12:45:09 +0200

    Make displacements setup recipe

---
+++ asr/setup/displacements.py
@@ -0,0 +1,59 @@
+"""Module for generating atomic structures with displaced atoms.
+
+The main recipe of this module is :func:`asr.setup.displacements.main`
+
+.. autofunction:: asr.setup.displacements.main
+"""
+
+from pathlib import Path
+from asr.core import command, option
+
+
+def get_displacement_folder(atomic_index,
+                            cartesian_index,
+                            displacement_sign,
+                            displacement):
+    cartesian_symbol = 'xyz'[cartesian_index]
+    displacement_symbol = '-+'
+    foldername = (f'{displacement}-{atomic_index}'
+                  f'-{displacement_symbol}{cartesian_symbol}')
+    folder = Path('displacements') / foldername
+    return folder
+
+
+def create_displacements_folder(folder):
+    folder.mkdir(parent=True, exist_ok=False)
+
+
+def get_all_displacements(atoms):
+    for ia in len(range(atoms)):
+        for iv in range(3):
+            for sign in [-1, 1]:
+                yield ia, iv, sign
+
+
+@command('asr.setup.displacements')
+@option('--delta', help='How much to displace atoms.')
+def main(delta=0.01):
+    """Generate atomic displacements.
+
+    Generate atomic structures with displaced atoms. The generated
+    atomic structures are written to 'structure.json' and put into a
+    directory with the structure
+
+         displacements/{displacement}-{atomic_index}-{displacement_symbol}{cartesian_symbol}
+
+    Notice that all generated directories are a sub-directory of displacements/.
+
+    """
+    from ase.io import read
+    structure = read('structure.json')
+    structure_positions_av = structure.get_positions()
+    for ia, iv, sign in get_all_displacements(structure):
+        folder = get_displacement_folder(ia, iv,
+                                         sign, delta)
+        create_displacements_folder(folder)
+        new_structure_positions_av = structure_positions_av.copy()
+        new_structure_positions_av[ia, iv] += sign * delta
+        structure.set_positions(new_structure_positions_av)
+        structure.write(folder / 'structure.json')
