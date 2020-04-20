0e5007b06caa4b335945398475de7a6318ed7c4a
Morten Gjerding <mortengjerding@gmail.com>
2020-04-20 10:15:02 +0200

    Update plasmafrequency to clean up after itself

--- asr/plasmafrequency.py
+++ asr/plasmafrequency.py
@@ -19,14 +19,23 @@ def get_kpts_size(atoms, density):
 def calculate(kptdensity=20):
     """Calculate excited states for polarizability calculation."""
     from gpaw import GPAW
+    from ase.parallel import world
+    from pathlib import Path
 
     calc_old = GPAW('gs.gpw', txt=None)
     kpts = get_kpts_size(atoms=calc_old.atoms, density=kptdensity)
     nval = calc_old.wfs.nvalence
-    calc = GPAW('gs.gpw', fixdensity=True, kpts=kpts,
-                nbands=2 * nval, txt='gsplasma.txt')
-    calc.get_potential_energy()
-    calc.write('es_plasma.gpw', 'all')
+    try:
+        calc = GPAW('gs.gpw', fixdensity=True, kpts=kpts,
+                    nbands=2 * nval, txt='gsplasma.txt')
+        calc.get_potential_energy()
+        calc.write('es_plasma.gpw', 'all')
+    except Exception:
+        if world.rank == 0:
+            es_file = Path("es_plasma.gpw")
+            if es_file.is_file():
+                es_file.unlink()
+        world.barrier()
 
 
 def webpanel(row, key_descriptions):
@@ -55,6 +64,8 @@ def main(tetra=True):
     from ase.io import read
     import numpy as np
     from ase.units import Hartree, Bohr
+    from pathlib import Path
+    from ase.parallel import world
 
     atoms = read('structure.json')
     nd = sum(atoms.pbc)
@@ -74,10 +85,17 @@ def main(tetra=True):
                   'domega0': 0.2,
                   'ecut': 1}
 
-    df = DielectricFunction('es_plasma.gpw', **kwargs)
-    df.get_polarizability(q_c=[0, 0, 0], direction='x',
-                          pbc=[True, True, False],
-                          filename=None)
+    try:
+        df = DielectricFunction('es_plasma.gpw', **kwargs)
+        df.get_polarizability(q_c=[0, 0, 0], direction='x',
+                              pbc=[True, True, False],
+                              filename=None)
+    finally:
+        if world.rank == 0:
+            es_file = Path("es_plasma.gpw")
+            if es_file.is_file():
+                es_file.unlink()
+        world.barrier()
     plasmafreq_vv = df.chi0.plasmafreq_vv.real
     data = {'plasmafreq_vv': plasmafreq_vv,
             '__key_descriptions__': {'plasmafreq_vv':
