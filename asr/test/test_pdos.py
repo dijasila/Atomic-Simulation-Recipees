96cd78b8dcd4b93b23ce71a287776ffe6aaaf499
Thorbjørn Skovhus <s134264@student.dtu.dk>
2020-04-24 14:14:17 +0200

    make a mocked version of raw_orbital_ldos

--- asr/test/test_pdos.py
+++ asr/test/test_pdos.py
@@ -13,6 +13,44 @@ class MockProgressbar:
         return enumerate(iterable)
 
 
+def get_number_of_electrons(angular):
+    """Make a simple calculation of the number of electrons 
+    based on the occurence of orbitals."""
+    count = 0
+    count += 2 * angular.count('s')
+    count += 6 * angular.count('p')
+    count += 10 * angular.count('d')
+    count += 14 * angular.count('f')
+
+    return count
+
+
+def mock_ldos(calc, a, spin, angular='spdf', **kwargs):
+    """Distribute weights on atoms, spins and angular momentum."""
+    import numpy as np
+    from asr.pdos import get_l_a
+
+    # Extract eigenvalues and weights
+    e_kn = calc.get_all_eigenvalues()
+    w_k = calc.wfs.kd.weight_k
+
+    # Do a simple normalization of the weights based atoms and spin
+    w_k /= 1  # calc.wfs.nspins not implemented yet in mock_gpaw
+    w_k /= len(calc.atoms)
+
+    # Figure out the total number of orbitals to be counted
+    l_a = get_l_a(calc.atoms.get_atomic_numbers())
+    nstates = 0
+    for a, orbitals in l_a.items():
+        nstates += get_number_of_electrons(orbitals)
+    # Normalize weights
+    w_k *= get_number_of_electrons(angular) / nstates
+
+    # Return flattened energies and weights
+    nb = e_kn.shape[1]
+    return e_kn.flatten(), np.tile(w_k, nb)
+
+
 # ---------- Tests ---------- #
 
 
@@ -21,9 +59,12 @@ def test_pdos(asr_tmpdir_w_params, mockgpaw, mocker,
               test_material, get_webcontent):
     from asr.pdos import main
 
-    mocker.patch("gpaw.utilities.dos.raw_orbital_LDOS", create=True)
     mocker.patch("gpaw.utilities.progressbar.ProgressBar",
                  MockProgressbar)
+    mocker.patch("gpaw.utilities.dos.raw_orbital_LDOS",
+                 mock_ldos)
+    mocker.patch("asr.pdos.raw_spinorbit_orbital_LDOS_hack",
+                 mock_ldos)
     test_material.write('structure.json')
     main()
     get_webcontent()
