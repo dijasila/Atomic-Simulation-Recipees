338a4d8f8ddc13aac43ecdb1984ea07723619947
Mark_Kamper <markas@dtu.dk>
2020-04-24 16:17:00 +0200

    Updated duplicates.py and test_duplicates.py

--- asr/test/test_duplicates.py
+++ asr/test/test_duplicates.py
@@ -1,5 +1,4 @@
 import pytest
-from ase.build import mx2
 from .materials import std_test_materials
 
 
@@ -11,30 +10,23 @@ def duplicates_test_material():
 @pytest.fixture()
 def duplicates_test_db(asr_tmpdir):
     import ase.db
-    import numpy as np
-    from ase.io import write
-    from asr.structureinfo import main
-    from gpaw import GPAW
-
 
     db = ase.db.connect("db.db")
-    
+
     # Setting up the reference database
-    
+
     non_mag_ref = std_test_materials[1]
     data_non_mag = {"magstate": "NM"}
-    db.write(non_mag_ref, data_non_mag) 
-    
-    
-    mag_ref = std_test_materials[1] 
-    data_mag = {"magstate": "FM"}
-    db.write(mag_ref, data_mag) 
-   
+    db.write(non_mag_ref, data_non_mag)
 
-    supercell_ref = std_test_materials[1].repeat((2,2,2))
+    mag_ref = std_test_materials[1]
+    data_mag = {"magstate": "FM"}
+    db.write(mag_ref, data_mag)
+
+    supercell_ref = std_test_materials[1].repeat((2, 2, 2))
     db.write(supercell_ref, data_non_mag)
 
-    return db 
+    return db
 
 
 @pytest.mark.ci
@@ -42,20 +34,14 @@ def test_duplicates(duplicates_test_material, duplicates_test_db, asr_tmpdir):
     from asr.duplicates import main
     from asr.structureinfo import main as main_struct_info
     from ase.io import write
-    from asr.core import read_json 
-    
-    
-    
+
     write("structure.json", duplicates_test_material)
     main_struct_info()
-   
-   
+
     results = main()
-    
 
     assert results["duplicate"]
     assert results["duplicate_IDs"] == [1, 3]
-
 
 
 @pytest.mark.ci
@@ -63,19 +49,15 @@ def test_duplicates_fm(duplicates_test_material, duplicates_test_db, asr_tmpdir)
     from asr.duplicates import main
     from asr.structureinfo import main as main_struct_info
     from ase.io import write
-    from asr.core import read_json, write_json 
-    
-    
+    from asr.core import write_json
+
     write("structure.json", duplicates_test_material)
     data = main_struct_info()
-    
+
     data["magstate"] = "FM"
-    write_json("results-asr.structureinfo.json", data) 
-    
-    
+    write_json("results-asr.structureinfo.json", data)
+
     results = main()
-    
 
     assert results["duplicate"]
     assert results["duplicate_IDs"] == [2]
-
