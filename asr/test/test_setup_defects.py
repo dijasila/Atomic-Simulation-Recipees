d3da1d62a2888cf9f93109327d688346d794fdcc
fBert31 <fafb@dtu.dk>
2020-04-30 08:31:24 +0200

    add test to setup.defects

--- asr/test/test_setup_defects.py
+++ asr/test/test_setup_defects.py
@@ -1,7 +1,9 @@
 import pytest
+from pytest import approx
 
 @pytest.mark.ci
-def test_setup_defects_specify_supercell(asr_tmpdir):
+def test_setup_defects_supercell():
+    #asr_tmpdir
     from .materials import std_test_materials
     from asr.setup.defects import main
     from ase.io import write, read
@@ -14,3 +16,23 @@ def test_setup_defects_specify_supercell(asr_tmpdir):
         assert atoms.get_scaled_positions()[i][0] == pristine.get_scaled_positions()[i][0]
         assert atoms.get_scaled_positions()[i][1] == pristine.get_scaled_positions()[i][1]
         assert atoms.get_scaled_positions()[i][2] == pristine.get_scaled_positions()[i][2]
+    for i in range(len(atoms.get_cell())):
+        assert atoms.get_cell()[i][0] == pristine.get_cell()[i][0]
+        assert atoms.get_cell()[i][1] == pristine.get_cell()[i][1]
+        assert atoms.get_cell()[i][2] == pristine.get_cell()[i][2]
+    for i in range(len(atoms.numbers)):
+        assert atoms.numbers[i] == pristine.numbers[i]
+
+    defect_ref = atoms.copy()
+    defect_ref.pop(0)
+    defect_rec = read('defects.BN_331.v_B/charge_0/unrelaxed.json')
+    for i in range(len(defect_ref.get_scaled_positions())):
+        assert defect_ref.get_scaled_positions()[i][0] == approx(defect_rec.get_scaled_positions()[i][0], abs=1e-2)
+        assert defect_ref.get_scaled_positions()[i][1] == approx(defect_rec.get_scaled_positions()[i][1], abs=1e-2)
+        assert defect_ref.get_scaled_positions()[i][2] == approx(defect_rec.get_scaled_positions()[i][2], abs=1e-2)
+    for i in range(len(defect_ref.get_cell())):
+        assert defect_ref.get_cell()[i][0] == approx(defect_rec.get_cell()[i][0], abs=1e-2)
+        assert defect_ref.get_cell()[i][1] == approx(defect_rec.get_cell()[i][1], abs=1e-2)
+        assert defect_ref.get_cell()[i][2] == approx(defect_rec.get_cell()[i][2], abs=1e-2)
+    for i in range(len(defect_ref.numbers)):
+        assert defect_ref.numbers[i] == defect_rec.numbers[i]
