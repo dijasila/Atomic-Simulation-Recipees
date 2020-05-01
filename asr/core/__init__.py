68308dc00ea830c13f8aa9ee5704a4aa61e34eee
Morten Gjerding <mortengjerding@gmail.com>
2020-05-01 15:13:44 +0200

    Some more fixed imports

--- asr/core/__init__.py
+++ asr/core/__init__.py
@@ -1,3 +1,5 @@
 from .utils import (read_json, write_json, parse_dict_string,  # noqa
-                    singleprec_dict, md5sum, file_barrier, unlink)  # noqa
-from .command import command, option, argument  # noqa
+                    singleprec_dict, md5sum, file_barrier, unlink,
+                    magnetic_atoms, chdir, encode_json, recursive_update,
+                    is_magnetic, get_dimensionality)  # noqa
+from .command import command, option, argument, get_recipes, ASRCommand  # noqa
