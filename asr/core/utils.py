0d06e516a9300026b4f9389e1d18dcd807592c00
Morten Gjerding <mortengjerding@gmail.com>
2020-05-01 14:21:56 +0200

    Refactor core/init into separate modules

---
+++ asr/core/utils.py
@@ -0,0 +1,176 @@
+import os
+import time
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Union, List
+
+import numpy as np
+from ase.io import jsonio
+import ase.parallel as parallel
+
+
+def recursive_update(dct, defaultdct):
+    if None in dct:
+        # This marks that we take default values from defaultdct
+        del dct[None]
+        for key in defaultdct:
+            if key not in dct:
+                dct[key] = defaultdct[key]
+
+    for key, value in dct.items():
+        if isinstance(value, dict) and None in value:
+            if key not in defaultdct:
+                del value[None]
+                continue
+            if not isinstance(defaultdct[key], dict):
+                del value[None]
+                continue
+            recursive_update(dct[key], defaultdct[key])
+
+
+def md5sum(filename):
+    from hashlib import md5
+    hash = md5()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+
+@contextmanager
+def chdir(folder, create=False, empty=False):
+    dir = os.getcwd()
+    if empty and folder.is_dir():
+        import shutil
+        shutil.rmtree(str(folder))
+    if create and not folder.is_dir():
+        os.mkdir(folder)
+    os.chdir(str(folder))
+    yield
+    os.chdir(dir)
+
+
+def is_magnetic():
+    import numpy as np
+    from ase.io import read
+    atoms = read('structure.json')
+    magmom_a = atoms.get_initial_magnetic_moments()
+    maxmom = np.max(np.abs(magmom_a))
+    if maxmom > 1e-3:
+        return True
+    else:
+        return False
+
+
+def get_dimensionality():
+    from ase.io import read
+    atoms = read('structure.json')
+    nd = int(np.sum(atoms.get_pbc()))
+    return nd
+
+
+mag_elements = {'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+                'Y', 'Zr', 'Nb', 'Mo', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In',
+                'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl'}
+
+
+def magnetic_atoms(atoms):
+    return np.array([symbol in mag_elements
+                     for symbol in atoms.get_chemical_symbols()],
+                    dtype=bool)
+
+
+def encode_json(data):
+    from ase.io.jsonio import MyEncoder
+    return MyEncoder(indent=1).encode(data)
+
+
+def write_json(filename, data):
+    from pathlib import Path
+    from ase.parallel import world
+
+    with file_barrier([filename]):
+        if world.rank == 0:
+            Path(filename).write_text(encode_json(data))
+
+
+def read_json(filename):
+    from pathlib import Path
+    dct = jsonio.decode(Path(filename).read_text())
+    return dct
+
+
+def unlink(path: Union[str, Path], world=None):
+    """Safely unlink path (delete file or symbolic link)."""
+    if isinstance(path, str):
+        path = Path(path)
+    if world is None:
+        world = parallel.world
+
+    world.barrier()
+    # Remove file:
+    if world.rank == 0:
+        try:
+            path.unlink()
+        except FileNotFoundError:
+            pass
+    else:
+        while path.is_file():
+            time.sleep(1.0)
+    world.barrier()
+
+
+@contextmanager
+def file_barrier(paths: List[Union[str, Path]], world=None,
+                 delete=True):
+    """Context manager for writing a file.
+
+    After the with-block all cores will be able to read the file.
+
+    Do "with file_barrier(['something.txt']):"
+
+    This will remove the file, write the file and wait for the file.
+    """
+    if world is None:
+        world = parallel.world
+
+    for i, path in enumerate(paths):
+        if isinstance(path, str):
+            path = Path(path)
+            paths[i] = path
+        # Remove file:
+        if delete:
+            unlink(path, world)
+
+    yield
+
+    # Wait for file:
+    i = 0
+    world.barrier()
+    while not all([path.is_file() for path in paths]):
+        filenames = ', '.join([path.name for path in paths
+                               if not path.is_file()])
+        if i > 0:
+            print(f'Waiting for ~{i}sec on existence of {filenames}'
+                  ' on all ranks')
+        time.sleep(1.0)
+        i += 1
+    world.barrier()
+
+
+def singleprec_dict(dct):
+    assert isinstance(dct, dict), f'Input {dct} is not dict.'
+
+    for key, value in dct.items():
+        if isinstance(value, np.ndarray):
+            if value.dtype == np.int64:
+                value = value.astype(np.int32)
+            elif value.dtype == np.float64:
+                value = value.astype(np.float32)
+            elif value.dtype == np.complex128:
+                value = value.astype(np.complex64)
+            dct[key] = value
+        elif isinstance(value, dict):
+            dct[key] = singleprec_dict(value)
+
+    return dct
