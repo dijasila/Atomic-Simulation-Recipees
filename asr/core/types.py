9a9d804d70964ce2896177bfbfc033904d233ed8
Morten Gjerding <mortengjerding@gmail.com>
2020-06-08 08:39:21 +0200

    Fix AtomsFile type to be more flexible in error handling

--- asr/core/types.py
+++ asr/core/types.py
@@ -1,16 +1,36 @@
 """Implement custom types for ASR."""
 import click
 from ase.io import read
+from ase.io.formats import UnknownFileTypeError
 
 
-class AtomsFileParamType(click.ParamType):
+class AtomsFile(click.ParamType):
     """Read atoms object from filename and return Atoms object."""
 
     name = "atomsfile"
 
+    def __init__(self, must_exist=True, *args, **kwargs):
+        """Initialize AtomsFile object.
+
+        Parameters
+        ----------
+        must_exist : bool
+            If False, errors relating to empty or missing files will be
+            ignored and the returned atoms will be None in that case. If True,
+            all errors will be raised if encountered.
+
+        Returns
+        -------
+        atoms : `ase.Atoms`
+        """
+        self.must_exist = must_exist
+        super().__init__(*args, **kwargs)
+
     def convert(self, value, param, ctx):
         """Convert string to atoms object."""
-        return read(value)
-
-
-AtomsFile = AtomsFileParamType()
+        try:
+            return read(value, parallel=False)
+        except (IOError, UnknownFileTypeError, StopIteration):
+            if self.must_exist:
+                raise
+            return None
