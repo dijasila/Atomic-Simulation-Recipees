693c9a075351fdcb825419d7d427414fffadd6d0
Morten Gjerding <mortengjerding@gmail.com>
2020-06-11 18:59:41 +0200

    Fix ASE BandStructure DeprecationWarning

--- asr/bandstructure.py
+++ asr/bandstructure.py
@@ -322,7 +322,7 @@ def bs_pbe(row,
     import matplotlib.pyplot as plt
     import matplotlib.patheffects as path_effects
     import numpy as np
-    from ase.dft.band_structure import BandStructure, BandStructurePlot
+    from ase.spectrum.band_structure import BandStructure, BandStructurePlot
     d = row.data.get('results-asr.bandstructure.json')
 
     path = d['bs_nosoc']['path']
