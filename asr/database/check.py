990b709055ee62bb77c39e20769e645fdf84b70a
Morten Gjerding <mogje@fysik.dtu.dk>
2020-06-23 10:10:46 +0200

    Update database.check to check for missing uids

--- asr/database/check.py
+++ asr/database/check.py
@@ -17,13 +17,17 @@ def main(dbname: str):
     print(f'Check integrity of database={dbname}.')
     with connect(dbname, serial=True) as db:
         uids = set()
+        missing_uids = set()
         duplicate_uids = set()
         child_uids = set()
         for row in db.select():
-            if row.uid in uids:
-                duplicate_uids.add(row.uid)
+            if 'uid' not in row:
+                missing_uids.add(row.id)
             else:
-                uids.add(row.uid)
+                if row.uid in uids:
+                    duplicate_uids.add(row.uid)
+                else:
+                    uids.add(row.uid)
             children = row.data.get('__children__', {})
             child_uids.update(set(children.values()))
 
@@ -31,17 +35,25 @@ def main(dbname: str):
     nmissing_childs = len(missing_child_uids)
 
     if missing_child_uids:
-        print(f'Missing {nmissing_childs} child uids in collected database. '
+        print(f'Missing {nmissing_childs} child uids in database. '
               'Child uids:')
         for uid in missing_child_uids:
             print(' ' * 4, uid)
 
     nduplicates = len(duplicate_uids)
     if duplicate_uids:
-        print(f'{nduplicates} duplicate uids in collected database. '
+        print(f'{nduplicates} duplicate uids in database. '
               'Duplicate uids:')
         for uid in duplicate_uids:
             print(' ' * 4, uid)
 
+    nmissing_uids = len(missing_uids)
+    if missing_uids:
+        print(f'{nmissing_uids} missing uids in database. '
+              "Missing row.id's:")
+        for uid in missing_uids:
+            print(' ' * 4, uid)
+
     return {'missing_child_uids': missing_child_uids,
-            'duplicate_uids': duplicate_uids}
+            'duplicate_uids': duplicate_uids,
+            'missing_uids': missing_uids}
