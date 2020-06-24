a8ce01df61e0fdc3e9e35adfd4e3831799d36b36
Morten Gjerding <mogje@fysik.dtu.dk>
2020-06-24 11:18:48 +0200

    Update dimensionality

--- asr/dimensionality.py
+++ asr/dimensionality.py
@@ -2,6 +2,15 @@ from asr.core import command, option, AtomsFile
 from ase import Atoms
 
 
+def get_dimtypes():
+    """Create a list of all dimensionality types."""
+    from itertools import product
+    s = set(product([0, 1], repeat=4))
+    s2 = sorted(s, key=lambda x: (sum(x), *[-t for t in x]))[1:]
+    string = "0123"
+    return ["".join(x for x, y in zip(string, s3) if y) + "D" for s3 in s2]
+
+
 @command('asr.dimensionality')
 @option('--atoms', type=AtomsFile(), default='structure.json')
 def main(atoms: Atoms):
@@ -16,10 +25,12 @@ def main(atoms: Atoms):
                    for interval in
                    analyze_dimensionality(atoms)]
 
+    dim_scores = {}
     # Fix for numpy.int64 in cdim which is not jsonable.
     for interval in k_intervals:
         cdim = {int(key): value for key, value in interval['cdim'].items()}
         interval['cdim'] = cdim
+        dim_scores[interval['dimtype']] = interval['score']
 
     results = {'k_intervals': k_intervals}
     primary_interval = k_intervals[0]
@@ -28,6 +39,9 @@ def main(atoms: Atoms):
 
     results['dim_primary'] = dim_primary
     results['dim_primary_score'] = dim_primary_score
+
+    for dimtype in get_dimtypes():
+        results[f'dim_score_{dimtype}'] = dim_scores.get(dimtype, 0)
     for nd in range(4):
         results[f'dim_nclusters_{nd}D'] = primary_interval['h'][nd]
 
--- asr/test/test_dimensionality.py
+++ asr/test/test_dimensionality.py
@@ -1,4 +1,5 @@
 from asr.dimensionality import main as dimensionality
+from asr.dimensionality import get_dimtypes
 
 
 def test_dimensionality(asr_tmpdir, test_material):
@@ -8,6 +9,11 @@ def test_dimensionality(asr_tmpdir, test_material):
 
     interval = results['k_intervals'][0]
     assert interval['dimtype'] == f'{nd}D'
+    primary = results['dim_primary']
+
+    dimtypes = get_dimtypes()
+    scores = [results[f'dim_score_{dimtype}'] for dimtype in dimtypes]
+    assert results[f'dim_score_{primary}'] == max(scores)
 
 
 def test_dimensionality_cli(asr_tmpdir, test_material):
