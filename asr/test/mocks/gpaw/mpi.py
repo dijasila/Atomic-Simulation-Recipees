448210fdae7dd1969ff4a7af20f47cbaee7e3cb2
Thorbjørn Skovhus <s134264@student.dtu.dk>
2020-07-24 10:37:47 +0200

    add a mock gpaw.mpi.world.sum

--- asr/test/mocks/gpaw/mpi.py
+++ asr/test/mocks/gpaw/mpi.py
@@ -13,11 +13,19 @@ def barrier():
     pass
 
 
+def world_sum(a, **ignored):
+    if isinstance(a, (int, float, complex)):
+        return a
+    else:
+        pass
+
+
 world = SimpleNamespace(size=1,
                         rank=0,
                         broadcast=broadcast,
                         barrier=barrier,
-                        new_communicator=new_communicator)
+                        new_communicator=new_communicator,
+                        sum=world_sum)
 
 serial_comm = None
 
