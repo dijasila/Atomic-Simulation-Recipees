bf342d9ca7f33f8d2f43cf5ceb7967689c259c1b
Morten Gjerding <mogje@fysik.dtu.dk>
2020-08-31 14:18:49 +0200

    Update results webpanel

--- asr/core/results.py
+++ asr/core/results.py
@@ -1,5 +1,5 @@
 """Implements ASRResults object and related quantities."""
-from typing import get_type_hints, List
+from typing import get_type_hints, List, Any
 
 
 def get_object_descriptions(obj):
@@ -15,7 +15,7 @@ def format_key_description_pair(key: str, attr_type: type, description: str):
     return f'{key}: {attr_type}\n    {description}'
 
 
-def set_docstring(obj):
+def set_docstring(obj) -> str:
     descriptions = get_object_descriptions(obj)
     types = get_object_types(obj)
     assert set(types) == set(descriptions)
@@ -32,7 +32,22 @@ def set_docstring(obj):
 class WebPanel:
     """Web-panel for presenting results."""
 
-    pass
+    def __call__(self, results):
+        return self.webpanel(results)
+
+    def webpanel(self, results):
+        """Make basic webpanel.
+
+        Simply prints all attributes.
+        """
+        rows = []
+        for key, value in results.items():
+            rows.append([key, value])
+        table = {'type': 'table',
+                 'header': ['key', 'value'],
+                 'rows': rows}
+        columns = [[table]]
+        return columns
 
 
 class ASRResults:
@@ -47,17 +62,16 @@ class ASRResults:
     """
 
     version: int = 0
+    webpanel: WebPanel = WebPanel()
+    prev_version: Any = None
 
-    def __init__(self, dct,
-                 webpanel: WebPanel):
+    def __init__(self, dct):
         """Initialize results from dict."""
-        self._check_dct(dct)
         self._dct = dct
-        self._webpanel = webpanel
 
     def __getitem__(self, item):
         """Get item from self.dct."""
-        return self.dct[item]
+        return self._dct[item]
 
     def __contains__(self, item):
         """Determine if item in self.dct."""
@@ -65,59 +79,22 @@ class ASRResults:
 
     def __iter__(self):
         """Iterate over keys."""
-        return self.dct.__iter__()
+        return self._dct.__iter__()
 
     def __getattr__(self, key):
         """Get attribute."""
-        return self.dct[key]
+        if key in self.keys():
+            return self._dct[key]
+        return self.key
 
     def values(self):
         """Wrap self.dct.values."""
-        return self.dct.values()
+        return self._dct.values()
 
     def items(self):
         """Wrap self.dct.items."""
-        return self.dct.items()
+        return self._dct.items()
 
     def keys(self):
         """Wrap self.dct.keys."""
-        return self.dct.keys()
-
-    def webpanel(self):
-        """Get web panel."""
-        raise NotImplementedError
-
-
-class GapResults(ASRResults):
-    """Ground state results.
-
-    Attributes
-    ----------
-    gap: float
-        The band gap [eV].
-    gap_dir: float
-        The direct band gap [eV].
-    """
-
-    gap: float
-    dipz: float
-
-
-class GSResults(ASRResults):
-    """Ground state results.
-
-    Attributes
-    ----------
-    gap: float
-        The band gap in eV.
-    gaps_nosoc: GapResults
-        Collection of band gap related results.
-    """
-
-    gap: float
-    dipz: float
-    gaps_nosoc: GapResults
-
-
-print(GSResults)
-print(GSResults.__dict__)
+        return self._dct.keys()
