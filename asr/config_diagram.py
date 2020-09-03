432ee652ad77a126ca47e97c4cd5388ec42235f9
Simone Manti <smanti@fysik.dtu.dk>
2020-09-03 11:30:56 +0200

    Removed setup cc-diagram

--- asr/config_diagram.py
+++ asr/config_diagram.py
@@ -1,4 +1,5 @@
-from asr.core import command, read_json, write_json
+from asr.core import command, option, read_json
+import numpy as np
 
 
 def webpanel(row, key_descriptions):
@@ -9,33 +10,53 @@ def webpanel(row, key_descriptions):
 
 
 @command('asr.config_diagram',
-         dependencies=['asr.setup.cc-diagram'],
          webpanel=webpanel)
-def calculate():
+@option('--state', help='Which confguration is displaced.', type=str)
+@option('--npoints', help='How many displacement points.', type=int)
+def calculate(state: str = 'ground', npoints: int = 5):
     """Calculate the energies of the displaced structures
        along the one-dimensional mode"""
 
     from gpaw import GPAW
     from ase.io import read
 
-    setup = read_json('results-asr.setup.cc-diagram.json')
-    folders = setup['folders']
-    state = setup['__params__']['state']
 
-    calc = GPAW('gs.gpw', txt=None)
     if state == 'excited':
-        calc = GPAW('ex.gpw', txt=None)
+        atoms_1 = read('structure.json')
+        atoms_2 = read('../structure.json')
+    else:
+        atoms_1 = read('structure.json')
+        atoms_2 = read('excited/structure.json')
+    
+    calc = GPAW('gs.gpw', txt=None)
 
-    for folder in folders:
-        params = read_json(folder + '/params.json')
-        Q = params['Q']
+    displ_n = np.linspace(-1.0, 1.0, npoints, endpoint=True)
+    m_a = atoms_1.get_masses()
+    pos_ai = atoms_1.positions.copy()
 
-        atoms = read(folder + '/structure.json')
-        atoms.set_calculator(calc)
-        # atoms.get_potential_energy()
+    delta_R = atoms_2.positions - atoms_1.positions
+    delta_Q = ((delta_R**2).sum(axis=-1) * m_a).sum()
+
+    Q_n = []
+    energies_n = []
+
+    for displ in displ_n:
+        Q = (((displ * delta_R)**2).sum(axis=-1) * m_a).sum()
+        Q_n.append(Q)
+
+        atoms_1.positions += displ * delta_R
+        atoms_1.positions = pos_ai
+        atoms_1.set_calculator(calc)
+        # atoms_1.get_potential_energy()
+
         energy = 0.06155**2 / 2 * 15.4669**2 * Q**2
-        params['energy'] = energy
-        write_json(folder + '/params.json', params)
+        energies_n.append(energy)
+
+    results = {'delta_Q': delta_Q,
+               'Q_n': Q_n,
+               'energies_n': energies_n}
+
+    return results
 
 
 @command("asr.config_diagram",
@@ -47,21 +68,11 @@ def main():
        Huang-Rhys factors"""
     import ase.units as units
     from math import sqrt
-    import numpy as np
 
-    setup = read_json('results-asr.setup.cc-diagram.json')
-    folders = setup['folders']
-    delta_Q = setup['delta_Q']
-
-    energies_n = []
-    Q_n = []
-
-    for folder in folders:
-        params = read_json(folder + '/params.json')
-        Q = params['Q']
-        energy = params['energy']
-        Q_n.append(Q)
-        energies_n.append(energy)
+    data = read_json('results-asr.config_diagram@calculate.json')
+    delta_Q = data['delta_Q']
+    energies_n = data['energies_n']
+    Q_n = data['Q_n']
 
     # Rescale energy by the minimum value
     energies_n = np.array(energies_n)
@@ -79,9 +90,7 @@ def main():
     # Estrapolation of the Huang-Rhys factor
     S = s**2 * delta_Q**2 * omega / 2
 
-    results = {'Q_n': Q_n,
-               'energies_n': energies_n,
-               'omega': omega,
+    results = {'omega': omega,
                'S': S}
 
     return results
