6a5335102395a7a2c3c2f78c2b9e7387ad98e3b2
fBert31 <fafb@dtu.dk>
2020-09-09 11:33:53 +0200

    included number of bands for params.json and updated todo list

--- asr/setup/excited.py
+++ asr/setup/excited.py
@@ -16,6 +16,13 @@ from gpaw.occupations import FixedOccupations
 from gpaw import restart
 from gpaw.response.pair import PairDensity
 
+# TODO:
+# - clean up imports
+# - restructure commands, options and default arguments
+# - possible, include spin option
+# - implement check for already existing folders
+# - write a test
+
 @command('asr.setup.excited')
 @option('--n', help='from_state', type=str)
 @option('--m', help='to_state', type=str)
@@ -37,22 +44,25 @@ def main(n: int = 1, m: int =  1):
     Pair=PairDensity(calc)
     n1=Pair.nocc2
     n2=Pair.nocc1
-    print(n1, n2, n3, n, m)
 
-    #spin0
+    #spin channel 1
     occ_n_alpha = np.hstack((np.ones(n1-n),np.zeros(m),np.ones(1),np.zeros(n3-(n1-n)-m-1)))
     occ_n_beta = np.hstack((np.ones(n2),np.zeros(n3-n2)))
     params = read_json('params.json')
     p_spin0 = params.copy()
     p_spin0['asr.gs@calculate']['calculator']['occupations'] = {'name': 'fixed', 'numbers': [occ_n_alpha, occ_n_beta]}
+    p_spin0['asr.gs@calculate']['calculator']['nbands'] = n3
     p_spin0['asr.relax']['calculator']['occupations'] = {'name': 'fixed', 'numbers': [occ_n_alpha, occ_n_beta]}
+    p_spin0['asr.relax']['calculator']['nbands'] = n3
     write_json('excited_spin0/params.json', p_spin0)
-    #spin1
+    #spin channel 2
     occ_n_alpha = np.hstack((np.ones(n1),np.zeros(n3-n1)))
     occ_n_beta = np.hstack((np.ones(n2-n),np.zeros(m),np.ones(1),np.zeros(n3-(n2-n)-m-1))) 
     p_spin1 = params.copy()
     p_spin1['asr.gs@calculate']['calculator']['occupations'] = {'name': 'fixed', 'numbers': [occ_n_alpha, occ_n_beta]}
+    p_spin1['asr.gs@calculate']['calculator']['nbands'] = n3
     p_spin1['asr.relax']['calculator']['occupations'] = {'name': 'fixed', 'numbers': [occ_n_alpha, occ_n_beta]}
+    p_spin1['asr.relax']['calculator']['nbands'] = n3
     write_json('excited_spin1/params.json', p_spin1)
 
     return None
