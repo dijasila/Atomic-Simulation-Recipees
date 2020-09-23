89ffad81eaddf9c6345fa3c85e5fe3d71218c442
fBert31 <fafb@dtu.dk>
2020-09-23 11:04:12 +0200

    test for fixing bug with myqueue

--- asr/wavefunction.py
+++ asr/wavefunction.py
@@ -6,56 +6,60 @@ from gpaw import GPAW, restart
 @command(module='asr.wavefunction',
          requires=['gs.gpw', 'structure.json'],
          resources='12:1h')
-@option('--spin', help='Specify which spin channel you want to consider. '
-        'Choose 0 for the first spin channel, 1 for the second spin channel, '
-        'and 2 if both should be considered.', type=int)
-@option('--state', help='Specify the specific state (band number) that you '
-        'want to consider. Note, that this argument is not used when the '
-        'gap state flag is active.', type=int)
-@option('--get-gapstates/--dont-get-gapstates', help='Should all of the gap'
-        ' states be saved and analyzed? Note, that if gap states are analysed'
-        ' the --state option will be neglected.', is_flag=True)
-@option('--analyze/--dont-analyze', help='Not only create cube files of '
-        'specific states, but also analyze them.', is_flag=True)
-def main(spin: int = 2,
-         state: int = 0,
-         get_gapstates: bool = False,
-         analyze: bool = False):
+# @option('--spin', help='Specify which spin channel you want to consider. '
+#         'Choose 0 for the first spin channel, 1 for the second spin channel, '
+#         'and 2 if both should be considered.', type=int)
+# @option('--state', help='Specify the specific state (band number) that you '
+#         'want to consider. Note, that this argument is not used when the '
+#         'gap state flag is active.', type=int)
+# @option('--get-gapstates/--dont-get-gapstates', help='Should all of the gap'
+#         ' states be saved and analyzed? Note, that if gap states are analysed'
+#         ' the --state option will be neglected.', is_flag=True)
+# @option('--analyze/--dont-analyze', help='Not only create cube files of '
+#         'specific states, but also analyze them.', is_flag=True)
+#def main(spin: int = 2,
+#         state: int = 0,
+#         get_gapstates: bool = False,
+#         analyze: bool = False):
+def main():
     """Write out wavefunction and analyze it.
 
     This recipe reads in an existing gs.gpw file and writes out wavefunctions
     of different states (either the one of a specific given bandindex or of
     all the defect states in the gap). Furthermore, it will feature some post
     analysis on those states.
+
+    Test.
     """
-    atoms = read('structure.json')
-    print('INFO: run fixdensity calculation')
-    calc = GPAW('gs.gpw', txt='analyze_states.txt', fixdensity=True)
-    calc.get_potential_energy()
-    if get_gapstates:
-        if spin == 0 or spin == 2:
-            print('INFO: evaluate gapstates for first spin channel ...')
-            states_0 = return_gapstates_fix(calc, spin=0)
-        if spin == 1 or spin == 2:
-            print('INFO: evaluate gapstates for second spin channel ...')
-            states_1 = return_gapstates_fix(calc, spin=1)
-    elif not get_gapstates:
-        states_0 = [state]
-        states_1 = [state]
-
-    print('INFO: write wavefunctions of gapstates ...')
-    for band in states_0:
-        wf = calc.get_pseudo_wave_function(band=band, spin=0)
-        fname = 'wf.{0}_{1}.cube'.format(band, 0)
-        write(fname, atoms, data=wf)
-    for band in states_1:
-        wf = calc.get_pseudo_wave_function(band=band, spin=1)
-        fname = 'wf.{0}_{1}.cube'.format(band, 1)
-        write(fname, atoms, data=wf)
-
-    if analyze:
-        # To be implemented
-        print('INFO: analyze chosen states.')
+#     atoms = read('structure.json')
+#     print('INFO: run fixdensity calculation')
+#     calc = GPAW('gs.gpw', txt='analyze_states.txt', fixdensity=True)
+#     calc.get_potential_energy()
+#     if get_gapstates:
+#         if spin == 0 or spin == 2:
+#             print('INFO: evaluate gapstates for first spin channel ...')
+#             states_0 = return_gapstates_fix(calc, spin=0)
+#         if spin == 1 or spin == 2:
+#             print('INFO: evaluate gapstates for second spin channel ...')
+#             states_1 = return_gapstates_fix(calc, spin=1)
+#     elif not get_gapstates:
+#         states_0 = [state]
+#         states_1 = [state]
+# 
+#     print('INFO: write wavefunctions of gapstates ...')
+#     for band in states_0:
+#         wf = calc.get_pseudo_wave_function(band=band, spin=0)
+#         fname = 'wf.{0}_{1}.cube'.format(band, 0)
+#         write(fname, atoms, data=wf)
+#     for band in states_1:
+#         wf = calc.get_pseudo_wave_function(band=band, spin=1)
+#         fname = 'wf.{0}_{1}.cube'.format(band, 1)
+#         write(fname, atoms, data=wf)
+# 
+#     if analyze:
+#         # To be implemented
+#         print('INFO: analyze chosen states.')
+    print('TEST')
 
     return None
 
