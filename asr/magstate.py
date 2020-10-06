20c8ad797029e02ce2902669b8dd75ec5d69a70b
Morten Gjerding <mogje@fysik.dtu.dk>
2020-10-06 14:35:13 +0200

    Also save the total magnetic moment

--- asr/magstate.py
+++ asr/magstate.py
@@ -42,10 +42,12 @@ def main():
     calc = GPAW('gs.gpw', txt=None)
     magstate = get_magstate(calc)
     magmoms = calc.get_property('magmoms', allow_calculation=False)
+    magmom = calc.get_property('magmom', allow_calculation=False)
     nspins = calc.get_number_of_spins()
     results = {'magstate': magstate.upper(),
                'is_magnetic': magstate != 'nm',
                'magmoms': magmoms,
+               'magmom': magmom,
                'nspins': nspins}
 
     return results
