5244b426e15b9213334b9f8562356f10c864cf04
fBert31 <fafb@dtu.dk>
2020-10-14 11:32:42 +0200

    add relaxation correction

--- asr/sj_analyze.py
+++ asr/sj_analyze.py
@@ -1,5 +1,6 @@
 from asr.core import command, option
 from pathlib import Path
+from ase.io import Trajectory
 
 
 @command(module='asr.sj_analyze',
@@ -20,6 +21,36 @@ def main():
     results = {}
 
     return results
+
+
+def get_transition_level(transition, correct_relax):
+    """
+    Calculates the charge transition level for a given charge transition.
+
+    :param transition: (List), transition (e.g. [0,-1])
+    :param correct_relax: (Boolean), True if transition energy will be corrected
+    """
+    # if possible, calculate correction due to relaxation in the charge state
+    if correct_relax:
+        traj = Trajectory('../charge_{}/relax.traj'.format(str(int(transition[1]))))
+        e_cor = traj[0].get_potential_energy() - traj[-1].get_potential_energy()
+    else:
+        e_cor = 0
+
+    if transition[0] > transition[1]:
+        calc = restart('sj_-0.5/gs.gpw', txt=None)
+    elif transition[1] > transition[0]:
+        calc = restart('sj_+0.5/gs.gpw', txt=None)
+
+    # extract HOMO or LUMO
+    # TODO: TBD
+
+    # reference to vaccum level
+    # TODO: TBD
+
+    # subtract relaxation correction
+
+    # return e_trans, e_cor, e_ref
 
 
 if __name__ == '__main__':
