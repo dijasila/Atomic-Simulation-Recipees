f8516cce30866da67bfed9b4bd5f74ad4b256ce7
fBert31 <fafb@dtu.dk>
2020-10-16 15:25:09 +0200

    do uid comparison for two databases

--- asr/database/crosslinks.py
+++ asr/database/crosslinks.py
@@ -1,5 +1,6 @@
 from typing import Union
 from asr.core import command, option, argument
+from ase.db import connect
 
 
 @command('asr.database.crosslinks')
@@ -8,5 +9,18 @@ def main(databases: Union[str, None] = None):
     """
     Create links between entries in given ASE databases.
     """
+    dblist = []
+    for element in databases:
+        db = connect(element)
+        dblist.append(db)
+
+    for i, row in enumerate(dblist[0].select()):
+        for j, row2 in enumerate(dblist[1].select()):
+            if row.link_uid == row2.link_uid:
+                id = j
+                # dblist[0].update(j, ...)
+                name = dblist[0].metadata['internal_links']['link_name']
+                print(name)
+
 
     return None
