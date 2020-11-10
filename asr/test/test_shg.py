bc88602d379a44d2cbfd66e515a0d0a1420bd0c3
Alireza Taghizadeh <ata@nano.aau.dk>
2020-11-10 13:04:41 +0100

    Style fix

--- asr/test/test_shg.py
+++ asr/test/test_shg.py
@@ -28,7 +28,7 @@ def test_shg(asr_tmpdir_w_params, test_material, mockgpaw, mocker, get_webconten
     for pol in sorted(sym_chi.keys()):
         if pol == 'zero':
             continue
-        chi_dict[pol] = np.random.rand(len(w_ls)) +  1j * np.random.random(len(w_ls))
+        chi_dict[pol] = np.random.rand(len(w_ls)) + 1j * np.random.random(len(w_ls))
     result = ASRResult(
         data={
             'chi': chi_dict,
@@ -43,28 +43,24 @@ def test_shg(asr_tmpdir_w_params, test_material, mockgpaw, mocker, get_webconten
     if len(sym_chi) != 1:
         chi_vvvl = make_full_chi(sym_chi, chi_dict)
         assert chi_vvvl.shape == (3, 3, 3, len(w_ls))
-        # calc_polarized_shg(sym_chi, chi_dict)
+        calc_polarized_shg(sym_chi, chi_dict)
 
     def get_shg(
-        freqs=w_ls,
-        eta=0.05,
-        pol='yyy',
-        eshift=0.0,
-        gauge='lg',
-        ftol=1e-4, Etol=1e-6,
-        band_n=None,
-        out_name='shg.npy',
-        mml_name='mml.npz'
-        ):
+            freqs=w_ls,
+            eta=0.05,
+            pol='yyy',
+            eshift=0.0,
+            gauge='lg',
+            ftol=1e-4, Etol=1e-6,
+            band_n=None,
+            out_name='shg.npy',
+            mml_name='mml.npz'):
 
-        chi = np.random.rand(len(freqs)) +  1j * np.random.random(len(freqs))
+        chi = np.random.rand(len(freqs)) + 1j * np.random.random(len(freqs))
         return np.vstack((freqs, chi))
 
     def make_nlodata(
-        gs_name = 'gs.gpw',
-        out_name = 'mml.npz',
-        ni = 0,
-        nf = 0):
+            gs_name='gs.gpw', out_name='mml.npz', ni=0, nf=0):
 
         pass
 
