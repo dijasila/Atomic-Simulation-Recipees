75bd3356aad16c7494eb084fc6b97e3da7fdb1fc
Morten Gjerding <mortengjerding@gmail.com>
2020-11-11 15:21:02 +0100

    Add count atoms and test of count atoms

--- asr/test/test_core_material.py
+++ asr/test/test_core_material.py
@@ -14,3 +14,16 @@ def test_asr_cli_results_figures_gs():
 
     keys = [key for key in material]
     assert "gap" in keys
+
+
+@pytest.mark.ci
+def test_material_count_atoms_equals_len_atoms(test_material):
+    from asr.core.material import Material
+    kvp = {}
+    data = {}
+
+    material = Material(test_material, kvp, data)
+    count = {}
+    for symbol in test_material.symbols:
+        count[symbol] = count.get(symbol, 0) + 1
+    assert material.count_atoms() == count
