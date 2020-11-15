c2d733cbb4c572e51185c5a2e7bd83ebce2d114a
Morten Gjerding <mortengjerding@gmail.com>
2020-11-15 09:43:04 +0100

    Improve test

--- asr/test/test_core_cache.py
+++ asr/test/test_core_cache.py
@@ -15,3 +15,15 @@ def test_cache(asr_tmpdir):
     cache.add(run_record)
     assert cache.has(run_spec)
     assert cache.get(run_spec) == run_record
+    run_spec = construct_run_spec(
+        name='asr.test',
+        parameters={'a': 2},
+        version=0,
+    )
+    run_record = RunRecord(run_specification=run_spec,
+                           result={'b': 1})
+
+    assert not cache.has(run_spec)
+    cache.add(run_record)
+    assert cache.has(run_spec)
+    assert cache.get(run_spec) == run_record
