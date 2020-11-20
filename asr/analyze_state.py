d0fe4a5c488643b5ce6119292545b3207e1e6f8d
fBert31 <fafb@dtu.dk>
2020-11-20 14:06:52 +0100

    start including hf coupling into asr.analyze_state

--- asr/analyze_state.py
+++ asr/analyze_state.py
@@ -431,8 +431,10 @@ class Result(ASRResult):
          returns=Result)
 @option('--mapping/--no-mapping', is_flag=True)
 @option('--radius', type=float)
+@option('--hf/--no-hf', is_flag=True)
 def main(mapping: bool = False,
-         radius: float = 2.0) -> Result:
+         radius: float = 2.0,
+         hf: bool = True) -> Result:
     """Analyze wavefunctions and analyze symmetry."""
 
     from ase.io.cube import read_cube_data
@@ -502,6 +504,11 @@ def main(mapping: bool = False,
                                                  spin)
         symmetry_results.append(symmetry_result)
 
+    if hf:
+        print('INFO: calculate hyperfine properties.')
+        atoms, calc = restart('gs.gpw', txt=None)
+        hr_results = get_hyperfine_results(atoms, calc)
+
     return Result.fromdata(
         pointgroup=point_group,
         defect_center=center,
@@ -526,6 +533,51 @@ def return_irrep_result(sym_name, sym_score) -> IrrepResult:
     return IrrepResult.fromdata(
         sym_name=sym_name,
         sym_score=sym_score)
+
+
+def get_hyperfine_results(atoms, calc):
+    "Calculate hyperfine splitting."
+    from math import pi
+    import numpy as np
+    import ase.units as units
+    from gpaw.hyperfine import hyperfine_parameters,gyromagnetic_ratios
+
+    symbols = atoms.symbols
+    magmoms = atoms.get_magnetic_moments()
+    total_magmom = atoms.get_magnetic_moment()
+    assert total_magmom != 0.0
+
+    g_factors = {symbol: ratio * 1e6 * 4 * pi * units._mp / units._e
+             for symbol, (n, ratio) in gyromagnetic_ratios.items()}
+
+    units == 'MHz'
+    scale = units._e / units._hplanck * 1e-6
+    unit = 'MHz'
+    A_avv = hyperfine_parameters(calc)
+    print('Hyperfine coupling paramters '
+        f'in {unit}:\n')
+    columns = ['1.', '2.', '3.']
+
+    print('  atom  magmom      ', '       '.join(columns))
+
+    used = {}
+    for a, A_vv in enumerate(A_avv):
+        symbol = symbols[a]
+        magmom = magmoms[a]
+        g_factor = g_factors.get(symbol, 1.0)
+        used[symbol] = g_factor
+        A_vv *= g_factor / total_magmom * scale
+        numbers = np.linalg.eigvalsh(A_vv)
+
+        print(f'{a:3} {symbol:>2}  {magmom:6.3f}',
+          ''.join(f'{x:9.2f}' for x in numbers))
+
+    print('\nCore correction included')
+    print(f'Total magnetic moment: {total_magmom:.3f}')
+
+    print('\nG-factors used:')
+    for symbol, g in used.items():
+        print(f'{symbol:2} {g:10.3f}')
 
 
 def get_localization_ratio(atoms, wf):
