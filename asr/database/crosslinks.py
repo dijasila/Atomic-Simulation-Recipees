b47232eb6a8307331e584495f4fb6230e382def0
fBert31 <fafb@dtu.dk>
2020-12-03 13:53:35 +0100

    get crosslinks ready for merge request

--- asr/database/crosslinks.py
+++ asr/database/crosslinks.py
@@ -18,19 +18,28 @@ class Result(ASRResult):
     )
 
 
-@command('asr.database.crosslinks')
-@option('--databaselink', type=str)
-@argument('databases', nargs=-1, type=str)
+@command('asr.database.crosslinks',
+         returns=Result)
+@option('--databaselink', type=str,
+        help='DB that links get created for.')
+@argument('databases', nargs=-1, type=str,
+          help='DBs that target DB should link to.')
 def create(databaselink: str,
            databases: Union[str, None] = None) -> Result:
-    """Create links between entries in given ASE databases."""
-    # connect to the link database and create dictionary of databases to
-    # link to
+    """Create links between entries in given ASE databases.
+
+    """
+    # connect to target and linking DBs
     link_db = connect(databaselink)
-    db_connections = {}  # [link_db]
+    db_connections = {}
     for dbfilenames in databases:
         db = connect(dbfilenames)
         db_connections[dbfilenames] = db
+
+    # make sure all DBs possess the correct metadata structure
+    check_metadata(link_db, db_name, target=True)
+    for dbfilename, dbconnection in db_connections.items():
+        check_metadata(dbconnection, dbfilename, target=False)
 
     # create dictionary which maps all uids to a row for a given database
     # save the uids_to_row dictionary for every database
@@ -73,78 +82,6 @@ def create(databaselink: str,
     return Result.fromdata(
         link_db=databaselink,
         connection_dbs=databases)
-
-
-# @prepare_result
-# class Result(ASRResult):
-#     """Container for database crosslinks results."""
-# 
-#     linked_database: str
-#     links: typing.List[LinkResults]
-# 
-#     key_descriptions = dict(
-#         linked_database='Database that crosslinks got created for.',
-#         links='List of LinkResults containers.')
-# 
- 
-# def return_link_results(linklist, urllist, db_name) -> LinkResults:
-#     return LinkResults.fromdata(
-#         name=db_name,
-#         link_names=linklist,
-#         link_urls=urllist)
-# 
-# 
-# @command(module='asr.database.crosslinks',
-#          dependencies=['asr.database.material_fingerprint',
-#                        'asr.database.crosslinks@create'],
-#          returns=Result)
-# @argument('database', nargs=1, type=str)
-# def main(database: str) -> Result:
-#     """Create links.
-# 
-#     Use created crosslink names and urls from asr.database.crosslinks@create
-#     and write HTML code for representation on webpage.
-#     """
-#     # First, get uid of structure in current directory to compare to respective
-#     # uid in the database
-#     results_fingerprint = read_json('results-asr.database.material_fingerprint.json')
-#     structure_uid = results_fingerprint['uid']
-# 
-#     # Second, connect to the crosslinked database and obtain the names, urls,
-#     # and types
-#     db = connect(database)
-#     for row in db.select():
-#         if row.uid == structure_uid:
-#             links = row.data['links']
-# 
-#     link_results_list = []
-#     for element in links:
-#         link_results = return_link_results(links[element]['link_names'],
-#                                            links[element]['link_urls'],
-#                                            element)
-#         link_results_list.append(link_results)
-# 
-#     return Result.fromdata(linked_database=database,
-#                            links=link_results_list)
-# 
-# 
-# def link_tables(row: AtomsRow) -> List[Dict[str, Any]]:
-#     import numpy as np
-# 
-#     data = row.data.get('results-asr.database.crosslinks.json')
-#     links = data['links']
-#     names = []
-#     urls = []
-#     types = []
-#     for element in links:
-#         for j in range(len(links[element]['link_names'])):
-#             names.append(links[element]['link_names'][j])
-#             urls.append(links[element]['link_urls'][j])
-#             types.append(element)
-# 
-#     table_array = np.array([names, urls, types])
-# 
-#     return table_array
 
 
 if __name__ == '__main__':
