ad7a7106c20fdb07fd142006b16fcd0795b1d489
fBert31 <fafb@dtu.dk>
2020-12-07 17:12:35 +0100

    introduce new type of comma-separated string

--- asr/core/__init__.py
+++ asr/core/__init__.py
@@ -3,7 +3,7 @@ from .utils import (read_json, write_json, parse_dict_string,  # noqa
                     singleprec_dict, md5sum, file_barrier, unlink,
                     chdir, encode_json, recursive_update, write_file,  # noqa
                     get_recipe_from_name, dct_to_object, read_file, decode_json)  # noqa
-from .types import AtomsFile, DictStr, clickify_docstring  # noqa
+from .types import AtomsFile, DictStr, clickify_docstring, CommaStr  # noqa
 from .results import (ASRResult, prepare_result, WebPanelEncoder, dct_to_result,  # noqa
                       UnknownDataFormat, obj_to_id)  # noqa
 from .command import (command, option, argument, get_recipes, ASRCommand,  # noqa
