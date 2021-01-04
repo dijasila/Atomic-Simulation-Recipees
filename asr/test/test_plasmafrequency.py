443abf9c99558b77f0d6f224f2aaf102b950fa69
Morten Gjerding <mortengjerding@gmail.com>
2021-01-04 13:25:44 +0100

    Fix plasma tests

--- asr/test/test_plasmafrequency.py
+++ asr/test/test_plasmafrequency.py
@@ -5,14 +5,12 @@ import pytest
 def test_plasmafrequency(asr_tmpdir_w_params, get_webcontent, mockgpaw,
                          test_material):
     """Test of the plasma freuquency recipe."""
-    from asr.plasmafrequency import calculate, main
+    from asr.plasmafrequency import main
     from pathlib import Path
     if sum(test_material.pbc) != 2:
         pytest.xfail("Plasma frequency is only implemented for 2D atm.")
-    test_material.write('structure.json')
-    calculate()
-    assert Path('es_plasma.gpw').is_file()
-    main()
+    main(atoms=test_material)
     assert not Path('es_plasma.gpw').is_file()
+    test_material.write('structure.json')
     content = get_webcontent()
     assert "plasmafrequency" in content
