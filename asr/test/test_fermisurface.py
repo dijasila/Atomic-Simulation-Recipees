a6331510750110629cae1f779a00265b7c50a088
Morten Gjerding <mortengjerding@gmail.com>
2021-01-04 10:50:02 +0100

    Fix fermisurface

--- asr/test/test_fermisurface.py
+++ asr/test/test_fermisurface.py
@@ -13,13 +13,15 @@ def test_fermisurface(
     fermi_level = 0.5
     mocker.patch.object(gpaw.GPAW, "_get_fermi_level")
     gpaw.GPAW._get_fermi_level.return_value = fermi_level
-    BN.write('structure.json')
-    result = main()
+
+    result = main(
+        atoms=BN,
+    ).result
 
     fermi_wave_vector = (2 * fermi_level / Hartree)**0.5 / Bohr
 
     moduli = np.sqrt(np.sum(result.contours[:, :2]**2, axis=1))
     assert moduli == pytest.approx(fermi_wave_vector, rel=0.01)
-
+    BN.write('structure.json')
     content = get_webcontent()
     assert 'fermi' in content
