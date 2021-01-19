56cec112538b8284f4327bbb1124612b624b999d
fBert31 <fafb@dtu.dk>
2021-01-19 15:38:17 +0100

    continue testing

--- asr/test/test_database_crosslinks.py
+++ asr/test/test_database_crosslinks.py
@@ -4,5 +4,10 @@ import pytest
 
 @pytest.mark.ci
 def test_database_crosslinks(asr_tmpdir, test_material):
-    print(test_material)
+    from ase.io import write, read
+    from asr.setup.defects import main
+
+    write('unrelaxed.json', std_test_materials[1])
+    main(supercell=[3, 3, 1])
+
     assert 1 == 1
