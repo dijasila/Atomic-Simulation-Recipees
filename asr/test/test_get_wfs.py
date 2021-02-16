1d54e705d6da74c6284ea6ce456e307229470939
fBert31 <fafb@dtu.dk>
2021-02-16 14:35:50 +0100

    add wfs test

--- asr/test/test_get_wfs.py
+++ asr/test/test_get_wfs.py
@@ -4,26 +4,24 @@ from .materials import Si, BN
 
 
 @pytest.mark.ci
-@pytest.mark.parallel
-@pytest.mark.parametrize("state", [0, 1, 2])
-def test_get_wfs(asr_tmpdir, mockgpaw, mocker, test_material, state):
+def test_get_wfs(asr_tmpdir, test_material):
+    import gpaw
+    import numpy as np
+    from pathlib import Path
     from ase.io import write
     from asr.gs import calculate, main
     from asr.get_wfs import main as get_wfs
-    import gpaw
 
     write('structure.json', test_material)
     calculate(
         calculator={
             "name": "gpaw",
-            "kpts": {"density": 6, "gamma": True},
+            "kpts": {"density": 2, "gamma": True},
         },
     )
 
     gsresults = main()
+    results = get_wfs()
 
-    # _, calc = restart('gs.gpw', txt=None)
-
-    results = get_wfs(state=state)
-
-    assert 0 == 0
+    assert Path('wf.0_0.cube').is_file()
+    assert results['above_below'] == (None, None)
