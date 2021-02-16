c015879d098f13fe520d47ff19c7c460d83cedba
fBert31 <fafb@dtu.dk>
2021-02-16 13:39:21 +0100

    small adjustments

--- asr/test/test_get_wfs.py
+++ asr/test/test_get_wfs.py
@@ -7,15 +7,10 @@ from .materials import Si, BN
 @pytest.mark.parallel
 @pytest.mark.parametrize("state", [0, 1, 2])
 def test_get_wfs(asr_tmpdir, mockgpaw, mocker, test_material, state):
+    from ase.io import write
     from asr.gs import calculate, main
     from asr.get_wfs import main as get_wfs
     import gpaw
-    # from gpaw import restart
-
-    mocker.path.object(gpaw.GPAW, "_get_eigenvalues")
-
-    spy = mocker.spy(asr.relax, "set_initial_magnetic_moments")
-    gpaw.GPAW._get_eigenvalues[0].return_value = eigenvalue
 
     write('structure.json', test_material)
     calculate(
