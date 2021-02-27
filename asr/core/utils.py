7e36853fb3f7aad53f2985919f0aea47613175d3
Morten Gjerding <mortengjerding@gmail.com>
2021-02-27 21:13:10 +0100

    Typo

--- asr/core/utils.py
+++ asr/core/utils.py
@@ -279,7 +279,11 @@ def get_recipe_from_name(name):
 def parse_mod_func(name):
     # Split a module function reference like
     # asr.relax@main into asr.relax and main.
-    mod, *func = name.split(':')
+    if '@' in name:
+        split_char = '@'
+    else:
+        split_char = ':'
+    mod, *func = name.split(split_char)
     if not func:
         func = ['main']
 
