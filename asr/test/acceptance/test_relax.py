3d9c33201f197fffb233e32d91e84620df5d27d1
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-03-16 12:52:30 +0100

    fix relax acceptance test

--- asr/test/acceptance/test_relax.py
+++ asr/test/acceptance/test_relax.py
@@ -15,8 +15,18 @@ def test_relax_fe_gpaw(asr_tmpdir):
                      [a, a, -a]],
                magmoms=[magmom],
                pbc=True)
-    Fe.write('unrelaxed.json')
-    main()
-    relaxed = read('structure.json')
+
+    parameters = dict(
+        name='gpaw',
+        mode={'name': 'pw', 'ecut': 200.0},
+        kpts=[2, 2, 2],
+        basis='sz(dzp)'
+    )
+
+    record = main(Fe,
+                  calculator=parameters,
+                  fmax=0.05)
+
+    relaxed = record.result['atoms']
     magmoms = relaxed.get_initial_magnetic_moments()
     assert magmoms[0] == pytest.approx(magmom, abs=0.1)
