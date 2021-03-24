fc7da81b6dd2ca17813dee39333d56c7b43e3f8d
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-03-24 13:37:43 +0100

    may not have inversion symmetry info

--- asr/bandstructure.py
+++ asr/bandstructure.py
@@ -378,7 +378,7 @@ def bs_pbe(row,
     e_mk = d['bs_soc']['energies']
     sz_mk = d['bs_soc']['sz_mk']
     sdir = row.get('spin_axis', 'z')
-    colorbar = not (row.magstate == 'NM' and row.has_inversion_symmetry)
+    colorbar = not (row.magstate == 'NM' and getattr(row, 'has_inversion_symmetry', False))
     ax, cbar = plot_with_colors(
         bsp,
         ax=ax,
