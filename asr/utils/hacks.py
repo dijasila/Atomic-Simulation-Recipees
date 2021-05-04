ea330cbff59d221714d93514f926331aa0a40316
Morten Gjerding <mortengjerding@gmail.com>
2021-05-04 20:07:34 +0200

    Fix tests

--- asr/utils/hacks.py
+++ asr/utils/hacks.py
@@ -1,5 +1,5 @@
 def gs_xcname_from_row(row):
-    params = row.data['results-asr.gs@calculate.json'].metadata.params
+    params = row.data.get_record('results-asr.gs@calculate.json').parameters
     if 'calculator' not in params:
         # What are the rules for when this piece of data exists?
         # Presumably the calculation used ASR defaults.
