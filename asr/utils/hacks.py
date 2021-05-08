b46d61bc170024ee04bfdbe04c1ed4fcc2d946f6
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-05-08 19:53:26 +0200

    start a class with all the methods that row should have

--- asr/utils/hacks.py
+++ asr/utils/hacks.py
@@ -1,13 +1,42 @@
 def gs_xcname_from_row(row):
-    data = row.data['results-asr.gs@calculate.json']
-    if not hasattr(data, 'metadata'):
-        # Old (?) compatibility hack
-        return 'PBE'
-    params = data.metadata.params
-    if 'calculator' not in params:
-        # What are the rules for when this piece of data exists?
-        # Presumably the calculation used ASR defaults.
-        return 'PBE'
-    # If the parameters are present, but were not set, we are using
-    # GPAW's default which is LDA.
-    return params['calculator'].get('xc', 'LDA')
+    return RowInfo(row).gs_xcname()
+
+
+class RowInfo:
+    def __init__(self, row):
+        self.row = row
+
+    @property
+    def gsdata(self):
+        return self.row.data['results-asr.gs@calculate.json']
+
+    def gs_xcname(self):
+        data = self.gsdata
+        if not hasattr(data, 'metadata'):
+            # Old (?) compatibility hack
+            return 'PBE'
+        params = data.metadata.params
+        if 'calculator' not in params:
+            # What are the rules for when this piece of data exists?
+            # Presumably the calculation used ASR defaults.
+            return 'PBE'
+        # If the parameters are present, but were not set, we are using
+        # GPAW's default which is LDA.
+        return params['calculator'].get('xc', 'LDA')
+
+    def evac_or_efermi(self):
+        gsdata = self.gsdata
+        evac = gsdata.evac
+        if evac is not None:
+            return EnergyReference('evac', evac, 'vacuum level')
+
+        efermi = gsdata.efermi
+        assert efermi is not None
+        return EnergyReference('efermi', efermi, 'Fermi level')
+
+
+class EnergyReference:
+    def __init__(self, key, value, prose_name):
+        self.key = key
+        self.value = value
+        self.prose_name = prose_name
