Fix tests and fix exfol energy calculation

asr/exfoliationenergy.py
@@ -49,9 +49,12 @@ def vdw_energy(atoms):
 
 
 def calculate_exfoliation(ml_e, vdw_e, bilayers_energies, atoms):
+    import numpy as np
     most_stable, bi_e = min(bilayers_energies, key=lambda t: t[1])
 
-    exf_e = 2 * (ml_e + vdw_e) - bi_e
+    area = np.linalg.norm(np.cross(atoms.cell[0], atoms.cell[1]))
+
+    exf_e = (2 * (ml_e + vdw_e) - bi_e) / area
     return exf_e, most_stable, [f for f, s in bilayers_energies]
 
 

asr/test/test_exfolenergy.py
@@ -49,8 +49,10 @@ def test_v_energy(asr_tmpdir):
     ml_e = np.random.rand() * 3
     vdw_e = np.random.rand()
 
-    exf_e, most_stable, bilayers = calculate_exfoliation(ml_e, vdw_e, bilayers_energies)
+    atoms = Atoms("H", cell=(5, 5, 5))
+    area = 5 * 5
+    exf_e, most_stable, bilayers = calculate_exfoliation(ml_e, vdw_e, bilayers_energies, atoms)
 
-    assert np.allclose(exf_e, 2 * (ml_e + vdw_e) - (-1.0)), exf_e
+    assert np.allclose(exf_e * area, 2 * (ml_e + vdw_e) - (-1.0)), exf_e
     assert most_stable == "S"
     assert all(x in bilayers for x in ["A", "B", "C", "S"])

asr/test/test_slidingequiv.py
@@ -395,9 +395,9 @@ def test_same_atoms(asr_tmpdir):
     bottom2 = bottom1.copy()
     top2 = bottom1.copy()
 
-    v = get_slide_vector(bottom1, top1, bottom2, top2)
+    v = get_slide_vector(bottom1, top1, bottom2, top2, np.zeros(3), np.zeros(3))
     assert v is not None
-    assert np.allclose(v, np.array([0, 0, 0])), v
+    assert np.allclose(v, np.zeros(3)), v
 
 
 @pytest.mark.ci
@@ -412,9 +412,9 @@ def test_same_misaligned(asr_tmpdir):
                                    [0, 1, 1],
                                    [0, 2, 0]], cell=(5, 5, 5))
 
-    v = get_slide_vector(bottom1, top1, bottom2, top2)
+    v = get_slide_vector(bottom1, top1, bottom2, top2, np.zeros(3), np.zeros(3))
     assert v is not None
-    assert np.allclose(v, np.array([0, 1, 0])), v
+    assert np.allclose(v, np.array([0, 1 / bottom1.cell[1, 1], 0])), v
 
 
 @pytest.mark.ci
@@ -429,7 +429,7 @@ def test_not_same_bottom_atoms(asr_tmpdir):
 
     top2 = bottom1.copy()
 
-    v = get_slide_vector(bottom1, top1, bottom2, top2)
+    v = get_slide_vector(bottom1, top1, bottom2, top2, np.zeros(3), np.zeros(3))
     assert v is None, v
 
 
@@ -444,7 +444,7 @@ def test_not_same_top_atoms(asr_tmpdir):
                                    [0, 0, 1],
                                    [0, 1, 0]], cell=(5, 5, 5))
 
-    v = get_slide_vector(bottom1, top1, bottom2, top2)
+    v = get_slide_vector(bottom1, top1, bottom2, top2, np.zeros(2), np.zeros(2))
     assert v is None, v
 
 