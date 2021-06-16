62bec48eea0734f4eb32cfd1d49454cff82580a5
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-17 00:27:19 +0200

    add loop-over-dependencies methods to cache

--- asr/core/cache.py
+++ asr/core/cache.py
@@ -133,6 +133,22 @@ class FileCacheBackend():
                 return True
         return False
 
+    def _immediate_dependencies(self, record):
+        # XXX We should probably get a O(1) __contains__ method
+        # XXX record == record2 seems to fail with an error
+        assert record.uid == self.get_record_from_uid(record.uid).uid
+        for dep in record.dependencies:
+            yield self.get_record_from_uid(dep.uid)
+
+    def recurse_dependencies(self, record):
+        found = set()
+        for dep_record in self._immediate_dependencies(record):
+            uid = record.uid
+            if uid not in found:
+                found.add(dep_record.uid)
+                yield dep_record
+                yield from self.recurse_dependencies(dep_record)
+
     def get_record_from_uid(self, run_uid):
         path = self.uid_table[run_uid]
         serialized_object = self._read_file(path)
