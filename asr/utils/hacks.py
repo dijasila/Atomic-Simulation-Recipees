3f8ce630c9723290baa79983d8a69c34c65de409
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-17 20:34:34 +0200

    all panels that used RowInfo now use DataContext, so remove RowInfo

--- asr/utils/hacks.py
+++ asr/utils/hacks.py
@@ -9,41 +9,6 @@ def gs_xcname_from_row(row):
     return params['calculator'].get('xc', 'LDA')
 
 
-class RowInfo:
-    """Utilities for retrieving data from database rows.
-
-    This object provides methods that are used in many recipes.
-    It exists to replace duplication across those recipes.  There are
-    probably better designs.
-    """
-
-    def __init__(self, row):
-        self.row = row
-
-    @property
-    def gsdata(self):
-        return self.row.data['results-asr.gs@calculate.json']
-
-    def gs_xcname(self):
-        return gs_xcname_from_row(self.row)
-
-    def have_evac(self):
-        return self.get_evac() is not None
-
-    def get_evac(self, default=None):
-        return self.row.get('evac', default)
-
-    def evac_or_efermi(self):
-        from asr.core.datacontext import EnergyReference
-        # We should probably be getting this data from GS results, not row
-        evac = self.get_evac()
-        if evac is not None:
-            return EnergyReference('evac', evac, 'vacuum level', 'vac')
-
-        efermi = self.row.get('efermi')
-        return EnergyReference('efermi', efermi, 'Fermi level', 'F')
-
-
 def get_parameter_description(recipename, parameters):
     from asr.database.browser import format_parameter_description
     desc = format_parameter_description(
