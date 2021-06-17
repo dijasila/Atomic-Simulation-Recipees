06d3f93adad180a568c14aed2d8f50d57a22e629
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-17 20:52:32 +0200

    no more need for gs_xcname_from_row()

--- asr/utils/hacks.py
+++ asr/utils/hacks.py
@@ -1,14 +1,3 @@
-def gs_xcname_from_row(row):
-    params = row.data.get_record('results-asr.gs@calculate.json').parameters
-    if 'calculator' not in params:
-        # What are the rules for when this piece of data exists?
-        # Presumably the calculation used ASR defaults.
-        return 'PBE'
-    # If the parameters are present, but were not set, we are using
-    # GPAW's default which is LDA.
-    return params['calculator'].get('xc', 'LDA')
-
-
 def get_parameter_description(recipename, parameters):
     from asr.database.browser import format_parameter_description
     desc = format_parameter_description(
