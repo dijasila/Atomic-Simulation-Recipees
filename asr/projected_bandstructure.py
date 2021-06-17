7527feb3857001202ed29655723c214c3059f9f8
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-17 19:21:20 +0200

    use datacontext with projected bandstructure

--- asr/projected_bandstructure.py
+++ asr/projected_bandstructure.py
@@ -9,7 +9,6 @@ from asr.core import (
 import typing
 
 from asr.database.browser import make_panel_description
-from asr.utils.hacks import gs_xcname_from_row, RowInfo
 from asr.bandstructure import calculate as bscalculate
 
 
@@ -26,32 +25,25 @@ plots.""",
 scf_projected_bs_filename = 'scf-projected-bs.png'
 
 
-def webpanel(result, row, key_descriptions):
-    from asr.database.browser import (fig,
-                                      entry_parameter_description,
-                                      describe_entry, WebPanel)
+def webpanel(result, context):
+    from asr.utils.hacks import get_parameter_description
+    from asr.database.browser import fig, describe_entry, WebPanel
 
-    xcname = gs_xcname_from_row(row)
+    xcname = context.xcname
 
-    # Projected band structure figure
-    parameter_description = entry_parameter_description(
-        row.data,
-        'asr.bandstructure@calculate')
-    dependencies_parameter_descriptions = ''
-    for dependency, exclude_keys in zip(
-            ['asr.gs@calculate'],
-            [set(['txt', 'fixdensity', 'verbose', 'symmetry',
-                  'idiotproof', 'maxiter', 'hund', 'random',
-                  'experimental', 'basis', 'setups'])]
-    ):
-        epd = entry_parameter_description(
-            row.data,
-            dependency,
-            exclude_keys=exclude_keys)
-        dependencies_parameter_descriptions += f'\n{epd}'
-    explanation = ('Orbital projected band structure without spin–orbit coupling\n\n'
-                   + parameter_description
-                   + dependencies_parameter_descriptions)
+    # XXX Why is it named bandstructure:calculate, why not projected?
+    desc1 = get_parameter_description(
+        'asr.bandstructure:calculate',
+        context.parameters)
+    desc2 = get_parameter_description(
+        'asr.gs',
+        context.ground_state().parameters)
+
+    explanation = ('Orbital projected band structure without '
+                   'spin–orbit coupling\n\n' + desc1 + '\n' + desc2)
+
+    # XXX Actually we use the SOC Fermilevel as reference in 3D systems
+    # don't we?
 
     panel = WebPanel(
         title=describe_entry(
@@ -392,7 +384,7 @@ def get_pie_markers(weight_xi, scale_marker=True, s=36., res=64):
     return pie_xi
 
 
-def projected_bs_scf(row, filename,
+def projected_bs_scf(context, filename,
                      npoints=40, markersize=36., res=64,
                      figsize=(5.5, 5), fontsize=10):
     """Produce the projected band structure.
@@ -412,11 +404,10 @@ def projected_bs_scf(row, filename,
     import matplotlib.pyplot as plt
     import matplotlib.patheffects as path_effects
     from matplotlib.lines import Line2D
-    import numpy as np
     from ase.spectrum.band_structure import BandStructure, BandStructurePlot
 
     # Extract projections data
-    data = row.data.get('results-asr.projected_bandstructure.json')
+    data = context.result
     weight_skni = data['weight_skni']
     yl_i = data['yl_i']
 
@@ -424,17 +415,16 @@ def projected_bs_scf(row, filename,
     c_i = get_yl_ordering(yl_i, data['symbols'])
 
     # Extract band structure data
-    d = row.data.get('results-asr.bandstructure.json')
+    d = context.bandstructure().result
     path = d['bs_nosoc']['path']
     ef = d['bs_nosoc']['efermi']
 
     # If a vacuum energy is available, use it as a reference
-    ref = row.get('evac', d.get('bs_nosoc').get('efermi'))
-    rowinfo = RowInfo(row)
-    eref = rowinfo.evac_or_efermi()  # XXX bs_nosoc versus the other one??
+    eref = context.energy_reference()
 
     # Determine plotting window based on band gap
-    gaps = row.data.get('results-asr.gs.json', {}).get('gaps_nosoc', {})
+    gs_results = context.gs_results()
+    gaps = gs_results['gaps_nosoc']
     if gaps.get('vbm'):
         emin = gaps.get('vbm') - 3
     else:
@@ -446,7 +436,7 @@ def projected_bs_scf(row, filename,
 
     # Take bands with energies in range
     e_skn = d['bs_nosoc']['energies']
-    inrange_skn = np.logical_and(e_skn > emin, e_skn < emax)
+    inrange_skn = (e_skn > emin) & (e_skn < emax)
     inrange_n = np.any(np.any(inrange_skn, axis=1), axis=0)
     e_skn = e_skn[:, :, inrange_n]
     weight_skni = weight_skni[:, :, inrange_n, :]
