f04002217894d573bbdc98e98e5b5e6802a9d7ae
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-28 16:58:07 +0200

    change indentation of help text.  Document --run/--dry-run

--- asr/core/cli.py
+++ asr/core/cli.py
@@ -885,49 +885,49 @@ def fromtree(
 
 totree_help = """Unpack an ASE database to a tree of folders.
 
-    This command unpacks an ASE database to into folders
-    that have a tree-like structure where directory names can be
-    given by the material parameters such stoichiometry or spacegroup
-    number.  For example: stoichiometry/spacegroup/formula.
+This command unpacks an ASE database to into folders
+that have a tree-like structure where directory names can be
+given by the material parameters such stoichiometry or spacegroup
+number.  For example: stoichiometry/spacegroup/formula.
 
-    The specific tree structure is given by the --tree-structure
-    option which can be customized according to the following table
+The specific tree structure is given by the --tree-structure
+option which can be customized according to the following table
 
-    * {stoi}: Material stoichiometry
-    * {spg}: Material spacegroup number
-    * {formula}: Chemical formula. A possible variant is {formula:metal}
-      in which case the formula will be sorted by metal atoms
-    * {reduced_formula}: Reduced chemical formula. Like {formula}
-      except the formula has been reduced, i.e., Mo2S4 -> MoS2.
-    * {wyck}: Unique Wyckoff positions. The unique alphabetically
-      sorted Wyckoff positions.
+* {stoi}: Material stoichiometry
+* {spg}: Material spacegroup number
+* {formula}: Chemical formula. A possible variant is {formula:metal}
+  in which case the formula will be sorted by metal atoms
+* {reduced_formula}: Reduced chemical formula. Like {formula}
+  except the formula has been reduced, i.e., Mo2S4 -> MoS2.
+* {wyck}: Unique Wyckoff positions. The unique alphabetically
+  sorted Wyckoff positions.
 
-    Examples:
+Examples:
 
-    For all these examples, suppose you have a database named "database.db".
+For all these examples, suppose you have a database named "database.db".
 
-    Unpack database using default parameters:
+Unpack database using default parameters:
 
-      $ asr database totree database.db --run"
+  $ asr database totree database.db --run
 
-    Don't actually unpack the database but do a dry-run:
+Don't actually unpack the database but do a dry-run:
 
-      $ asr database main database.totree database.db"
+  $ asr database main database.totree database.db
 
-    Only select a part of the database to unpack:
+Only select a part of the database to unpack:
 
-      $ asr database totree database.db --selection "natoms<3" --run
+  $ asr database totree database.db --selection "natoms<3" --run
 
-    Set custom folder tree-structure:
+Set custom folder tree-structure:
 
-      $ asr database totree database.db \
+  $ asr database totree database.db \
 --tree-structure "tree/{stoi}/{spg}/{formula:metal}" --run
 
-    Divide the tree into 2 chunks (in case the study of the materials
-    is divided between 2 people). Also sort after number of atoms,
-    so computationally expensive materials are divided evenly::
+Divide the tree into 2 chunks (in case the study of the materials
+is divided between 2 people). Also sort after number of atoms,
+so computationally expensive materials are divided evenly::
 
-      $ asr database totree database.db --sort natoms --chunks 2 --run
+  $ asr database totree database.db --sort natoms --chunks 2 --run
 
 """
 
@@ -949,10 +949,12 @@ def with_docstring(doc):
 
 @database.command()
 @click.argument('database', nargs=1, type=str)
-@click.option('--run/--dry-run', is_flag=True)
+@click.option('--run/--dry-run', is_flag=True,
+              help='use --run to actually unpack the database.  Default is '
+              '--dry-run which only simulates what would happen with --run.')
 @click.option('-s', '--selection', help='ASE-DB selection', type=str,
               default='')
-@click.option('-t', '--tree-structure', type=str,
+@click.option('-t', '--tree-structure', type=str, metavar='TREE',
               help='Specify folder tree structure (see description).',
               default='tree/{stoi}/{reduced_formula:abc}')
 @click.option('--sort', help='Sort the generated materials '
@@ -960,8 +962,9 @@ def with_docstring(doc):
 @click.option(
     '--copy/--no-copy', is_flag=True, help='Copy pointer tagged files')
 @click.option('--atomsfile',
-              help="Filename to unpack atomic structure to. "
-              "By default, don't write atoms file.",
+              metavar='FILE',
+              help='Filename to unpack atomic structure to. '
+              'By default, do not write atoms file.',
               type=str)
 @click.option(
     '-c', '--chunks', metavar='N', help='Divide the tree into N chunks',
