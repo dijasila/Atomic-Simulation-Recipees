a9a9452f949583d105915f840a6b0430470750a5
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-06-30 14:11:51 +0200

    update comment

--- asr/core/datacontext.py
+++ asr/core/datacontext.py
@@ -7,16 +7,11 @@ class RecordNotFound(LookupError):
 
 
 class DataContext:
-    # Can we find a more fitting name for this?
-    #
-    # Basically the context object provides information which is
+    # The context object provides information which is
     # relevant for web panels but is not on the result object itself.
-    # So "DataContext" makes sense but sounds a little bit abstract.
+    # This includes access to input parameters, record and dependencies,
+    # and so on.
 
-    # We need to add whatever info from Records that's needed by web panels.
-    # But not the records themselves -- they're not part of the database
-    # and we would like it to be possible to generate the figures
-    # from a database without additional info.
     def __init__(self, row, record):
         self.row = row
         self.record = record
