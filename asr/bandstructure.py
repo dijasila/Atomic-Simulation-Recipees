d2152ef396c8baae3954ad4fffd412def0ed2102
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-07-26 17:41:25 +0200

    flakes

--- asr/bandstructure.py
+++ asr/bandstructure.py
@@ -1,1 +1,3 @@
 from asr.c2db.bandstructure import main, calculate, legend_on_top
+
+__all__ = ['main', 'calculate', 'legend_on_top']
--- asr/projected_bandstructure.py
+++ asr/projected_bandstructure.py
@@ -1,1 +1,3 @@
 from asr.c2db.projected_bandstructure import main
+
+__all__ = ['main']
