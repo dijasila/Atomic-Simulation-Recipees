bc63afc75eef4c00b1f43568d5f38e780b457e19
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-05 11:41:39 +0200

    Update test of database project

--- asr/test/test_database_project.py
+++ asr/test/test_database_project.py
@@ -1,19 +1,24 @@
 from asr.database.project import make_project_from_config
+from asr.test.materials import BN, Si
+
 
 def test_database_project():
 
     no_grouping = lambda x, y: False
 
-    row_generator = [BN, graphene]
+    row_generator = [BN, Si]
     make_key_value_pairs = lambda data: dict(natoms=len(data))
+    key_descriptions = {
+        "natoms": KeyDescription(long="Number of atoms", short="natoms", unit="Number")
+    }
     project = make_project_from_config(
         grouping=no_grouping,
-        get_rows=row_generator,     
+        get_rows=row_generator,
         make_kvp=make_key_value_pairs,
     )
 
     assert project.grouping == no_grouping
-    
+
     db = project.collect_database()
 
-    assert len(db)
+    assert len(db) == 2
