10eee6b39d81a4fa1c71fa809a2279bd05b550d6
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-05 11:27:01 +0200

    Extract database project creation from app.py

--- asr/database/project.py
+++ asr/database/project.py
@@ -1,15 +1,61 @@
 """Define an object that represents a database project.
 
 """
+import pathlib
 
+
+@dataclass
 class DatabaseProject:
-    """Class that represents a database projects.
-    
-    Contains 
+    """Class that represents a database project.
+
+    Contains
     """
 
+    name: str
+    title: str
+    key_descriptions: KeyDescriptions
+    uid_key: str
+    database: pathlib.Path
+    row_to_dict_function: callable
+    handle_query_function: callable = args2query
+    default_columns: list[str] = ["formula", "uid"]
+    table_template: str = "asr/database/templates/table.html"
+    row_template: str = "asr/database/templates/row.html"
+
+    def todict(self) -> dict:
+        """Compile dict spec useful for ASE web application."""
+
+        return self.__dict__
+
     def group_rows(self):
-        ...        
+        ...
+
+
+def args2query(args):
+    return args["query"]
+
+
+def row_to_dict(row, project, layout_function, tmpdir):
+    project_name = project["name"]
+    uid = row.get(project["uid_key"])
+    s = Summary(
+        row,
+        create_layout=layout_function,
+        key_descriptions=project["key_descriptions"],
+        prefix=str(tmpdir / f"{project_name}/{uid}-"),
+    )
+    return s
+
+
+class DatabaseView:
+    """Class that represents a database view."""
+
+    def key_descriptions(self):
+        ...
+
+
+class Config:
+    pass
 
 
 def make_project_from_configuration_file(path: pathlib.Path):
@@ -19,4 +65,9 @@ def make_project_from_configuration_file(path: pathlib.Path):
     return project
 
 
-def read_configuration_file(path:)
+def read_configuration_file(path: pathlib.Path) -> Config:
+    ...
+
+
+def make_project_from_config():
+    return DatabaseProject()
