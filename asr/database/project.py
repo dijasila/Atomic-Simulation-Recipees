63b5d6e857e03f77723a733444a3d2a61e037eab
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-05 11:41:18 +0200

    More refactoring

--- asr/database/project.py
+++ asr/database/project.py
@@ -2,6 +2,9 @@
 
 """
 import pathlib
+import typing
+import dataclass
+from asr.database.key_descriptions import KeyDescriptions
 
 
 @dataclass
@@ -15,7 +18,7 @@ class DatabaseProject:
     title: str
     key_descriptions: KeyDescriptions
     uid_key: str
-    database: pathlib.Path
+    database: ase.db.Database
     row_to_dict_function: callable
     handle_query_function: callable = args2query
     default_columns: list[str] = ["formula", "uid"]
@@ -29,6 +32,48 @@ class DatabaseProject:
 
     def group_rows(self):
         ...
+
+
+def make_project_from_database_metadata(
+    database: str,
+    extra_kvp_descriptions: typing.Optional[dict] = None,
+) -> DatabaseProject:
+    db = connect(database, serial=True)
+    metadata = db.metadata
+    name = metadata.get("name", Path(database).name)
+
+    (self.tmpdir / name).mkdir()
+
+    def layout(*args, **kwargs):
+        return browser.layout(*args, pool=pool, **kwargs)
+
+    metadata = db.metadata
+    return {
+        "name": name,
+        "title": metadata.get("title", name),
+        "key_descriptions": create_key_descriptions(db, extra_kvp_descriptions),
+        "uid_key": metadata.get("uid", "uid"),
+        "database": db,
+        "handle_query_function": args2query,
+        "row_to_dict_function": partial(
+            row_to_dict,
+            layout_function=layout,
+            tmpdir=self.tmpdir,
+        ),
+        "default_columns": metadata.get("default_columns", ["formula", "uid"]),
+        "table_template": str(
+            metadata.get(
+                "table_template",
+                "asr/database/templates/table.html",
+            )
+        ),
+        "search_template": str(
+            metadata.get("search_template", "asr/database/templates/search.html")
+        ),
+        "row_template": str(
+            metadata.get("row_template", "asr/database/templates/row.html")
+        ),
+    }
 
 
 def args2query(args):
