eb1dd695912dc9180b872f824850f63a47ea8f0a
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-08 09:02:57 +0200

    Extract testing functionality of app rows

--- asr/database/app.py
+++ asr/database/app.py
@@ -323,44 +323,49 @@ def _main(dbapp, databases, host, test, extra_kvp_descriptions, pool):
     flask = dbapp.flask
 
     if test:
-        import traceback
-
-        projects = dbapp.projects
-
-        flask.testing = True
-        with flask.test_client() as c:
-            for name in projects:
-                print(f"Testing {name}")
-                c.get(f"/{name}/").data.decode()
-                project = projects[name]
-                db = project["database"]
-                uid_key = project["uid_key"]
-                n = len(db)
-                uids = []
-                for row in db.select(include_data=False):
-                    uids.append(row.get(uid_key))
-                    if len(uids) == n:
-                        break
-                print(len(uids))
-
-                for i, uid in enumerate(uids):
-                    url = f"/{name}/row/{uid}"
-                    print(f"\rRows: {i + 1}/{len(uids)} {url}", end="", flush=True)
-                    try:
-                        c.get(url).data.decode()
-                    except KeyboardInterrupt:
-                        raise
-                    except Exception:
-                        print()
-                        row = db.get(uid=uid)
-                        exc = traceback.format_exc()
-                        exc += (
-                            f"Problem with {uid}: "
-                            f"Formula={row.formula} "
-                            f"Crystal type={row.crystal_type}\n" + "-" * 20 + "\n"
-                        )
-                        with Path("errors.txt").open(mode="a") as fid:
-                            fid.write(exc)
-                            print(exc)
+        check_rows_of_all_projects(dbapp)
     else:
         flask.run(host=host, debug=True)
+
+
+def check_rows_of_all_projects(dbapp):
+    import traceback
+
+    flask = dbapp.flask
+    projects = dbapp.projects
+
+    flask.testing = True
+    with flask.test_client() as c:
+        for name in projects:
+            print(f"Testing {name}")
+            c.get(f"/{name}/").data.decode()
+            project = projects[name]
+            db = project["database"]
+            uid_key = project["uid_key"]
+            n = len(db)
+            uids = []
+            for row in db.select(include_data=False):
+                uids.append(row.get(uid_key))
+                if len(uids) == n:
+                    break
+            print(len(uids))
+
+            for i, uid in enumerate(uids):
+                url = f"/{name}/row/{uid}"
+                print(f"\rRows: {i + 1}/{len(uids)} {url}", end="", flush=True)
+                try:
+                    c.get(url).data.decode()
+                except KeyboardInterrupt:
+                    raise
+                except Exception:
+                    print()
+                    row = db.get(uid=uid)
+                    exc = traceback.format_exc()
+                    exc += (
+                        f"Problem with {uid}: "
+                        f"Formula={row.formula} "
+                        f"Crystal type={row.crystal_type}\n" + "-" * 20 + "\n"
+                    )
+                    with Path("errors.txt").open(mode="a") as fid:
+                        fid.write(exc)
+                        print(exc)
