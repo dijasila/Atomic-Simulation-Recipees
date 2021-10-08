084f40eecdc5bd0ac0ddd32ab4bbdab1286faa7e
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-08 15:17:11 +0200

    Refactor test

--- asr/test/test_database_app.py
+++ asr/test/test_database_app.py
@@ -8,19 +8,26 @@ from asr.database.project import make_project
 from asr.test.materials import Ag
 
 
-@pytest.mark.ci
-def test_simple_app(asr_tmpdir):
+@pytest.fixture
+def project(asr_tmpdir):
     database = connect("test_database.db")
     database.write(Ag)
-
     project = make_project(name="database.db", database=database, key_descriptions={})
+    return project
 
+
+@pytest.fixture
+def client(project):
     tmpdir = Path("tmp/")
     tmpdir.mkdir()
     app = ASRDBApp(tmpdir=tmpdir)
     app.initialize_project(project)
-
     app.flask.testing = True
     with app.flask.test_client() as client:
-        response = client.get("/database.db/").data.decode()
-        assert "<h1>database.db</h1>" in response
+        yield client
+
+
+@pytest.mark.ci
+def test_simple_app(client):
+    response = client.get("/database.db/").data.decode()
+    assert "<h1>database.db</h1>" in response
