aa45d85ae03d62065e57f8cd0531aeb881157981
Morten Gjerding <mogje@fysik.dtu.dk>
2021-10-11 09:08:24 +0200

    Add more tests

--- asr/test/test_database_project.py
+++ asr/test/test_database_project.py
@@ -3,8 +3,8 @@ from asr.database.project import get_project_from_namespace
 from types import SimpleNamespace
 
 
-@pytest.mark.ci
-def test_make_project_from_module(database_with_one_row):
+@pytest.fixture
+def project(database_with_one_row):
 
     namespace = SimpleNamespace(
         name="project.name",
@@ -13,4 +13,19 @@ def test_make_project_from_module(database_with_one_row):
 
     project = get_project_from_namespace(namespace)
 
+    return project
+
+
+@pytest.mark.ci
+def test_project_from_namespace_has_name(project):
     assert project.name == "project.name"
+
+
+@pytest.mark.ci
+def test_project_from_namespace_has_database(project, database_with_one_row):
+    assert project.database == database_with_one_row
+
+
+@pytest.mark.ci
+def test_project_from_namespace_has_title(project):
+    assert project.title == "project.name"
