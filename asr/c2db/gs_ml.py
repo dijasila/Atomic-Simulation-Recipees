464d7db9cdbabca75c2e8b871b4c77c05b3dc63e
Nikolaj Rorbak Knosgaard <nirkn@slid.fysik.dtu.dk>
2021-11-02 08:32:28 +0100

    added ml recipes for gs and band structure

---
+++ asr/c2db/gs_ml.py
@@ -0,0 +1,116 @@
+from asr.core import command, option, DictStr, ASRResult, prepare_result
+from asr.database.browser import make_panel_description, describe_entry
+from asr.gs import GapsResult
+import typing
+import numpy as np
+import os
+
+
+
+@command("asr.gs_ml",
+         requires=["gs.gpw"],
+         creates=["gs_matrix_elements.npz","gs_ml.gpw"]
+         )
+def calculate():
+    """Extract matrix elements for electronic fingerprints."""
+    import sys
+    sys.path.append('/home/niflheim/nirkn/electronic-structure-machine-learning/')
+    from efp import MLGPAW
+    ML = MLGPAW('gs.gpw')
+    ML.update_eigenvalues()
+
+
+@prepare_result
+class Result(ASRResult):
+    """Container for ground state results.
+
+    Examples
+    --------
+    >>> res = Result(data=dict(etot=0), strict=False)
+    >>> res.etot
+    0
+    """
+
+    etot: float
+    evac: float
+    evacdiff: float
+    efermi: float
+    gap: float
+    vbm: float
+    cbm: float
+    gap_dir: float
+    vbm_dir: float
+    cbm_dir: float
+    gap_dir_nosoc: float
+    gap_nosoc: float
+    gaps_nosoc: GapsResult
+    k_vbm_c: typing.Tuple[float, float, float]
+    k_cbm_c: typing.Tuple[float, float, float]
+    k_vbm_dir_c: typing.Tuple[float, float, float]
+    k_cbm_dir_c: typing.Tuple[float, float, float]
+    skn1: typing.Tuple[int, int, int]
+    skn2: typing.Tuple[int, int, int]
+    skn1_dir: typing.Tuple[int, int, int]
+    skn2_dir: typing.Tuple[int, int, int]
+
+    key_descriptions = dict(
+        etot='Total energy [eV].',
+        efermi='Fermi level [eV].',
+        gap='Band gap [eV].',
+        vbm='Valence band maximum [eV].',
+        cbm='Conduction band minimum [eV].',
+        gap_dir='Direct band gap [eV].',
+        vbm_dir='Direct valence band maximum [eV].',
+        cbm_dir='Direct conduction band minimum [eV].',
+        gap_dir_nosoc='Direct gap without SOC [eV].',
+        gap_nosoc='Gap without SOC [eV].',
+        gaps_nosoc='Container for bandgap results without SOC.',
+        k_vbm_c='Scaled k-point coordinates of valence band maximum (VBM).',
+        k_cbm_c='Scaled k-point coordinates of conduction band minimum (CBM).',
+        k_vbm_dir_c='Scaled k-point coordinates of direct valence band maximum (VBM).',
+        k_cbm_dir_c='Scaled k-point coordinates of direct calence band minimum (CBM).',
+        skn1="(spin,k-index,band-index)-tuple for valence band maximum.",
+        skn2="(spin,k-index,band-index)-tuple for conduction band minimum.",
+        skn1_dir="(spin,k-index,band-index)-tuple for direct valence band maximum.",
+        skn2_dir="(spin,k-index,band-index)-tuple for direct conduction band minimum.",
+    )
+
+@command(module='asr.gs_ml',
+         dependencies=['asr.gs_ml@calculate'],
+         returns=Result)
+def main() -> Result:
+    """Extract derived quantities from groundstate in gs.gpw."""
+    from gpaw import GPAW
+    from ase.io import read
+    from asr.calculators import get_calculator
+    from asr.gs import gaps
+
+    calc = GPAW('gs_ml.gpw')
+
+    etot = calc.get_potential_energy()
+    gaps_nosoc = gaps(calc, soc=False)
+    gaps_soc = gaps(calc, soc=True)
+    return Result.fromdata(
+        etot=etot,
+        gaps_nosoc=gaps_nosoc,
+        gap_dir_nosoc=gaps_nosoc.gap_dir,
+        gap_nosoc=gaps_nosoc.gap,
+        gap=gaps_soc.gap,
+        vbm=gaps_soc.vbm,
+        cbm=gaps_soc.cbm,
+        gap_dir=gaps_soc.gap_dir,
+        vbm_dir=gaps_soc.vbm_dir,
+        cbm_dir=gaps_soc.cbm_dir,
+        k_vbm_c=gaps_soc.k_vbm_c,
+        k_cbm_c=gaps_soc.k_cbm_c,
+        k_vbm_dir_c=gaps_soc.k_vbm_dir_c,
+        k_cbm_dir_c=gaps_soc.k_cbm_dir_c,
+        skn1=gaps_soc.skn1,
+        skn2=gaps_soc.skn2,
+        skn1_dir=gaps_soc.skn1_dir,
+        skn2_dir=gaps_soc.skn2_dir,
+        efermi=gaps_soc.efermi)
+
+
+if __name__ == '__main__':
+    main.cli()
