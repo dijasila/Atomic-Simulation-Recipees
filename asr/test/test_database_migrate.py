91d6e6a2a9f68eb59847e8bc273b15777fb64a27
Morten Gjerding <mogje@fysik.dtu.dk>
2021-12-03 09:02:09 +0100

    Integrate migration test with asr-test-data repo

--- asr/test/test_database_migrate.py
+++ asr/test/test_database_migrate.py
@@ -1,34 +1,50 @@
+import os
 from pathlib import Path
 
 import pytest
 
+from asr.database import connect
 from asr.database.migrate import (
-    write_converted_database,
     write_collapsed_database,
+    write_converted_database,
     write_migrated_database,
 )
 
-from asr.database import connect
+ASR_TEST_DIR = os.environ.get("ASR_TEST_DATA")
 
-COLLAPSE_DB = Path(__file__).parent / "data/mos2_collapsable_database.db"
+if ASR_TEST_DIR is not None:
+    DIRECTORY_WITH_DATABASES_TO_BE_MIGRATED = (
+        ASR_TEST_DIR
+        + "/ASR_MIGRATION_TEST_DATABASES"
+    )
+    databases_to_be_migrated = list(
+        Path(DIRECTORY_WITH_DATABASES_TO_BE_MIGRATED).glob("*.db")
+    )
+else:
+    databases_to_be_migrated = []
 
 
-@pytest.fixture
-def database_to_be_collapsed():
-    db = connect(COLLAPSE_DB)
+@pytest.fixture(
+    params=databases_to_be_migrated,
+    ids=[str(pth.absolute()) for pth in databases_to_be_migrated],
+)
+def database_to_be_migrated(request):
+    database_name = request.param
+    db = connect(database_name)
     return db
 
 
-@pytest.mark.skipif(not COLLAPSE_DB.is_file(), reason="Database to test doesn't exist.")
+@pytest.mark.skipif(
+    not databases_to_be_migrated,
+    reason="No databases to test migration on.",
+)
 @pytest.mark.ci
-def test_collapse_database(database_to_be_collapsed, asr_tmpdir):
-    assert len(database_to_be_collapsed) == 31
+def test_collapse_database(database_to_be_migrated, asr_tmpdir):
     with connect("collapsed.db") as collapsed:
-        write_collapsed_database(database_to_be_collapsed, collapsed)
-    assert len(collapsed) == 1
+        write_collapsed_database(database_to_be_migrated, collapsed)
     row = collapsed.get(id=1)
-    assert 'records' not in row.data
-    assert 'records' not in row.row.data
+    assert "records" not in row.data
+    assert "records" not in row.row.data
 
     with connect("converted.db") as converted:
         write_converted_database(collapsed, converted)
@@ -37,12 +53,14 @@ def test_collapse_database(database_to_be_collapsed, asr_tmpdir):
 
     records = row.records
     gwrecords = [rec for rec in records if rec.name == "asr.c2db.gw:main"]
-    assert len(gwrecords) == 1
+    if gwrecords:
+        assert len(gwrecords) == 1
+        gwrecord = gwrecords[0]
+        assert (
+            "asr.c2db.bandstructure:calculate"
+            in gwrecord.parameters.dependency_parameters
+        )
 
-    gwrecord = gwrecords[0]
-
-    assert "asr.c2db.bandstructure:calculate" in \
-        gwrecord.parameters.dependency_parameters
     with connect("migrated.db") as migrated:
         write_migrated_database(converted, migrated)
     assert "records" in migrated.get(id=1).data
