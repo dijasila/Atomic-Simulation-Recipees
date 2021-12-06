894da1c92daaa9794ec2c5ec05051bf0d3c81fb1
Morten Gjerding <mogje@fysik.dtu.dk>
2021-12-06 15:56:32 +0100

    Fix dos recipe

--- asr/test/test_dos.py
+++ asr/test/test_dos.py
@@ -4,5 +4,7 @@ from asr.c2db.dos import main
 
 @pytest.mark.ci
 @pytest.mark.parallel
-def test_dos(asr_tmpdir_w_params, mockgpaw, test_material):
+def test_dos(asr_tmpdir_w_params, mockgpaw, test_material, get_webcontent):
     main(atoms=test_material, kptdensity=2)
+    test_material.write("structure.json")
+    get_webcontent()
