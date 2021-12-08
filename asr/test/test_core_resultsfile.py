5e0add3b3d58dc5806f876eea56bd7e6d38590e1
Morten Gjerding <mortengjerding@gmail.com>
2021-12-08 12:35:26 +0100

    Make test of directory based migration

--- asr/test/test_core_resultsfile.py
+++ asr/test/test_core_resultsfile.py
@@ -1,7 +1,15 @@
+import os
+import shutil
+from pathlib import Path
+
 import pytest
-from asr.core.resultfile import convert_row_data_to_contexts, make_records_from_contexts
+
 from asr.c2db.relax import Result
+from asr.core.resultfile import (convert_row_data_to_contexts,
+                                 make_records_from_contexts)
+
 from .materials import Ag
+from asr.core.resultfile import get_resultfile_records_in_directory
 
 
 @pytest.mark.parametrize("recipename", ["asr.c2db.relax", "asr.c2db.gs:calculate"])
@@ -30,3 +38,23 @@ def test_creating_records_from_old_row_data_format(
     records = make_records_from_contexts(contexts)
     record = records[0]
     assert record.name == recipename
+
+
+ASR_TEST_DIR = os.environ.get("ASR_TEST_DATA")
+
+if ASR_TEST_DIR is not None:
+    directories = (
+        Path(ASR_TEST_DIR) / "EXAMPLE_MATERIAL_FOLDERS"
+    ).absolute().glob("*")
+else:
+    directories = []
+
+@pytest.mark.skipif(
+    ASR_TEST_DIR is None,
+    reason="No databases to test migration on.",
+)
+@pytest.mark.parametrize("directory", directories)
+@pytest.mark.ci
+def test_migrating_realistic_directories(asr_tmpdir, directory):
+    resultfile_records = get_resultfile_records_in_directory(directory)
+    assert resultfile_records
