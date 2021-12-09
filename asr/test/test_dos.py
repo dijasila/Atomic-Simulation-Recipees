9307f1915bdff23f29c2d62f9632113c467aaae0
Morten Gjerding <mogje@fysik.dtu.dk>
2021-12-09 10:30:20 +0100

    test: dont run dos test in parallel

--- asr/test/test_dos.py
+++ asr/test/test_dos.py
@@ -3,7 +3,6 @@ from asr.c2db.dos import main
 
 
 @pytest.mark.ci
-@pytest.mark.parallel
 def test_dos(asr_tmpdir_w_params, mockgpaw, test_material, get_webcontent):
     main(atoms=test_material, kptdensity=2)
     test_material.write("structure.json")
