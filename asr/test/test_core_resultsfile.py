13e121d1d51739e100db923ca9981ba9cac5f52d
Morten Gjerding <mogje@fysik.dtu.dk>
2021-12-09 10:16:36 +0100

    test: fix mark on test for migrating directory

--- asr/test/test_core_resultsfile.py
+++ asr/test/test_core_resultsfile.py
@@ -8,7 +8,9 @@ from asr.core.resultfile import get_resultfile_records_in_directory
 ASR_TEST_DIR = os.environ.get("ASR_TEST_DATA")
 
 if ASR_TEST_DIR is not None:
-    directories = (Path(ASR_TEST_DIR) / "EXAMPLE_MATERIAL_FOLDERS").absolute().glob("*")
+    directories = list(
+        (Path(ASR_TEST_DIR) / "EXAMPLE_MATERIAL_FOLDERS").absolute().glob("*")
+    )
 else:
     directories = []
 
@@ -17,8 +19,11 @@ else:
     ASR_TEST_DIR is None,
     reason="No databases to test migration on.",
 )
-@pytest.mark.parametrize("directory", directories)
-@pytest.mark.ci
-def test_migrating_realistic_directories(asr_tmpdir, directory):
+@pytest.mark.parametrize(
+    "directory", directories, ids=[str(pth) for pth in directories]
+)
+@pytest.mark.integration_test_gpaw
+# Needs gpaw to extract parameters from existing .gpw files
+def test_migrating_directory_succeeds(asr_tmpdir, directory):
     resultfile_records = get_resultfile_records_in_directory(directory)
-    assert len(resultfile_records)
+    assert resultfile_records
