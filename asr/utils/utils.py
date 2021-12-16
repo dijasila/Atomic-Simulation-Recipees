9708f13f0e7d57d46ad9ed94d17abad8a6f8fa5d
Stefano Americo <steame@dtu.dk>
2021-12-16 11:54:40 +0100

    Added function to asr.utils to extract lowest force structure from partially completed relax calculations

--- asr/utils/utils.py
+++ asr/utils/utils.py
@@ -11,3 +11,26 @@ def timed_print(*args, wait=20):
     if _LATEST_PRINT is None or (now - _LATEST_PRINT).seconds > wait:
         print(*args)
         _LATEST_PRINT = now
+
+
+def extract_structure(traj='relax.traj', tol=0.02, dest='structure.json'):
+    """Extract the structure with lowest fmax from a trajectory file 
+       if the lowest fmax is less than the requested tolerance,
+       Then write it to file.
+    """
+    from ase.io import Trajectory
+    import numpy as np
+
+    def fmax(atoms):
+        forces = atoms.get_forces()
+        return np.sqrt((forces**2).sum(axis=1).max())
+    
+    traj = Trajectory(traj)
+    forces = np.asarray([fmax(atoms) for atoms in traj])
+    fmin = forces.min()
+    if fmin <= tol:
+        print(fmin)
+        struct = traj[forces.argmin()]
+        struct.write(dest)
+    else:
+        print(f'Lowest fmax found: {fmin}, larger than requested {tol}')
