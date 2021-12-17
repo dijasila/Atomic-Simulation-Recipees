7e90aa375ea2dd05bcb87d4621847110a81f7936
Morten Gjerding <mortengjerding@gmail.com>
2021-12-17 15:39:46 +0100

    refactor: Move mutations from resultfile

--- asr/core/resultfile.py
+++ asr/core/resultfile.py
@@ -10,17 +10,19 @@ from ase.db.core import AtomsRow
 
 from asr.core import ASRResult
 
-from .command import get_recipes
 from .dependencies import Dependencies, Dependency
 from .metadata import construct_metadata
-from .migrate import mutation
 from .parameters import Parameters
 from .record import Record
 from .results import decode_object
-from .selector import Selector
-from .serialize import JSONSerializer
 from .specification import RunSpecification, get_new_uuid
-from .utils import get_recipe_from_name, parse_mod_func, read_file, write_file
+from .utils import (
+    get_recipe_from_name,
+    parse_mod_func,
+    fix_recipe_name_if_recipe_has_been_moved,
+    add_main_to_name_if_missing,
+)
+from .old_defaults import get_old_defaults
 
 
 def generate_uids(resultfiles) -> typing.Dict[pathlib.Path, str]:
@@ -94,8 +96,6 @@ def construct_record_from_context(
 ):
     from asr.core.codes import Code, Codes
     from asr.core.results import MetaDataNotSetError
-
-    from .record import Record
 
     result = record_context.result
     recipename = record_context.recipename
@@ -216,7 +216,7 @@ def fix_asr_gs_record_missing_calculator(record: "Record"):
     if record.name == 'asr.c2db.gs:calculate':
         if 'calculator' not in record.parameters:
             record.parameters.calculator = \
-                OLD_DEFAULTS['asr.c2db.gs:calculate']['calculator']
+                get_old_defaults()['asr.c2db.gs:calculate']['calculator']
 
 
 def get_relevant_resultfile_parameters(path, directory):
@@ -276,33 +276,6 @@ def get_recipe_name_from_filename(filename):
     name = name.replace("@", ":")
     name = add_main_to_name_if_missing(name)
     name = fix_recipe_name_if_recipe_has_been_moved(name)
-    return name
-
-
-def fix_recipe_name_if_recipe_has_been_moved(name: str) -> str:
-    if is_recipe_that_was_moved_to_c2db_subpackage(name):
-        name = extend_name_with_c2db_subpackage(name)
-    return name
-
-
-def is_recipe_that_was_moved_to_c2db_subpackage(name: str) -> bool:
-    count = name.count(".")
-    RECIPES_THAT_WASNT_MOVED_TO_C2DB_DIRECTORY = [
-        "asr.structureinfo", "asr.setinfo",
-        "asr.structureinfo:main", "asr.setinfo:main",
-    ]
-    if (
-        name.startswith("asr")
-        and count == 1
-        and name not in RECIPES_THAT_WASNT_MOVED_TO_C2DB_DIRECTORY
-    ):
-        return True
-    return False
-
-
-def extend_name_with_c2db_subpackage(name: str) -> str:
-    first, *rest = name.split(".")
-    name = ".".join([first, "c2db", *rest])
     return name
 
 
@@ -408,12 +381,6 @@ def get_dependency_matcher_from_name(
         dependencies.append(dep)
 
     return make_dependency_matcher(dependencies, "recipename")
-
-
-def add_main_to_name_if_missing(dep: str) -> str:
-    if ':' not in dep:
-        dep = dep + ':main'
-    return dep
 
 
 @dataclass
@@ -555,12 +522,6 @@ def fix_result_object_missing_name(recipename, result):
     return result
 
 
-def remove_main_in_name(name: str) -> str:
-    if name.endswith(':main'):
-        name = name[:-5]
-    return name
-
-
 def make_records_from_contexts(contexts):
     records = []
     for context in contexts:
@@ -605,156 +566,3 @@ def get_dependency_parameters(dependencies, records):
         params.update(get_dependency_parameters(dep.dependencies, records))
 
     return params
-
-
-PATH = pathlib.Path(__file__).parent / 'old_resultfile_defaults.json'
-TMP_DEFAULTS = JSONSerializer().deserialize(read_file(PATH))
-OLD_DEFAULTS: typing.Dict[str, typing.Dict[str, typing.Any]] = {}
-for tmpkey, value in TMP_DEFAULTS.items():
-    OLD_DEFAULTS[
-        add_main_to_name_if_missing(
-            fix_recipe_name_if_recipe_has_been_moved(tmpkey)
-        )
-    ] = value
-
-
-sel = Selector()
-sel.version = sel.EQ(-1)
-
-
-@mutation(selector=sel, uid="9269242a035a4731bcd5ac609ff0a086", eagerness=-1)
-def update_resultfile_record_to_version_0(record):
-    """Extract missing parameters from dependencies, add them and set version to 0."""
-    default_params = OLD_DEFAULTS[record.name]
-    name = record.name
-    recipe = get_recipe_from_name(name)
-    sig = recipe.get_signature()
-    parameters = record.parameters
-
-    sig_parameters = {parameter for parameter in sig.parameters}
-
-    unused_old_params = set(parameters.keys())
-    missing_params = set()
-    dep_params = record.parameters.get('dependency_parameters', {})
-    unused_dependency_params = {name: set(values)
-                                for name, values in dep_params.items()}
-
-    params = recipe.get_argument_descriptors()
-    atomsparam = [param for name, param in params.items()
-                  if name == 'atoms'][0]
-    atomsfilename = atomsparam['default']
-
-    new_parameters = Parameters({})
-    for key in sig_parameters:
-        if key in parameters:
-            param_value = parameters[key]
-            new_parameters[key] = param_value
-            unused_old_params.remove(key)
-            remove_matching_dependency_params(
-                dep_params, unused_dependency_params, key, param_value)
-            continue
-        elif key == 'atoms':
-            # Atoms are treated differently
-            new_parameters[key] = parameters.atomic_structures[atomsfilename]
-            continue
-        dep_names, dep_values = find_deps_matching_key(
-            dep_params, key,
-        )
-        if dep_values:
-            assert all_values_equal(dep_values)
-            new_parameters[key] = dep_values[0]
-            for dependency in dep_names:
-                remove_dependency_param(unused_dependency_params, key, dependency)
-        else:
-            missing_params.add(key)
-
-    unused_old_params.remove('atomic_structures')
-    unused_old_params -= set(['dependency_parameters'])
-    unused_dependency_params = {
-        (name, value)
-        for name, values in unused_dependency_params.items()
-        for value in values
-        if value != 'atomic_structures'
-    }
-
-    if missing_params and not unused_old_params and not unused_dependency_params:
-        for key in missing_params:
-            new_parameters[key] = default_params[key]
-    else:
-        missing_params_msg = (
-            'Could not extract following parameters from '
-            f'dependencies={missing_params}. '
-        )
-        unused_old_params_msg = \
-            f'Parameters from resultfile record not used={unused_old_params}. '
-        unused_dependency_params_msg = \
-            f'Dependency parameters unused={unused_dependency_params}. '
-        assert not (unused_old_params or missing_params or unused_dependency_params), (
-            ''.join(
-                [
-                    missing_params_msg if missing_params else '',
-                    unused_old_params_msg if unused_old_params else '',
-                    unused_dependency_params_msg if unused_dependency_params else '',
-                    f'Please add a mutation for {name} that fixes these issues '
-                    'and run migration tool again.',
-                ]
-            )
-        )
-
-    # Sanity check
-    assert set(new_parameters.keys()) == set(sig_parameters)
-
-    record.run_specification.parameters = new_parameters
-    record.version = 0
-    return record
-
-
-def remove_matching_dependency_params(
-    dep_params,
-    unused_dependency_params,
-    key,
-    param_value,
-):
-    dep_names, dep_values = find_deps_matching_key(dep_params, key)
-    for dep_name, dep_value in zip(dep_names, dep_values):
-        if dep_value == param_value:
-            remove_dependency_param(
-                unused_dependency_params, key, dep_name)
-
-
-def all_values_equal(values):
-    first_value = values[0]
-    return all(
-        first_value == other_value
-        for other_value in values[1:]
-    )
-
-
-def find_deps_matching_key(dep_params, key):
-    candidate_dependency_names = []
-    candidate_dependency_values = []
-    for depname, recipedepparams in dep_params.items():
-        try:
-            candidate_dependency_values.append(recipedepparams[key])
-        except KeyError:
-            continue
-        candidate_dependency_names.append(depname)
-    return candidate_dependency_names, candidate_dependency_values
-
-
-def get_defaults_from_all_recipes():
-    defaults = {}
-    for recipe in get_recipes():
-        defaults[recipe.name] = recipe.defaults
-
-    return defaults
-
-
-def remove_dependency_param(unused_dependency_params, key, dependency):
-    unused_dependency_params[dependency].remove(key)
-
-
-if __name__ == '__main__':
-    defaults = get_defaults_from_all_recipes()
-    jsontxt = JSONSerializer().serialize(defaults)
-    write_file(PATH, jsontxt)
