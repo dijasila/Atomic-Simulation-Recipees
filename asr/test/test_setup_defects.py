57fd9d9ca9145489fa9e37dc9377b4f8dd4973e1
fBert31 <fafb@dtu.dk>
2022-01-04 11:00:32 +0100

    update tests

--- asr/test/test_setup_defects.py
+++ asr/test/test_setup_defects.py
@@ -23,25 +23,27 @@ def test_setup_defects(asr_tmpdir):
     from .materials import std_test_materials
     from asr.setup.defects import main
     from ase.io import write, read
+    from ase.calculators.calculator import compare_atoms
 
     atoms = std_test_materials[1]
     write('unrelaxed.json', atoms)
     atoms = atoms.repeat((3, 3, 1))
     main(supercell=(3, 3, 1))
     pristine = read('defects.pristine_sc.331/structure.json')
-    for i in range(len(atoms.get_scaled_positions())):
-        assert atoms.get_scaled_positions(
-        )[i][0] == pristine.get_scaled_positions()[i][0]
-        assert atoms.get_scaled_positions(
-        )[i][1] == pristine.get_scaled_positions()[i][1]
-        assert atoms.get_scaled_positions(
-        )[i][2] == pristine.get_scaled_positions()[i][2]
-    for i in range(len(atoms.get_cell())):
-        assert atoms.get_cell()[i][0] == pristine.get_cell()[i][0]
-        assert atoms.get_cell()[i][1] == pristine.get_cell()[i][1]
-        assert atoms.get_cell()[i][2] == pristine.get_cell()[i][2]
-    for i in range(len(atoms.numbers)):
-        assert atoms.numbers[i] == pristine.numbers[i]
+    assert compare_atoms(atoms, pristine) == []
+    # for i in range(len(atoms.get_scaled_positions())):
+    #     assert atoms.get_scaled_positions(
+    #     )[i][0] == pristine.get_scaled_positions()[i][0]
+    #     assert atoms.get_scaled_positions(
+    #     )[i][1] == pristine.get_scaled_positions()[i][1]
+    #     assert atoms.get_scaled_positions(
+    #     )[i][2] == pristine.get_scaled_positions()[i][2]
+    # for i in range(len(atoms.get_cell())):
+    #     assert atoms.get_cell()[i][0] == pristine.get_cell()[i][0]
+    #     assert atoms.get_cell()[i][1] == pristine.get_cell()[i][1]
+    #     assert atoms.get_cell()[i][2] == pristine.get_cell()[i][2]
+    # for i in range(len(atoms.numbers)):
+    #     assert atoms.numbers[i] == pristine.numbers[i]
 
     pathlist = list(Path('.').glob('defects.BN_331*/charge_*/'))
     for path in pathlist:
