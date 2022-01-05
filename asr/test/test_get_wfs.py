3e3c86cddd52c3887b0393bb06286b63cfbf057b
fBert31 <fafb@dtu.dk>
2022-01-05 11:05:15 +0100

    flake8

--- asr/test/test_get_wfs.py
+++ asr/test/test_get_wfs.py
@@ -2,27 +2,6 @@ import pytest
 import numpy as np
 
 
-# @pytest.mark.ci
-# def test_get_wfs(asr_tmpdir, test_material):
-#     from pathlib import Path
-#     from ase.io import write
-#     from asr.gs import calculate, main
-#     from asr.get_wfs import main as get_wfs
-# 
-#     write('structure.json', test_material)
-#     calculate(
-#         calculator={
-#             "name": "gpaw",
-#             "kpts": {"density": 2, "gamma": True},
-#         },
-#     )
-# 
-#     main()
-#     results = get_wfs()
-# 
-#     assert Path('wf.0_0.cube').is_file()
-#     assert results['above_below'] == (None, None)
-
 @pytest.mark.parametrize('ef', np.arange(-5, 3, 0.5))
 @pytest.mark.ci
 def test_return_erange_states(ef):
