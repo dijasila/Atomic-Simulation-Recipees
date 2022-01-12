557cfbf83c05d3e57097001cc7c3719b380543b5
fBert31 <fafb@dtu.dk>
2022-01-12 15:45:34 +0100

    updated tests for asr.sj_analyze

--- asr/test/test_sj_analyze.py
+++ asr/test/test_sj_analyze.py
@@ -12,8 +12,7 @@ transitions = [{
     'transition_values': {
         'transition': 1,
         'erelax': 0,
-        'evac': 0}}
-    ]
+        'evac': 0}}]
 
 
 @pytest.mark.parametrize('tran', [[transitions[0]], transitions[1]])
@@ -28,21 +27,30 @@ def test_calculate_formation_energies(tran):
               + (vbm
                  - transitions[0]['transition_values']['transition']) * 1),
         '-1': (eform_0
-              + (vbm
-                 - transitions[1]['transition_values']['transition']) * -1)}
+               + (vbm
+                  - transitions[1]['transition_values']['transition']) * -1)}
     eforms = calculate_formation_energies(eform_0, transitions, vbm)
 
     for eform in eforms:
         assert eform[0] == pytest.approx(results[f'{str(eform[1])}'])
 
 
+@pytest.mark.parametrize('ref_trans', ['1/2', '2/3', '-1/-2'])
 @pytest.mark.ci
-def test_ordered_transitions():
-    from asr.sj_analyze import order_transition
+def test_ordered_transitions(ref_trans):
+    from asr.sj_analyze import order_transitions
 
-    new = {'transition_name': '2/3',
+    new = {'transition_name': ref_trans,
            'transition_values': {
                'transition': 1,
                'erelax': 0,
                'evac': 0}}
     new_transitions = [transitions[1], new, transitions[0]]
+    if ref_trans == '1/2' or ref_trans == '2/3':
+        order = ['0/1', ref_trans, '0/-1']
+    else:
+        order = ['0/1', '0/-1', ref_trans]
+
+    ordered_transitions = order_transitions(new_transitions)
+    for i, transition in enumerate(ordered_transitions):
+        assert transition['transition_name'] == order[i]
