dfe8c575246a517e035a0e2c3946eea9e99db286
fBert31 <fafb@dtu.dk>
2022-01-12 13:59:00 +0100

    WIP for sj_analyze tests

--- asr/test/test_sj_analyze.py
+++ asr/test/test_sj_analyze.py
@@ -4,13 +4,21 @@ import pytest
 transitions = [{
     'transition_name': '0/1',
     'transition_values': {
-        'transition': 2,
+        'transition': 0.5,
         'erelax': 0,
-        'evac': 0}}]
+        'evac': 0}},
+    {
+    'transition_name': '0/-1',
+    'transition_values': {
+        'transition': 1,
+        'erelax': 0,
+        'evac': 0}}
+    ]
 
 
+@pytest.mark.parametrize('tran', [[transitions[0]], transitions[1]])
 @pytest.mark.ci
-def test_calculate_formation_energies():
+def test_calculate_formation_energies(tran):
     from asr.sj_analyze import calculate_formation_energies
     vbm = 0.4
     eform_0 = 1
@@ -18,8 +26,23 @@ def test_calculate_formation_energies():
         '0': eform_0,
         '1': (eform_0
               + (vbm
-                 - transitions[0]['transition_values']['transition']) * 1)}
+                 - transitions[0]['transition_values']['transition']) * 1),
+        '-1': (eform_0
+              + (vbm
+                 - transitions[1]['transition_values']['transition']) * -1)}
     eforms = calculate_formation_energies(eform_0, transitions, vbm)
 
     for eform in eforms:
         assert eform[0] == pytest.approx(results[f'{str(eform[1])}'])
+
+
+@pytest.mark.ci
+def test_ordered_transitions():
+    from asr.sj_analyze import order_transition
+
+    new = {'transition_name': '2/3',
+           'transition_values': {
+               'transition': 1,
+               'erelax': 0,
+               'evac': 0}}
+    new_transitions = [transitions[1], new, transitions[0]]
