5ca3108098e6c3f0ee75a72c07d309c9968a7f32
fBert31 <fafb@dtu.dk>
2022-01-26 13:44:11 +0100

    fix test pipeline

--- asr/test/test_hyperfine.py
+++ asr/test/test_hyperfine.py
@@ -6,6 +6,7 @@ def test_g_factors_from_gyromagnetic_ratios():
     from asr.hyperfine import (g_factors_from_gyromagnetic_ratios,
                                gyromagnetic_ratios)
 
+    result_hydrogen = 5.5856946
     g_factors = g_factors_from_gyromagnetic_ratios(gyromagnetic_ratios)
-    print(g_factors, gyromagnetic_ratios)
-    assert False
+
+    assert g_factors['H'] == pytest.approx(result_hydrogen)
