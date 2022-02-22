2fa02e83d5277229fea90466657abb89c8a7d0b3
fBert31 <fafb@dtu.dk>
2022-02-22 09:07:20 +0100

    fix import structure for hyperfine tests

--- asr/test/test_hyperfine.py
+++ asr/test/test_hyperfine.py
@@ -1,12 +1,14 @@
 import pytest
-from .materials import std_test_materials
+import numpy as np
+from .materials import std_test_materials, BN
+from asr.hyperfine import (g_factors_from_gyromagnetic_ratios,
+                           gyromagnetic_ratios,
+                           rescale_hyperfine_tensor,
+                           get_gyro_results)
 
 
 @pytest.mark.ci
 def test_g_factors_from_gyromagnetic_ratios():
-    from asr.hyperfine import (g_factors_from_gyromagnetic_ratios,
-                               gyromagnetic_ratios)
-
     result_hydrogen = 5.5856946
     g_factors = g_factors_from_gyromagnetic_ratios(gyromagnetic_ratios)
 
@@ -15,12 +17,6 @@ def test_g_factors_from_gyromagnetic_ratios():
 
 @pytest.mark.ci
 def test_paired_system(asr_tmpdir):
-    from .materials import BN
-    from asr.hyperfine import (gyromagnetic_ratios,
-                               rescale_hyperfine_tensor,
-                               g_factors_from_gyromagnetic_ratios)
-    import numpy as np
-
     atoms = BN.copy()
     refarray = np.ones((len(atoms), 3, 3))
     symbols = atoms.symbols
@@ -49,12 +45,6 @@ def test_paired_system(asr_tmpdir):
 @pytest.mark.parametrize('atoms', std_test_materials)
 @pytest.mark.ci
 def test_get_gyro_results(atoms):
-    from asr.hyperfine import (gyromagnetic_ratios,
-                               rescale_hyperfine_tensor,
-                               g_factors_from_gyromagnetic_ratios,
-                               get_gyro_results)
-    import numpy as np
-
     refarray = np.ones((len(atoms), 3, 3))
     symbols = atoms.symbols
     magmoms = np.ones(len(atoms))
