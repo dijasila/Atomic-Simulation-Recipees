3beb07939ca5c4d611c44b6e83d5963ca686f7d3
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-03-24 12:07:11 +0100

    structureinfo fails on some computers (?) because what should have been a value is passed as a key somewhere.  Since web panel code is changing, remove special testing web panel and run an integration test instead

--- asr/test/test_structureinfo.py
+++ asr/test/test_structureinfo.py
@@ -1,55 +1,9 @@
-import typing
-from asr.core import ASRResult
-from asr.structureinfo import Result
-
-skip_keys = {'version', 'prev_version', 'key_descriptions'}
+from ase.build import bulk
+from asr.structureinfo import main
 
 
-def fill_in_arbitrary_result_data(cls):
-
-    type_hints = typing.get_type_hints(cls)
-
-    data = {}
-    for key, tp in type_hints.items():
-        if key in skip_keys:
-            continue
-        if issubclass(tp, ASRResult):
-            data[key] = fill_in_arbitrary_result_data(tp)
-        elif issubclass(tp, typing.Tuple):
-            data[key] = tuple(tp2(2) for tp2 in tp.__args__)
-        elif tp == dict:
-            data[key] = {'key': 'value'}
-        else:
-            data[key] = tp(2)
-
-    return cls(data=data)
-
-
-class Row:
-
-    def __init__(self, result, data):
-        self.data = data
-        self.result = result
-        self.cod_id = 'some_cod_id'
-        self.icsd_id = 'some_icsd_id'
-        self.doi = 'some_doi'
-        setattr(self, 'class', 'some class')
-
-    def get(self, key):
-        if hasattr(self, key):
-            return getattr(self, key)
-        return getattr(self.result, key)
-
-
-def test_cod_id():
-    result = fill_in_arbitrary_result_data(Result)
-
-    row = Row(
-        result,
-        data={'results-asr.structureinfo.json': result},
-    )
-    webpanels = result.format_as('ase_webpanel', row, {})
-    webpanel = webpanels[0]
-    tablerows = webpanel['columns'][0][0]['rows']
-    keys = [row[0] for row in tablerows]
-    assert 'cod_id' in keys
+def test_cod_id(asr_tmpdir):
+    atoms = bulk('Si')
+    atoms.write('structure.json')
+    result = main()
+    assert result['spacegroup'] == 'Fd-3m'
