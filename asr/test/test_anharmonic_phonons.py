6b3166a45105af03aca91a603a10dbb7629c9e84
Urko Petralanda <urkopetralanda@hotmail.com>
2022-05-07 11:29:19 +0200

    adapt test file to idealgas

--- asr/test/test_anharmonic_phonons.py
+++ asr/test/test_anharmonic_phonons.py
@@ -12,7 +12,7 @@ def test_anharmonic_phonons():
     atoms = bulk('Ni')
     atoms.write("structure.json")
 
-    main(cellsize=5, calculator = 'EMT', nat_dim =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=4.0, cut2=3.0, cut3=2.0)
+    main(cellsize=5, calculator = 'IdealGas', nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=4.0, cut2=3.0, cut3=2.0)
 
     result = Path('results-asr.anharmonic_phonons3_result.json')
     assert result.is_file()
