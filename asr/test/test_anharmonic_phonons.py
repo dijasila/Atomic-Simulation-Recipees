d523f6ea92a20ed3c28d32399e9696103a3a5406
Urko Petralanda <urkopetralanda@hotmail.com>
2022-05-14 22:50:57 +0200

    test

--- asr/test/test_anharmonic_phonons.py
+++ asr/test/test_anharmonic_phonons.py
@@ -5,14 +5,14 @@ from pathlib import Path
 @pytest.mark.ci
 def test_anharmonic_phonons():
     """test for anharmonic_phonons"""
-    from asr.anharmonic_phonons import  main
-    from asr.core import read_json
+    from asr.anharmonic_phonons import main
     from ase.build import bulk
 
     atoms = bulk('Ni')
     atoms.write("structure.json")
 
-    main(cellsize=3, calculator={'name': 'lj'}, nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=3.5, cut2=2.5, cut3=1.5)
+    main(cellsize=3, calculator={'name': 'lj'}, nd=3, mesh_ph3=1,
+         number_structures=1, t1=300, t2=300, cut1=3.5, cut2=2.5, cut3=1.5)
 
     result = Path('results-asr.anharmonic_phonons_result.json')
     assert result.is_file()
