77645678005cd2723d2882ce1b902b4800bb263f
Urko Petralanda <urkopetralanda@hotmail.com>
2022-05-14 18:41:39 +0200

    adapt test to new calculate function

--- asr/test/test_anharmonic_phonons.py
+++ asr/test/test_anharmonic_phonons.py
@@ -12,7 +12,7 @@ def test_anharmonic_phonons():
     atoms = bulk('Ni')
     atoms.write("structure.json")
 
-    main(cellsize=3, calculator = 'IdealGas', nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=3.5, cut2=2.5, cut3=1.5)
+    main(cellsize=3, calculator={'name': 'lj'}, nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=3.5, cut2=2.5, cut3=1.5)
 
     result = Path('results-asr.anharmonic_phonons_result.json')
     assert result.is_file()
