67cfc8ec07584f966a81d9379d49256d3e7e2563
Urko Petralanda <urkopetralanda@hotmail.com>
2022-05-14 12:00:52 +0200

    faster test

--- asr/test/test_anharmonic_phonons.py
+++ asr/test/test_anharmonic_phonons.py
@@ -12,7 +12,7 @@ def test_anharmonic_phonons():
     atoms = bulk('Ni')
     atoms.write("structure.json")
 
-    main(cellsize=5, calculator = 'IdealGas', nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=4.0, cut2=3.0, cut3=2.0)
+    main(cellsize=3, calculator = 'IdealGas', nd =3, mesh_ph3 = 1, number_structures=1, t1=300, t2 = 300, cut1=3.5, cut2=2.5, cut3=1.5)
 
-    result = Path('results-asr.anharmonic_phonons3_result.json')
+    result = Path('results-asr.anharmonic_phonons_result.json')
     assert result.is_file()
