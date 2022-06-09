c1f13a0007e7dbf53d1367032a65a7cd66a64f1e
Fredrik Nilsson <fanni@dtu.dk>
2022-06-09 16:29:10 +0200

    flakes

--- asr/relax_defects.py
+++ asr/relax_defects.py
@@ -1,9 +1,9 @@
 from typing import Sequence
 
-# Define calculators that are needed for the params.json file                                                                                                                                              
-# of each individual defect and charge folder.                                                                                                                                                             
-# Note, that the only real change to the default relax and gs                                                                                                                                              
-# parameters is 'spinpol' here. Should be changed in 'master'.                                                                                                      
+# Define calculators that are needed for the params.json file
+# of each individual defect and charge folder.
+# Note, that the only real change to the default relax and gs
+# parameters is 'spinpol' here. Should be changed in 'master'.
 relax_calc_dict = {'name': 'gpaw',
                    'mode': {
                        'name': 'pw',
@@ -24,6 +24,7 @@ relax_calc_dict = {'name': 'gpaw',
                        'width': 0.02},
                    'spinpol': True}
 
+
 def main(rn, atoms,
          charge_states: Sequence[int] = [0],
          calculator: dict = relax_calc_dict):
@@ -38,13 +39,13 @@ def main(rn, atoms,
     return atoms_dict
 
 
-
-
 class SetupAndRelaxDefects:
-    def __init__(self, rn, atoms, charge_states=[0], calculator=relax_calc_dict,setup_defect_kwargs={}):
+    def __init__(self, rn, atoms, charge_states=[0], calculator=relax_calc_dict,
+                 setup_defect_kwargs={}):
         from asr.setup.defects import main as main_setup
-        
-        self.Defect_dict = main_setup(rn,atoms,**setup_defect_kwargs)
+
+        self.Defect_dict = main_setup(rn, atoms, **setup_defect_kwargs)
         self.relaxed_defect = {}
         for key, item in self.Defect_dict.items():
-            self.relaxed_defect[key] = main(rn,atoms=item.output,charge_states=charge_states)
+            self.relaxed_defect[key] = main(rn, atoms=item.output,
+                                            charge_states=charge_states)
