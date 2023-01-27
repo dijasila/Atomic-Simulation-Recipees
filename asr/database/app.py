cf70fbed0eba7eb0f44bbdb4cd9d0fda78a03412
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-01-27 12:34:29 +0100

    move the testing code out of the way

--- asr/database/app.py
+++ asr/database/app.py
@@ -106,7 +106,6 @@ class WebApp:
     def initialize_project(self, database, extra_kvp_descriptions=None,
                            pool=None):
         from asr.database import browser
-        from functools import partial
 
         db = connect(database, serial=True)
         metadata = db.metadata
@@ -300,42 +299,9 @@ def _main(databases, host, test, extra_kvp_descriptions, pool):
         webapp.initialize_project(database, extra_kvp_descriptions, pool)
 
     if test:
-        import traceback
         app.testing = True
-        with app.test_client() as c:
-            for name in projects:
-                print(f'Testing {name}')
-                c.get(f'/{name}/').data.decode()
-                project = projects[name]
-                db = project.database
-                uid_key = project.uid_key
-                n = len(db)
-                uids = []
-                for row in db.select(include_data=False):
-                    uids.append(row.get(uid_key))
-                    if len(uids) == n:
-                        break
-                print(len(uids))
-
-                for i, uid in enumerate(uids):
-                    url = f'/{name}/row/{uid}'
-                    print(f'\rRows: {i + 1}/{len(uids)} {url}',
-                          end='', flush=True)
-                    try:
-                        c.get(url).data.decode()
-                    except KeyboardInterrupt:
-                        raise
-                    except Exception:
-                        print()
-                        row = db.get(uid=uid)
-                        exc = traceback.format_exc()
-                        exc += (f'Problem with {uid}: '
-                                f'Formula={row.formula} '
-                                f'Crystal type={row.crystal_type}\n'
-                                + '-' * 20 + '\n')
-                        with Path('errors.txt').open(mode='a') as fid:
-                            fid.write(exc)
-                            print(exc)
+        from asr.database.app_testing import run_testing
+        run_testing(app, projects)
     else:
         webapp.app.run(host=host, debug=True)
 
