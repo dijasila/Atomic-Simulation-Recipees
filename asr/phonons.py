b5a3d5901659675286e3443a6a7322ea084ea57b
Peder Meisner Lyngby <pmely@slid.fysik.dtu.dk>
2023-02-09 10:19:36 +0100

    flake8

--- asr/phonons.py
+++ asr/phonons.py
@@ -72,12 +72,12 @@ def calculate(n: int = 2, ecut: float = 800, kptdensity: float = 6.0,
     with paropen('phonons.txt', mode='a') as fd:
         params['txt'] = fd
         with get_calculator()(**params) as calc:
-                supercell = [n if periodic else 1 for periodic in atoms.pbc]
-                p = Phonons(atoms=atoms, calc=calc, supercell=supercell)
-                if world.rank == 0:
-                    p.cache.strip_empties()
-                world.barrier()
-                p.run()
+            supercell = [n if periodic else 1 for periodic in atoms.pbc]
+            p = Phonons(atoms=atoms, calc=calc, supercell=supercell)
+            if world.rank == 0:
+                p.cache.strip_empties()
+            world.barrier()
+            p.run()
 
 def requires():
     return ['results-asr.phonons@calculate.json']
