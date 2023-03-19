854deaa92de4cc0515535e445cec64fd150035c5
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-03-19 16:35:02 +0100

    make worker use mpi directly

--- asr/core/worker.py
+++ asr/core/worker.py
@@ -1,7 +1,19 @@
 from htwutil.worker import main
 from asr.core.repository import ASRRepository
+from gpaw.mpi import world
+
+
+class HTWCommunicatorWrapper:
+    def __init__(self, comm):
+        self._comm = comm
+        self.rank = comm.rank
+        self.size = comm.size
+
+    def broadcast_object(self, obj):
+        from ase.parallel import broadcast
+        return broadcast(obj, root=0, comm=self._comm)
 
 
 if __name__ == '__main__':
     repo = ASRRepository.find()
-    main(repo)
+    main(repo, comm=HTWCommunicatorWrapper(world))
