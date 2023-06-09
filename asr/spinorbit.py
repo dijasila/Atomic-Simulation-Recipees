6a7cb0211477d24207f063de772eeed4db34f089
Joachim Sødequist <joaso@dtu.dk>
2023-06-09 15:07:03 +0200

    Remove Groundstate factorization again, and replace with calculator

--- asr/spinorbit.py
+++ asr/spinorbit.py
@@ -1,6 +1,4 @@
 from asr.core import command, option, ASRResult, prepare_result
-from typing import List
-from itertools import product
 import numpy as np
 
 
@@ -12,6 +10,8 @@ def sphere_points(distance=None):
 
     import math
     N = math.ceil(129600 / (math.pi) * 1 / distance**2)
+    if N <= 1:
+        return np.array([0.]), np.array([0.])
 
     A = 4 * math.pi
     a = A / N
@@ -37,32 +37,7 @@ def sphere_points(distance=None):
         import warnings
         warnings.warn('xy-plane not included in sampling')
 
-    return thetas * 180 / math.pi, phis * 180 / math.pi % 180
-
-
-class GroundState:
-    def __init__(self, gpw):
-        self.gpw = gpw
-
-    def is_collinear(self):
-        from gpaw import GPAW
-        calc = GPAW(self.gpw)
-
-        is_collinear = 'qspiral' not in calc.parameters['mode'].keys()
-
-        if not is_collinear:
-            qn = calc.parameters['mode']['qspiral']
-            if np.linalg.norm(qn) < 1e-14:
-                is_collinear = True
-
-        return is_collinear
-
-    def band_energy(self, projected, theta, phi, occcalc):
-        from gpaw.spinorbit import soc_eigenstates
-        return soc_eigenstates(
-            calc=self.gpw, projected=projected,
-            theta=theta, phi=phi,
-            occcalc=occcalc).calculate_band_energy()
+    return thetas * 180 / math.pi, phis * 180 / math.pi
 
 
 @prepare_result
@@ -76,8 +51,8 @@ class PreResult(ASRResult):
                         'phi_tp': 'Orientation of magnetic order from x->y [deg]',
                         'projected_soc': 'Projected SOC for non-collinear spin spirals'}
 
-    def stereographic_projection(self):
-        
+    def get_projected_points(self):
+
         def stereo_project_point(inpoint, axis=0, r=1, max_norm=1):
             point = np.divide(inpoint * r, inpoint[axis] + r)
             point[axis] = 0
@@ -97,44 +72,50 @@ class PreResult(ASRResult):
         return projected_points
 
 
-
 @command(module='asr.spinorbit',
          resources='1:4h')
 @option('--gpw', help='The file path for the GPAW calculator context.', type=str)
 @option('--distance', type=float,
         help='Distance between sample points on the sphere')
-@option('--projected_soc', type=bool,
+@option('--projected', type=bool,
         help='For non-collinear spin spirals, projected SOC should be applied (True)')
 @option('--width', type=float,
         help='The fermi smearing of the SOC calculation (eV)')
-def calculate(gpw: str = 'gs.gpw', distance: float = 2.0,
-              projected_soc: bool = None, width: float = 0.001) -> ASRResult:
+def calculate(gpw: str = 'gs.gpw', distance: float = 10.0,
+              projected: bool = None, width: float = 0.001) -> ASRResult:
     '''Calculates the spin-orbit coupling at equidistant points on a unit sphere. '''
+    from gpaw import GPAW
+    calc = GPAW(gpw)
+    return _calculate(calc, distance, projected, width)
 
-    gs = GroundState(gpw)
-    return _calculate(gs, distance, projected_soc, width)
 
-
-def _calculate(gs: GroundState, distance: float,
-               projected_soc: bool, width: float) -> ASRResult:
+def _calculate(calc, distance: float,
+               projected: bool, width: float) -> ASRResult:
 
     from gpaw.occupations import create_occ_calc
+    from gpaw.spinorbit import soc_eigenstates
+
     occcalc = create_occ_calc({'name': 'fermi-dirac', 'width': width})
 
-    projected_soc = not gs.is_collinear() if projected_soc is None else projected_soc
+    is_collinear = 'qspiral' not in calc.parameters['mode'].keys()
+    if not is_collinear:
+        qn = calc.parameters['mode']['qspiral']
+        assert qn is not None, "qspiral must be a List or similar iterable"
+        assert len(qn) == 3, f"The length of qspiral must be 3, we found {len(qn)}"
+        if np.linalg.norm(qn) < 1e-14:
+            is_collinear = True
+
+    projected = not is_collinear if projected is None else projected
     theta_tp, phi_tp = sphere_points(distance=distance)
 
     soc_tp = np.array([])
     for theta, phi in zip(theta_tp, phi_tp):
-        en_soc = gs.band_energy(projected=projected_soc, theta=theta, phi=phi,
-                                occcalc=occcalc)
-        # Noise should not be an issue since it is the same for the calculator
-        # en_soc_0 = soc_eigenstates(calc, projected=projected, scale=0.0,
-        #                            theta=theta, phi=phi).calculate_band_energy()
+        en_soc = soc_eigenstates(calc=calc, projected=projected, theta=theta, phi=phi,
+                                 occcalc=occcalc).calculate_band_energy()
         soc_tp = np.append(soc_tp, en_soc)
 
     return PreResult.fromdata(soc_tp=soc_tp, theta_tp=theta_tp, phi_tp=phi_tp,
-                              projected_soc=projected_soc)
+                              projected_soc=projected)
 
 
 def webpanel(result, row, key_descriptions):
@@ -142,7 +123,7 @@ def webpanel(result, row, key_descriptions):
     rows = [['Spinorbit bandwidth', str(np.round(result.get('soc_bw'), 1))],
             ['Spinorbit Minimum (&theta;, &phi;)', '('
              + str(np.round(result.get('theta_min'), 1))
-             + ', ' + str(np.round(result.get('phi_min')[1], 1)) + ')']]
+             + ', ' + str(np.round(result.get('phi_min'), 1)) + ')']]
     spiraltable = {'type': 'table',
                    'header': ['Property', 'Value'],
                    'rows': rows}
@@ -197,15 +178,14 @@ def plot_stereographic_energies(row, fname, display_sampling=False):
     from matplotlib import pyplot as plt
     from scipy.interpolate import griddata
 
-    socdata = row.data.get('results-asr.spinorbit@calculate.json')
-    soc = (socdata['soc_tp'] - min(socdata['soc_tp'])) * 10**3
-
+    results = row.data.get('results-asr.spinorbit@calculate.json')
+    soc = (results['soc_tp'] - min(results['soc_tp'])) * 10**3
+    projected_points = results.get_projected_points()
     plt.figure(figsize=(5 * 1.25, 5))
     ax = plt.gca()
+
     norm = Normalize(vmin=min(soc), vmax=max(soc))
-
     X, Y, Z = np.array(projected_points).T
-
     xi = np.linspace(min(X), max(X), 100)
     yi = np.linspace(min(Y), max(Y), 100)
     zi = griddata((X, Y), soc, (xi[None, :], yi[:, None]))
