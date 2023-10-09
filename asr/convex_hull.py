011c84354c8a06e0c7dd73593daef6792ff1b652
Peder Meisner Lyngby <pederlyngby@gmail.com>
2023-10-09 15:42:06 +0200

    formatting

--- asr/convex_hull.py
+++ asr/convex_hull.py
@@ -546,174 +546,209 @@ def plot(row, fname, thisrow):
 
 def plot_html(row, fname, thisrow):
     from ase.phasediagram import PhaseDiagram
-    import matplotlib.pyplot as plt
-    import plotly
+    from plotly.offline import plot
     import plotly.graph_objs as go
     import plotly.express as px
-    import pandas 
+    import pandas
+    import re
 
     colors = px.colors.qualitative.D3
 
-    data = row.data['results-asr.convex_hull.json']
+    data = row.data["results-asr.convex_hull.json"]
 
     count = row.count_atoms()
     if not (2 <= len(count) <= 3):
         return
 
-    references = data['references']
+    references = data["references"]
 
     df_ref = pandas.DataFrame(references)
 
     # Why are we even using thisrow? Isn't it the same as row?
-    df_ref['thisrow']= df_ref.apply(lambda x: True if x['uid'] == thisrow.uid else False, axis=1)
+    df_ref["thisrow"] = df_ref.apply(
+        lambda x: True if x["uid"] == thisrow.uid else False, axis=1
+    )
 
-
-    names = [ref['label'] for ref in references]
+    names = [ref["label"] for ref in references]
     latexnames = [
-        format(
-            Formula(name.split(' ')[0]).reduce()[0],
-            'html'
-        )
-        for name in names
+        format(Formula(name.split(" ")[0]).reduce()[0], "html") for name in names
     ]
 
-    df_ref['latexname'] = latexnames
+    df_ref["latexname"] = latexnames
 
     # Highlight this material by making it bold
-    name_coloumn_to_plot = 'latexname'
-    df_ref.loc[df_ref['thisrow'], name_coloumn_to_plot] = '<b>' +  df_ref[df_ref['thisrow']][name_coloumn_to_plot].values[0] + '</b>'
+    name_coloumn_to_plot = "latexname"
+    df_ref.loc[df_ref["thisrow"], name_coloumn_to_plot] = (
+        "<b>" + df_ref[df_ref["thisrow"]][name_coloumn_to_plot].values[0] + "</b>"
+    )
 
     pdrefs = []
 
     for reference in references:
-        h = reference['natoms'] * reference['hform']
-        pdrefs.append((reference['formula'], h))
-    
+        h = reference["natoms"] * reference["hform"]
+        pdrefs.append((reference["formula"], h))
 
     pd = PhaseDiagram(pdrefs, verbose=False)
-
-    hull_energies = get_hull_energies(pd)
 
     if len(count) == 2:
         xcoord, energy, _, hull, simplices, xlabel, ylabel = pd.plot2d2()
 
-        df_ref['xcoord'] = xcoord
-        df_ref['energy'] = energy
-        df_ref['hull'] = hull
-
-
+        df_ref["xcoord"] = xcoord
+        df_ref["energy"] = energy
+        df_ref["hull"] = hull
 
         figs = []
 
         for i, j in simplices:
-            fig_temp = px.line(x=xcoord[[i, j]], y=energy[[i, j]],color_discrete_sequence=[colors[2]])
+            fig_temp = px.line(
+                x=xcoord[[i, j]], y=energy[[i, j]], color_discrete_sequence=[colors[2]]
+            )
             figs.append(fig_temp)
 
         delta = energy.ptp() / 30
         ymin = energy.min() - 2.5 * delta
         A, B = pd.symbols
 
-        xlabel_text = f'{A}<sub>1-x</sub>{B}<sub>x</sub>'
+        xlabel_text = f"{A}<sub>1-x</sub>{B}<sub>x</sub>"
 
-        hover_data = {'xcoord': True, 'energy': ':.2f', 'legend': False, 'latexname': True, 'uid': True, 'name': True}
-        fig_temp = px.scatter(df_ref,x='xcoord',y='energy',color='legend',symbol='legend', hover_data=hover_data, custom_data=['link'],
-                    labels={
-                     "xcoord": xlabel_text + ', x',
-                     "energy": '\u0394H [eV/atom]',
-                     'latexname': 'Formula',
-                    },
-                    color_discrete_sequence=colors,
-                    symbol_sequence=['circle', 'circle-open'])
-        
+        hover_data = {
+            "xcoord": True,
+            "energy": ":.2f",
+            "legend": False,
+            "latexname": True,
+            "uid": True,
+            "name": True,
+        }
+        fig_temp = px.scatter(
+            df_ref,
+            x="xcoord",
+            y="energy",
+            color="legend",
+            symbol="legend",
+            hover_data=hover_data,
+            custom_data=["link"],
+            labels={
+                "xcoord": xlabel_text + ", x",
+                "energy": "\u0394H [eV/atom]",
+                "latexname": "Formula",
+            },
+            color_discrete_sequence=colors,
+            symbol_sequence=["circle", "circle-open"],
+        )
+
         # Set edgecolor to same color as facecolor
-        fig_temp.data[0].marker.line.color= colors[0]
-        fig_temp.data[1].marker.line.color= colors[1]
+        fig_temp.data[0].marker.line.color = colors[0]
+        fig_temp.data[1].marker.line.color = colors[1]
 
         figs.append(fig_temp)
-    
 
-       
-        fig = go.Figure(data=sum([fig.data for fig in figs], ()), layout_yaxis_range=[ymin,0.1])
+        fig = go.Figure(
+            data=sum([fig.data for fig in figs], ()), layout_yaxis_range=[ymin, 0.1]
+        )
 
         #  Highlight materials on the hull with formula and thisrow
         materials_with_text = df_ref[df_ref.hull | df_ref.thisrow]
 
         for row in materials_with_text.itertuples(index=False):
-            fig.add_annotation(x=row.xcoord, y=row.energy,
-                                    text=row.latexname,
-                                    xanchor="left",
-                                    showarrow=False,
-                                    xshift=10,
-                                    )
+            fig.add_annotation(
+                x=row.xcoord,
+                y=row.energy,
+                text=row.latexname,
+                xanchor="left",
+                showarrow=False,
+                xshift=10,
+            )
 
-        fig.update_traces(textposition='middle right',
-                          marker={'size': 8},
-                          marker_line_width=2)
+        fig.update_traces(
+            textposition="middle right", marker={"size": 8}, marker_line_width=2
+        )
         fig.update_layout(
             xaxis_title=xlabel_text,
-            yaxis_title='\u0394H [eV/atom]',
-            yaxis=dict(zerolinecolor='lightgrey'),
-            xaxis=dict(zerolinecolor='lightgrey'),
+            yaxis_title="\u0394H [eV/atom]",
+            yaxis=dict(zerolinecolor="lightgrey"),
+            xaxis=dict(zerolinecolor="lightgrey"),
             legend=dict(
-                    orientation="h",
-                    entrywidth=100,
-                    yanchor="bottom",
-                    y=1.1,
-                    xanchor="left",
-                    x=0.01,
-                    font = dict(size=14)
-                    ),
-            width=500, height=500,
-            plot_bgcolor='white'
-            )
+                orientation="h",
+                entrywidth=100,
+                yanchor="bottom",
+                y=1.1,
+                xanchor="left",
+                x=0.01,
+                font=dict(size=14),
+            ),
+            width=500,
+            height=500,
+            plot_bgcolor="white",
+        )
 
         fig.update_xaxes(
             mirror=True,
-            ticks='outside',
+            ticks="outside",
             showline=True,
-            linecolor='black',
-            gridcolor='lightgrey'
+            linecolor="black",
+            gridcolor="lightgrey",
         )
         fig.update_yaxes(
             mirror=True,
-            ticks='outside',
+            ticks="outside",
             showline=True,
-            linecolor='black',
-            gridcolor='lightgrey',
+            linecolor="black",
+            gridcolor="lightgrey",
         )
 
     else:
         x, y, e = pd.points[:, 1:].T
-        df_ref['x'] = x
-        df_ref['y'] = y
-        df_ref['e'] = e
+        df_ref["x"] = x
+        df_ref["y"] = y
+        df_ref["e"] = e
 
         hull = np.array(pd.hull)
-        df_ref['hull'] = hull
-        
+        df_ref["hull"] = hull
+
         figs = []
         for i, j, k in pd.simplices:
-            fig_temp = go.Figure(data=[go.Mesh3d(x=x[[i, j, k, i]],
-                                                 y=y[[i, j, k, i]],
-                                                 z=e[[i, j, k, i]], 
-                                                 color=colors[2], opacity=0.5),])
-            fig_temp.update_traces(hoverinfo='skip')
+            fig_temp = go.Figure(
+                data=[
+                    go.Mesh3d(
+                        x=x[[i, j, k, i]],
+                        y=y[[i, j, k, i]],
+                        z=e[[i, j, k, i]],
+                        color=colors[2],
+                        opacity=0.5,
+                    ),
+                ]
+            )
+            fig_temp.update_traces(hoverinfo="skip")
             figs.append(fig_temp)
 
         # Plot materials
-        hover_data = {'x': False, 'y': False, 'e': ':.2f', 'legend': False, 'latexname': True, 'uid': True, 'name': True}
-        fig_temp = px.scatter_3d(df_ref, x='x', y='y', z='e', hover_data=hover_data, color='legend', custom_data=['link'], 
-                                color_discrete_sequence=colors,
-                                labels={"x": pd.symbols[1],
-                                        "y": pd.symbols[2],
-                                        "e": '\u0394H [eV/atom]',
-                                        'latexname': 'Formula',
-                                    },)
-        fig_temp.update_traces(marker={'size': 6})
+        hover_data = {
+            "x": False,
+            "y": False,
+            "e": ":.2f",
+            "legend": False,
+            "latexname": True,
+            "uid": True,
+            "name": True,
+        }
+        fig_temp = px.scatter_3d(
+            df_ref,
+            x="x",
+            y="y",
+            z="e",
+            hover_data=hover_data,
+            color="legend",
+            custom_data=["link"],
+            color_discrete_sequence=colors,
+            labels={
+                "x": pd.symbols[1],
+                "y": pd.symbols[2],
+                "e": "\u0394H [eV/atom]",
+                "latexname": "Formula",
+            },
+        )
+        fig_temp.update_traces(marker={"size": 6})
         figs.append(fig_temp)
-
-
-
 
         delta = e.ptp() / 30
         ymin = e.min() - 2.5 * delta
@@ -723,73 +758,50 @@ def plot_html(row, fname, thisrow):
         materials_with_text = df_ref[df_ref.hull | df_ref.thisrow]
         annotations = []
         for row in materials_with_text.itertuples(index=False):
-
-            annotations.append(dict(
-                showarrow=False,
-                x=row.x,
-                y=row.y,
-                z=row.e,
-                text=row.latexname,
-                xanchor="left",
-                xshift=10,
-                opacity=0.7))
+            annotations.append(
+                dict(
+                    showarrow=False,
+                    x=row.x,
+                    y=row.y,
+                    z=row.e,
+                    text=row.latexname,
+                    xanchor="left",
+                    xshift=10,
+                    opacity=0.7,
+                )
+            )
 
         fig.update_layout(
-            width=500, 
+            width=500,
             height=500,
             scene=dict(
                 xaxis_title=pd.symbols[1],
                 yaxis_title=pd.symbols[2],
-                zaxis_title='\u0394H [eV/atom]',
+                zaxis_title="\u0394H [eV/atom]",
                 zaxis=dict(range=[ymin, 0.1]),
                 annotations=annotations,
+                aspectratio={"x": 1, "y": 1, "z": 1},
             ),
-            )
-        
-        fig.layout.scene.aspectratio = {'x':1, 'y':1, 'z':1}
-        fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-
-        
-        fig.update_layout(
-            plot_bgcolor='white'
+            margin=dict(l=0, r=0, b=0, t=0),
+            legend=dict(
+                orientation="h",
+                entrywidth=100,
+                yanchor="bottom",
+                y=0.9,
+                xanchor="left",
+                x=0.01,
+                font=dict(size=14),
+            ),
         )
-        fig.update_xaxes(
-            mirror=True,
-            ticks='outside',
-            showline=True,
-            linecolor='black',
-            gridcolor='lightgrey'
-        )
-        fig.update_yaxes(
-            mirror=True,
-            ticks='outside',
-            showline=True,
-            linecolor='black',
-            gridcolor='lightgrey',
-        )
-
-        fig.update_layout(yaxis=dict(zerolinecolor='lightgrey'),
-                          xaxis=dict(zerolinecolor='lightgrey'),
-                          legend=dict(
-                                    orientation="h",
-                                    entrywidth=100,
-                                    yanchor="bottom",
-                                    y=0.9,
-                                    xanchor="left",
-                                    x=0.01,
-                                    font = dict(size=14)
-                                    )
-                          )
 
     # Below would be enough if we didn't want to make the plot clickable
-    #fig.write_html(fname, include_mathjax='cdn', include_plotlyjs='cdn')
+    # fig.write_html(fname, include_mathjax='cdn', include_plotlyjs='cdn')
 
-
-    # Makes plots clickable to go to material page
-    from plotly.offline import plot
-    import re
+    # Make plots clickable to go to material page
     # Get HTML representation of plotly.js and this figure
-    plot_div = plot(fig, output_type='div', include_mathjax='cdn', include_plotlyjs='cdn')
+    plot_div = plot(
+        fig, output_type="div", include_mathjax="cdn", include_plotlyjs="cdn"
+    )
 
     # Get id of html div element that looks like
     # <div id="301d22ab-bfba-4621-8f5d-dc4fd855bb33" ... >
@@ -797,7 +809,7 @@ def plot_html(row, fname, thisrow):
     div_id = res.groups()[0]
 
     # Build JavaScript callback for handling clicks
-    # and opening the URL in the trace's customdata 
+    # and opening the URL in the trace's customdata
     js_callback = """
     <script>
     var plot_element = document.getElementById("{div_id}");
@@ -810,7 +822,9 @@ def plot_html(row, fname, thisrow):
         }}
     }})
     </script>
-    """.format(div_id=div_id)
+    """.format(
+        div_id=div_id
+    )
 
     # Build HTML string
     html_str = """
@@ -820,15 +834,13 @@ def plot_html(row, fname, thisrow):
     {js_callback}
     </body>
     </html>
-    """.format(plot_div=plot_div, js_callback=js_callback)
+    """.format(
+        plot_div=plot_div, js_callback=js_callback
+    )
 
     # Write out HTML file
-    with open(fname, 'w') as f:
+    with open(fname, "w") as f:
         f.write(html_str)
-
-
-    #fig.show()
-
 
 
 def convex_hull_tables(row: AtomsRow) -> List[Dict[str, Any]]:
