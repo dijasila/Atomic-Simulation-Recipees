423e6a9ce3b07b0300add5198fec2feff31204c0
tboland1 <tmabo@dtu.dk>
2023-10-20 16:52:24 +0200

    more random results that build the 'final' result

--- asr/randomresults.py
+++ asr/randomresults.py
@@ -34,8 +34,6 @@ class CalculateResult(ASRResult):
         's0_pi_km': ('Expectation value of spin in the easy-axis direction '
                      'for the Berry phases at k_2=pi'),
     }
-
-
 ######### charge_neutrality #########
 @prepare_result
 class ConcentrationResult(ASRResult):
@@ -48,8 +46,6 @@ class ConcentrationResult(ASRResult):
         defect_name='Name of the defect (see "defecttoken" of DefectInfo).',
         concentrations='List of concentration tuples containing (conc., eform @ SCEF, '
                        'chargestate).')
-
-
 @prepare_result
 class SelfConsistentResult(ASRResult):
     """Container for results under certain chem. pot. condition."""
@@ -71,3 +67,53 @@ class SelfConsistentResult(ASRResult):
         p0='Hole carrier concentration at SC Fermi level.',
         defect_concentrations='List of ConcentrationResult containers.',
         dopability='p-/n-type or intrinsic nature of material.')
+######### defect symmetry #########
+@prepare_result
+class IrrepResult(ASRResult):
+    """Container for results of an individual irreproducible representation."""
+
+    sym_name: str
+    sym_score: float
+
+    key_descriptions: typing.Dict[str, str] = dict(
+        sym_name='Name of the irreproducible representation.',
+        sym_score='Score of the respective representation.')
+@prepare_result
+class SymmetryResult(ASRResult):
+    """Container for symmetry results for a given state."""
+
+    irreps: typing.List[IrrepResult]
+    best: str
+    error: float
+    loc_ratio: float
+    state: int
+    spin: int
+    energy: float
+
+    key_descriptions: typing.Dict[str, str] = dict(
+        irreps='List of irreproducible representations and respective scores.',
+        best='Irreproducible representation with the best score.',
+        error='Error of identification of the best irreproducible representation.',
+        loc_ratio='Localization ratio for a given state.',
+        state='Index of the analyzed state.',
+        spin='Spin of the analyzed state (0 or 1).',
+        energy='Energy of specific state aligned to pristine semi-core state [eV].'
+    )
+@prepare_result
+class PristineResult(ASRResult):
+    """Container for pristine band edge results."""
+
+    vbm: float
+    cbm: float
+    gap: float
+
+    key_descriptions: typing.Dict[str, str] = dict(
+        vbm='Energy of the VBM (ref. to the vacuum level in 2D) [eV].',
+        cbm='Energy of the CBM (ref. to the vacuum level in 2D) [eV].',
+        gap='Energy of the bandgap [eV].')
+#########  #########
+
+#########  #########
+
+#########  #########
+
