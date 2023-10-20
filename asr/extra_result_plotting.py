b4624689989b641c755d9ca86df5f0fe397f8aab
tboland1 <tmabo@dtu.dk>
2023-10-20 16:52:42 +0200

    refactored more extra plotting functions

--- asr/extra_result_plotting.py
+++ asr/extra_result_plotting.py
@@ -262,6 +262,206 @@ def draw_band_edges(ax, gap):
     ax.axvspan(gap, 100, alpha=0.5, color='grey')
 
 
+# chc
+def filrefs(refs):
+    from asr.fere import formulas_eq
+    nrefs = []
+    visited = []
+    for (form, v) in refs:
+        seen = False
+        for x in visited:
+            if formulas_eq(form, x):
+                seen = True
+                break
 
+        if seen:
+            continue
+        visited.append(form)
+
+        vals = list(filter(lambda t: formulas_eq(t[0], form), refs))
+
+        minref = min(vals, key=lambda t: t[1])
+
+        nrefs.append(minref)
+
+    return nrefs
+# convex_hull
+from ase.phasediagram import PhaseDiagram
+from matplotlib import patches
+def get_hull_energies(pd: PhaseDiagram):
+    hull_energies = []
+    for ref in pd.references:
+        count = ref[0]
+        refenergy = ref[1]
+        natoms = ref[3]
+        decomp_energy, indices, coefs = pd.decompose(**count)
+        ehull = (refenergy - decomp_energy) / natoms
+        hull_energies.append(ehull)
+
+    return hull_energies
+class ObjectHandler:
+    def legend_artist(self, legend, orig_handle, fontsize, handlebox):
+        x0, y0 = handlebox.xdescent, handlebox.ydescent
+        width, height = handlebox.width, handlebox.height
+        patch = patches.Polygon(
+            [
+                [x0, y0],
+                [x0, y0 + height],
+                [x0 + 3 / 4 * width, y0 + height],
+                [x0 + 1 / 4 * width, y0],
+            ],
+            closed=True, facecolor='C2',
+            edgecolor='none', lw=3,
+            transform=handlebox.get_transform())
+        handlebox.add_artist(patch)
+        patch = patches.Polygon(
+            [
+                [x0 + width, y0],
+                [x0 + 1 / 4 * width, y0],
+                [x0 + 3 / 4 * width, y0 + height],
+                [x0 + width, y0 + height],
+            ],
+            closed=True, facecolor='C3',
+            edgecolor='none', lw=3,
+            transform=handlebox.get_transform())
+        handlebox.add_artist(patch)
+        return patch
+
+
+# defect symmetry
+def get_number_of_rows(res, spin, vbm, cbm):
+    counter = 0
+    for i in range(len(res)):
+        if (int(res[i]['spin']) == spin
+           and res[i]['energy'] < cbm
+           and res[i]['energy'] > vbm):
+            counter += 1
+
+    return counter
+def get_matrixtable_array(state_results, vbm, cbm, ef,
+                          spin, style):
+    Nrows = get_number_of_rows(state_results, spin, vbm, cbm)
+    state_array = np.empty((Nrows, 5), dtype='object')
+    rowlabels = []
+    spins = []
+    energies = []
+    symlabels = []
+    accuracies = []
+    loc_ratios = []
+    for i, row in enumerate(state_results):
+        rowname = f"{int(state_results[i]['state']):.0f}"
+        label = str(state_results[i]['best'])
+        labelstr = label.lower()
+        splitstr = list(labelstr)
+        if len(splitstr) == 2:
+            labelstr = f'{splitstr[0]}<sub>{splitstr[1]}</sub>'
+        if state_results[i]['energy'] < cbm and state_results[i]['energy'] > vbm:
+            if int(state_results[i]['spin']) == spin:
+                rowlabels.append(rowname)
+                spins.append(f"{int(state_results[i]['spin']):.0f}")
+                energies.append(f"{state_results[i]['energy']:.2f}")
+                if style == 'symmetry':
+                    symlabels.append(labelstr)
+                    accuracies.append(f"{state_results[i]['error']:.2f}")
+                    loc_ratios.append(f"{state_results[i]['loc_ratio']:.2f}")
+    state_array = np.empty((Nrows, 5), dtype='object')
+    rowlabels.sort(reverse=True)
+
+    for i in range(Nrows):
+        state_array[i, 1] = spins[i]
+        if style == 'symmetry':
+            state_array[i, 0] = symlabels[i]
+            state_array[i, 2] = accuracies[i]
+            state_array[i, 3] = loc_ratios[i]
+        state_array[i, 4] = energies[i]
+    state_array = state_array[state_array[:, -1].argsort()]
+
+    return state_array, rowlabels
+def get_transition_table(row, e_hls):
+    """Create table for HOMO-LUMO transition in both spin channels."""
+
+    transition_table = table(row, 'Kohn—Sham HOMO—LUMO gap', [])
+    for i, element in enumerate(e_hls):
+        transition_table['rows'].extend(
+            [[describe_entry(f'Spin {i}',
+                             f'KS HOMO—LUMO gap for spin {i} channel.'),
+              f'{element:.2f} eV']])
+
+    return transition_table
+def get_spin_data(data, spin):
+    """Create symmetry result only containing entries for one spin channel."""
+    spin_data = []
+    for sym in data.data['symmetries']:
+        if int(sym.spin) == spin:
+            spin_data.append(sym)
+
+    return spin_data
+def draw_levels_occupations_labels(ax, spin, spin_data, ecbm, evbm, ef,
+                                   gap, levelflag):
+    """Loop over all states in the gap and plot the levels.
+
+    This function loops over all states in the gap of a given spin
+    channel, and dravs the states with labels. If there are
+    degenerate states, it makes use of the degeneracy_counter, i.e. if two
+    degenerate states follow after each other, one of them will be drawn
+    on the left side (degoffset=0, degeneracy_counter=0), the degeneracy
+    counter will be increased by one and the next degenerate state will be
+    drawn on the right side (degoffset=1, degeneracy_counter=1). Since we
+    only deal with doubly degenerate states here, the degeneracy counter
+    will be set to zero again after drawing the second degenerate state.
+
+    For non degenerate states, i.e. deg = 1, all states will be drawn
+    in the middle and the counter logic is not needed.
+    """
+    # initialize degeneracy counter and offset
+    degeneracy_counter = 0
+    degoffset = 0
+    for sym in spin_data:
+        energy = sym.energy
+        is_inside_gap = evbm < energy < ecbm
+        if is_inside_gap:
+            spin = int(sym.spin)
+            irrep = sym.best
+            # only do drawing left and right if levelflag, i.e.
+            # if there is a symmetry analysis to evaluate degeneracies
+            if levelflag:
+                deg = [1, 2]['E' in irrep]
+            else:
+                deg = 1
+                degoffset = 1
+            # draw draw state on the left hand side
+            if deg == 2 and degeneracy_counter == 0:
+                degoffset = 0
+                degeneracy_counter = 1
+            # draw state on the right hand side, set counter to zero again
+            elif deg == 2 and degeneracy_counter == 1:
+                degoffset = 1
+                degeneracy_counter = 0
+            # intitialize and draw the energy level
+            lev = Level(energy, ax=ax, spin=spin, deg=deg,
+                        off=degoffset)
+            lev.draw()
+            # add occupation arrow if level is below E_F
+            if energy <= ef:
+                lev.add_occupation(length=gap / 15.)
+            # draw label based on irrep
+            if levelflag:
+                static = None
+            else:
+                static = 'A'
+            lev.add_label(irrep, static=static)
+def draw_band_edge(energy, edge, color, *, offset=2, ax):
+    if edge == 'vbm':
+        eoffset = energy - offset
+        elabel = energy - offset / 2
+    elif edge == 'cbm':
+        eoffset = energy + offset
+        elabel = energy + offset / 2
+
+    ax.plot([0, 1], [energy] * 2, color='black', zorder=1)
+    ax.fill_between([0, 1], [energy] * 2, [eoffset] * 2, color='grey', alpha=0.5)
+    ax.text(0.5, elabel, edge.upper(), color='w', weight='bold', ha='center',
+            va='center', fontsize=12)
+# defect info
 
 
