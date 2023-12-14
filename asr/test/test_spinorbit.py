65ce9a5879220e20d0aa3bd37c4230e18aa8bafb
Joachim Sødequist <joaso@dtu.dk>
2023-12-14 15:51:50 +0100

    Update spinorbit test to one true integration test rather than mocking GPAW

--- asr/test/test_spinorbit.py
+++ asr/test/test_spinorbit.py
@@ -3,10 +3,7 @@ from numpy import array, ndarray
 
 
 @pytest.mark.ci
-@pytest.mark.parametrize("qspiral", [None, (0, 0, 0), [0.5, 0., 0.],
-                                     array([0.11, 0.11, 0.11])])
-def test_spinorbit(asr_tmpdir_w_params, mockgpaw, test_material, get_webcontent,
-                   qspiral):
+def test_collinear_spinorbit(asr_tmpdir_w_params, test_material, get_webcontent):
     """Test of spinorbit recipe."""
     from asr.spinorbit import calculate, main
     from asr.gs import calculate as gs_calculate
@@ -14,29 +11,60 @@ def test_spinorbit(asr_tmpdir_w_params, mockgpaw, test_material, get_webcontent,
 
     test_material.write('structure.json')
     calculator = {"name": "gpaw",
-                  "mode": {"mode": "pw", "ecut": 300},
-                  "kpts": {"density": 6, "gamma": True}}
-
-    if qspiral is not None:
-        calculator['mode']['qspiral'] = qspiral
+                  "mode": {"name": "pw", "ecut": 300},
+                  "kpts": {"density": 6, "gamma": True},
+                  "txt": "gs.txt"}
 
     gs_calculate(calculator)
-
-    calculate('gs.gpw', 100.0, None, 0.001)
+    calculate(gpw='gs.gpw', distance=90.0, projected=False, width=0.001)
     result = main()
 
-    if isinstance(qspiral, ndarray) and all(qspiral == [0.11, 0.11, 0.11]) \
-       or qspiral == [0.5, 0., 0.]:
-        assert result.projected_soc is True
-    elif qspiral is None or qspiral == [0., 0., 0.] or qspiral == (0, 0, 0):
-        assert result.projected_soc is False
-    else:
-        assert False, f'Case was not tested: qspiral = {qspiral}'
-
+    assert result.projected_soc is False
+    
     if world.size == 1:
         content = get_webcontent()
         assert "<td>Spinorbitbandwidth(meV)</td>" in content, content
         assert "<td>SpinorbitMinimum(&theta;,&phi;)</td>" in content, content
+
+
+# @pytest.mark.ci
+# @pytest.mark.parametrize("qspiral", [(0, 0, 0), [0.5, 0., 0.],
+#                                      array([0.11, 0.11, 0.11])])
+# def test_spinorbit(asr_tmpdir_w_params, test_material, get_webcontent,
+#                    qspiral):
+#     """Test of spinorbit recipe."""
+#     from asr.spinorbit import calculate, main
+#     from asr.gs import calculate as gs_calculate
+#     from asr.spinspiral import spinspiral
+#     from ase.parallel import world
+
+#     # test_material.write('structure.json')
+#     magmoms = test_material.get_initial_magnetic_moments()
+#     calculator = {"mode": {"name": "pw", "ecut": 300},
+#                   "kpts": {"density": 6, "gamma": True},
+#                   "txt": "gs.txt"}
+
+#     projected=False
+#     if qspiral is not None:
+#         calculator['mode']['qspiral'] = qspiral
+#         projected=True
+
+#     spinspiral(calculator=calculator, atoms=test_material)
+#     calculate(gpw='gs.gpw', distance=100.0, projected=projected, width=0.001)
+#     result = main()
+
+#     if isinstance(qspiral, ndarray) and all(qspiral == [0.11, 0.11, 0.11]) \
+#        or qspiral == [0.5, 0., 0.]:
+#         assert result.projected_soc is True
+#     elif qspiral is None or qspiral == [0., 0., 0.] or qspiral == (0, 0, 0):
+#         assert result.projected_soc is False
+#     else:
+#         assert False, f'Case was not tested: qspiral = {qspiral}'
+
+#     if world.size == 1:
+#         content = get_webcontent()
+#         assert "<td>Spinorbitbandwidth(meV)</td>" in content, content
+#         assert "<td>SpinorbitMinimum(&theta;,&phi;)</td>" in content, content
 
 
 @pytest.mark.ci
